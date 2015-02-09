5821cf4cd09649faa707983fa675112c20d88771
ubuntu-extremeli <ubuntu@extremeli.trendi.guru>
2015-02-09 17:10:51 +0000

    box unboxed images (by using entire image) and fingerprint them -  now happening in fingerprint)db_params_mongo, and fingerprint_all_mongo.sh now fingerprints categories and then finshes off any undone images

--- fingerprint_db_params_mongo.py
+++ fingerprint_db_params_mongo.py
@@ -7,7 +7,9 @@ from bson import datetime
 import logging
 import pymongo
 import numpy as np
-
+import urllib
+import os
+import cv2
 
 def get_all_subcategories(category_collection, category_id):
     subcategories = []
@@ -23,12 +25,89 @@ def get_all_subcategories(category_collection, category_id):
     print(subcategories)
     return subcategories
 
+def get_size_from_url(url):
+    item_found = False
+    temp_dir = 'temp'
+    if not os.path.isdir(temp_dir):
+        os.makedirs(temp_dir)
+    #put images in local directory
+    FILENAME = url.split('/')[-1].split('#')[0].split('?')[0]
+    FILENAME = os.path.join(temp_dir,FILENAME)
+    res = urllib.urlretrieve(url, FILENAME)
+    #pdb.set_trace()
+    #main prog starts here
+    img = cv2.imread(FILENAME)
+    h = img.shape[0]
+    w = img.shape[1]
+    return(w,h)
+
+def fingerprint_the_unfingerprinted():
+    """
+    fingerprint everything in db - should not overwrite currently-written stuff
+    """    
+    db = pymongo.MongoClient().mydb
+    print('fingerprinting unfingerprinted items')
+    query = db.products.find().batch_size(100) # batch_size required because cursor timed out without it
+    total_items = query.count()
+    i = 1
+    n_human_boxed = 0
+    n_existing_boxes = 0
+    n_unbounded_images = 0
+    for doc in query:
+        print "Starting {i} of {total}...".format(i=i, total=total_items)
+        image_url = doc["image"]["sizes"]["XLarge"]["url"]
+        print "Image URL: {0}".format(image_url)
+        # if there is a valid human BB, skip it
+        if "human_bb" in doc.keys() and doc["human_bb"] != [0, 0, 0, 0] and doc["human_bb"] is not None:
+            chosen_bounding_box = doc["human_bb"]
+    	    print('human bounding_box:'+str(chosen_bounding_box))
+            logging.debug("Human bb found: {bb} for item: {id}".format(bb=chosen_bounding_box, id=doc["id"]))
+    	    n_human_boxed += 1
+        #otherwise if there is a valid automatically generated bb skip it
+        elif "bounding_box" in doc.keys() and doc["bounding_box"] != [0,0,0,0] and doc["bounding_box"] is not None:
+    	    chosen_bounding_box = doc["bounding_box"]
+	    print('existing bounding_box:'+str(chosen_bounding_box))
+            logging.debug("classifier bb found: {bb} for item: {id}".format(bb=chosen_bounding_box, id=doc["id"]))
+    	    if chosen_bounding_box[0] == 0 and chosen_bounding_box[1] == 0:
+		n_unbounded_images += 1
+    	    else:
+    		n_existing_boxes += 1
+        else:
+    	    w,h = get_size_from_url(image_url)
+    	    chosen_bounding_box = [0,0,w,h]
+            try:
+            	fingerprint = fp.fp(get_cv2_img_array(image_url), chosen_bounding_box)
+            	db.products.update({"id": doc["id"]},
+                                      {"$set": {"fingerprint": fingerprint.tolist(),
+                                                "fp_date": datetime.datetime.now(),
+                                                "bounding_box": np.array(chosen_bounding_box).tolist()}
+                                      })
+ 	   	print('full image bounding_box:'+str(chosen_bounding_box))
+    		n_unbounded_images += 1
+            except Exception as e:
+            	logging.warning("Exception caught while fingerprinting, skipping: {0}".format(e))
+        i += 1
+    	print('auto-boxed images:'+str(n_existing_boxes)+' human-boxed images:'+str(n_human_boxed)+' unboxed images:'+str(n_unbounded_images))
+#    	s = raw_input('hit return for next')
+
+    return(n_existing_boxes,n_human_boxed,n_unbounded_images)
 
 def main():
-    # logging.basicConfig(filename='fingerprint_db.log', level=logging.DEBUG)
+    """
+    example: python fingerprint_db_params_mongo.py mens-shirts shirtClassifier.xml true
+    to run on entire database and fingerprint anything not that doesnt already have a fingerprint, do:
+    python fingerprint_db_params_mongo.py undone
+    """
+    
+# logging.basicConfig(filename='fingerprint_db.log', level=logging.DEBUG)
     first_run = False
-
-    #example: python fingerprint_db_params_mongo.py mens-shirts shirtClassifier.xml true
+    print(sys.argv)
+    if len(sys.argv) == 2:
+        category_name = sys.argv[1]
+    	if category_name in ['undone','Undone','pending']: #fingerprint everything that doesn't currently have a fingerprint
+    		return(fingerprint_the_unfingerprinted())
+    	else:
+    		return({'error':'dont know what the fingerprint, maybe missing parameters?'})
     if len(sys.argv) >= 3:
         category_name = sys.argv[1]
         classifier_xml = sys.argv[2]
@@ -36,12 +115,10 @@ def main():
             first_run = True
     else:
         print("Missing parameters. Example use: python fingerprint_db_params_mongo.py mens-shirts shirtClassifier.xml true")
+    	return({'error':'missing parameters'})	
 
     db = pymongo.MongoClient().mydb
     query_doc = {}
-    # product_collection = db.products
-    # query_doc = {"categories": {"$elemMatch": {"id": {"$in": get_all_subcategories(db.categories, category_name)}}}}
-
     #during first run we should record date
     if first_run is True:
         db.globals.update({"_id": "FP_DATE"}, {"$set": {category_name: datetime.datetime.now()}})
