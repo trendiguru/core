5f59201c2f2593c478a49b6fb34dde3bbf226ca3
Nadav <nadav@trendiguru.com>
2015-03-08 10:59:29 +0200

    debug and correction of find_n_..

--- NNSearch.py
+++ NNSearch.py
@@ -126,9 +126,9 @@ def findNNs(targetDict, entries, nMatches):
     #print('indices'+str(indices))
     '''
 
-def find_n_nearest_neighbors(target_vector, entries, number_of_matches, distance_function):
+def find_n_nearest_neighbors(target_dict, entries, number_of_matches, distance_function):
     # list of tuples with (entry,distance). Initialize with first n distance values
-    nearest_n = [(entries[i], distance_function(entries[i][FP_KEY], target_vector))
+    nearest_n = [(entries[i], distance_function(entries[i][FP_KEY], target_dict[FP_KEY]))
                  for i in range(0, number_of_matches)]
     nearest_n.sort(key=lambda tup: tup[1])
     # last item in the list (index -1 - go python!)
@@ -136,7 +136,7 @@ def find_n_nearest_neighbors(target_vector, entries, number_of_matches, distance
 
     # Loop through remaining entries, if one of them is better, insert it in the correct location and remove last item
     for i in range(number_of_matches, len(entries)):
-        d = distance_1_k(entries[i][FP_KEY], target_vector, 1.5)
+        d = distance_1_k(entries[i][FP_KEY], target_dict, 1.5)
         if d < farthest_nearest:
             insert_at = number_of_matches-2
             while d < nearest_n[insert_at][1]:
