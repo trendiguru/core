bb7e5a125f6d6f1c1049f84e31ca3e6980fd5827
ubuntu-extremeli <ubuntu@extremeli.trendi.guru>
2015-03-10 08:54:07 +0000

    pushing all yesterdays changes

--- Utils.py
+++ Utils.py
@@ -6,6 +6,7 @@ import numpy
 import requests
 from cv2 import imread, imdecode
 import logging
+from bson import objectid
 
 
 def get_cv2_img_array(url_or_path_to_image_file_or_cv2_image_array):
@@ -33,9 +34,9 @@ def lookfor_next_unbounded_image(queryobject):
     got_unbounded_image = False
     urlN=None   #if nothing eventually is found None is returned for url
     images = queryobject["images"]
-    print('images:'+str(images))
+    #print('images:'+str(images))
     for entry in images:
-    	print('entry:'+str(entry))
+    #	print('entry:'+str(entry))
 #    	n=n+1
 #	strN=string+str(n)  #this is to build strings like 'Main Image URL angle 5' or 'Style Gallery Image 7'
 #	bbN = strN+' bb' #this builds strings like 'Main Image URL angle 5 bb' or 'Style Gallery Image 7 bb'
@@ -55,7 +56,7 @@ def lookfor_next_unbounded_image(queryobject):
 	    got_unbounded_image = True
 	    print('image is not bounded!!')
 	    return(urlN)
-    	elif not(isinstance(entry["human_bb"]),list):
+    	elif not isinstance(entry["human_bb"],list):
 	    urlN=entry['url']
 	    got_unbounded_image = True
 	    print('illegal bb!! (not a list)')
@@ -130,20 +131,40 @@ def test_lookfor_next():
     resultDict = {}
     while doc is not None:
             #training docs contains lots of different images (URLs) of the same clothing item
-    	logging.debug(str(doc))
-        print('doc:'+str(doc))
+    	#logging.debug(str(doc))
+        #print('doc:'+str(doc))
  #       for prefix in prefixes:
         url = lookfor_next_unbounded_image(doc)
         if url:
        	    resultDict["url"] = url
             resultDict["_id"] = str(doc['_id'])
-    	    print('resultDict:'+str(resultDict));
+    	    print('resultDict:'+str(resultDict))
             return resultDict
         else:
             print("no unbounded image found for string:" + str(prefix)+" in current doc")
             logging.debug("no unbounded image found for string:" + str(prefix)+ " in current doc")
     return resultDict
 
+
+
+def test_insert_bb(dict,bb):
+    db=pymongo.MongoClient().mydb
+    doc = db.good_training_set.find_one({ '_id': objectid.ObjectId(dict['_id']) }) 
+    imagelist = doc['images']
+    print('imagelist:'+str(imagelist))
+    for item in imagelist:
+    	print('item:'+str(item))
+        print('desired url:'+str(dict['url'])+'actual item url:'+str(item['url']))
+        if item['url'] == dict['url']:  #this is the right image
+        	print('MATCH')
+                item['human_bb'] = bb
+                print('imagelist after bb insertion:'+str(imagelist))
+    	 	db.good_training_set.update({"_id":objectid.ObjectId(dict["_id"])}, {'$set':{'images':imagelist}})
+    		return True
+
+def test_lookfor_and_insert():
+    dict = test_lookfor_next()
+    test_insert_bb(dict,[10,20,30,40])
 
 class GZipCSVReader:
     def __init__(self, filename):
