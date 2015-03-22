a8b38d227539ddc964af55acbbeab3b0973814b2
Lior Sabag <sabag.lior@gmail.com>
2015-03-22 12:54:22 +0200

    local variable

--- mat2obj.py
+++ mat2obj.py
@@ -5,7 +5,8 @@ import hdf5storage as h5
 
 
 class MatObj(object):
-    def __init__(self, mapping, get_keys=lambda arg: []):
+    def __init__(self, mapping, get_keys=None):
+        get_keys = get_keys or get_type_names_or_keys
         for key in get_keys(mapping):
             if get_keys(mapping[key]):
                 self.__setattr__(key, MatObj(mapping[key], get_keys))
@@ -13,15 +14,28 @@ class MatObj(object):
                 self.__setattr__(key, mapping[key])
 
 
-
-
-def mat2obj(folder=".", filename):
+def mat2obj(folder=".", filename="model2.mat"):
     f = h5.read(folder, filename, options=h5.Options.matlab_compatible)
-    return MatObj(f, get_dtype_name)
+    return MatObj(f)
 
 
-def get_dtype_name(npa):
-    if npa.dtype.names:
+def get_type_names_or_keys(npa):
+    try:
         return npa.dtype.names
-    else:
-        return []
+    except AttributeError:
+        try:
+            return npa.keys()
+        except AttributeError:
+            return None
+
+
+def dtype_dict(npa):
+    res_dict = {}
+    if type(npa) == np.ndarray and npa.dtype.fields:
+        for key, value in npa.dtype.fields.iteritems():
+            if value[0] == np.dtype('object') and npa[key].shape == (1, 1):
+                res_dict[key] = dtype_dict(npa[key][0, 0])
+            else:
+                res_dict[key] = value
+    return res_dict
+
