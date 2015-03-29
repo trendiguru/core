e52ec0733663506e6584b8b2bab8ecc46e23ea4c
jeremy rutman <jeremy.rutman@gmail.com>
2015-03-29 13:51:31 +0300

    unit testing  unit_test_rate_fingerprint now ok it seems, requiring non-negative rating and no errors - test for calculate_self_confusion_vector and rate_fingerprint

--- fingerprint_stuff/unit_test_rate_fingerprint.py
+++ fingerprint_stuff/unit_test_rate_fingerprint.py
@@ -5,12 +5,15 @@ import pymongo
 import os
 import urlparse
 # import default
-import Utils
 import unittest
 import imp
 import sys
+import numpy as np
+import subprocess
+
 import rate_fingerprint
 import fingerprint_core
+import Utils
 import NNSearch
 import constants
 
@@ -27,19 +30,20 @@ class rate_fingerprint_test(unittest.TestCase):
     #        raise RuntimeError('Test error!')
 
     def setUp(self):
+       # subprocess.call('sudo /home/jeremy/mongoloid.sh')
         pass
 
     def test_rate_fingerprint(self):
         print('test the self_rate_fingerprint fucntion in rate_fingerprint')
         rating = rate_fingerprint.self_rate_fingerprint(fingerprint_function=fingerprint_core.fp,
                               weights=np.ones(constants.fingerprint_length), distance_function=NNSearch.distance_1_k,
-                              distance_power=1.5)
+                              distance_power=0.5)
         unittest.assertTrue(rating>0)
 
     def test_calculate_self_confusion_vector(self):
-        confusion_vector = calculate_self_confusion_vector(fingerprint_function=fingerprint_function, weights=weights,
-                                                           distance_function=distance_function,
-                                                           distance_power=distance_power)
+        confusion_vector = rate_fingerprint.calculate_self_confusion_vector(fingerprint_function=fingerprint_core.fp, weights=np.ones(constants.fingerprint_length),
+                                                           distance_function=NNSearch.distance_1_k,
+                                                           distance_power=0.5)
 
         unittest.assertTrue(isinstance(confusion_vector,np.ndarray))
 
