e4a201063f73877e5fca416a63be211f5e346ab6
jeremy rutman <jeremy.rutman@gmail.com>
2015-04-06 18:54:19 +0300

    added another unit test for lookfor_next_bounded

--- unit_test_Utils.py
+++ unit_test_Utils.py
@@ -25,6 +25,12 @@ class OutcomesTest(unittest.TestCase):
         print('answer from lookfor_next_bounded_in_db:'+str(answer))
         self.assertTrue(isinstance(answer, dict) or isinstance(answer, basestring))
 
+    def test_lookfor_next_bounded_in_db_with_args(self):
+    #answer should be a dictionary of info about bb or an error string if no bb found
+        answer = Utils.lookfor_next_bounded_in_db(current_item=1, current_image=2,only_get_boxed_images=True)
+        print('answer from lookfor_next_bounded_in_db:'+str(answer))
+        self.assertTrue(isinstance(answer, dict) or isinstance(answer, basestring))
+
 if __name__ == '__main__':
     unittest.main()
 
