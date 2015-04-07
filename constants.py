db6fb2451aa3f8caffaf26edd998b67048e5439c
Nadav <nadav@trendiguru.com>
2015-04-07 10:23:38 +0300

    1. added shirtClassifier.xml to source 2. constants.py was updated with a classifier to category dictionary

--- constants.py
+++ constants.py
@@ -1,8 +1,35 @@
-#file containing constants for general tg use
+# file containing constants for general tg use
 
-#fingerprint related consts
+# fingerprint related consts
 fingerprint_length = 56
 
-#fp rating related constants
-min_images_per_doc = 10 #item has to have at least this number of pics
-max_items = 100  #max number of items to consider for rating fingerprint
+# fp rating related constants
+min_images_per_doc = 10  # item has to have at least this number of pics
+max_items = 100  # max number of items to consider for rating fingerprint
+
+# classifier to category relation
+classifier_to_category_dict = {"dressClassifier.xml": ["dresses", "bridal-mother-dresses", "bridal-bridesmaid-dresses",
+                                                       "maternity-dresses"],
+                               "pantsClassifier.xml": ["womens-pants", "mens-athletic-pants", "teen-guys-pants",
+                                                       "mens-pants", "mens-jeans", "mens-big-and-tall-jeans",
+                                                       "mens-big-and-tall-pants", "teen-girls-pants", "womens-pants",
+                                                       "athletic-pants", "jeans", "plus-size-pants", "plus-size-jeans",
+                                                       "petite-pants", "petite-jeans", "maternity-pants",
+                                                       "maternity-jeans"],
+                               "shirtClassifier.xml": ["womens-tops", "teen-girls-sweaters", "teen-girls-tops",
+                                                       "teen-girls-outerwear", "teen-girls-jackets",
+                                                       "teen-girls-sweatshirts", "athletic-tops", "athletic-jackets",
+                                                       "sweatshirts", "plus-size-outerwear", "plus-size-sweatshirts",
+                                                       "plus-size-sweaters", "plus-size-jackets", "plus-size-tops",
+                                                       "jackets", "petite-sweatshirts", "petite-outerwear",
+                                                       "petite-sweaters", "petite-jackets", "petite-tops",
+                                                       "maternity-tops", "maternity-sweaters", "maternity-jackets",
+                                                       "maternity-outerwear", "sweaters", "womens-outerwear",
+                                                       "mens-athletic-jackets", "mens-athletic-shirts",
+                                                       "teen-guys-outerwear", "teen-guys-shirts", "teen-guys-blazers",
+                                                       "teen-guys-sweaters", "mens-tee-shirts", "mens-shirts",
+                                                       "mens-outerwear", "mens-sweatshirts", "mens-sweaters",
+                                                       "mens-big-and-tall-shirts", "mens-big-and-tall-sweaters",
+                                                       "mens-big-and-tall-coats-and-jackets",
+                                                       "mens-big-and-tall-blazers"]}
+
