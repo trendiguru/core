4ad07e336151bc21c483f86b05150ea57a3518f5
Nadav <nadav@trendiguru.com>
2015-04-07 17:46:53 +0300

    1. background_removal was updated. 2. new file - kassper - contains a few kaspper features 3. find similar mongo was CHANGED! :
    now 'new_image' is first to be called (with the same args as before).
    'new_image' calls:
      - find_top_n_results - returns fp vectors and closest results
      - image_2_svg - returns a svg file of the outline mask

---
+++ kassper.py
@@ -0,0 +1,96 @@
+__author__ = 'Nadav Paz'
+
+import numpy as np
+import cv2
+import os
+import subprocess
+
+
+def get_mask(image):
+    mask = np.zeros((image.shape[0], image.shape[1]), np.uint8)
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            if image[i][j][0] == 0 and image[i][j][1] == 0 and image[i][j][2] == 0:
+                mask[i][j] = 0
+            else:
+                mask[i][j] = 255
+    return mask
+
+
+def clutter_removal(image, thresh):     # non-recursive
+    mask = get_mask(image)
+    h = mask.shape[0]
+    w = mask.shape[1]
+    mask[0:h-1, 0] = 0
+    mask[0:h-1, w-1] = 0
+    mask[0, 0:w-1] = 0
+    mask[h-1, 0:w-1] = 0
+
+    def find_blob(pixel):    # update mask with 2's at removal candidates
+        currblob = []
+        potential_blob = []
+        potential_blob.append(pixel)
+
+        def find_relevant_neighbors(pixel):
+            y = pixel[0]
+            x = pixel[1]
+            if mask[y][x-1] == 255:
+                potential_blob.append((y, x-1))
+            if mask[y-1][x] == 255:
+                potential_blob.append((y-1, x))
+            if mask[y][x+1] == 255:
+                potential_blob.append((y, x+1))
+            if mask[y+1][x] == 255:
+                potential_blob.append((y+1, x))
+            return
+
+        for pixel in potential_blob:
+            y = pixel[0]
+            x = pixel[1]
+            if mask[y][x] == 255:
+                currblob.append(pixel)
+                find_relevant_neighbors(pixel)
+                mask[y][x] = 2
+        return currblob
+
+    for i in range(0, h):
+        for j in range(0, w):
+            if mask[i][j] == 255:
+                currblob = find_blob((i, j))
+                for (n, m) in currblob:
+                    if len(currblob) < thresh:    # classified as clutter
+                        mask[n][m] = 0
+                    else:
+                        mask[n][m] = 1
+    return mask
+
+
+def skin_removal(face_image, gc_image):
+    without_skin = gc_image.copy()
+    hsv = cv2.cvtColor(gc_image, cv2.COLOR_BGR2HSV)
+    face_hsv = cv2.cvtColor(face_image, cv2.COLOR_BGR2HSV)
+    bins = 180
+    n_pixels = face_image.shape[0] * face_image.shape[1]
+    hist_hue = cv2.calcHist([face_hsv], [0], None, [bins], [0, 180])
+    hist_hue = np.divide(hist_hue, n_pixels)
+    skin_hue_list = []
+    for l in range(0, 180):
+        if hist_hue[l] > 0.013:
+            skin_hue_list.append(l)
+    for i in range(0, gc_image.shape[0]):
+        for j in range(0, gc_image.shape[1]):
+            if hsv[i][j][0] in skin_hue_list:
+                without_skin[i][j][0] = 0
+                without_skin[i][j][1] = 0
+                without_skin[i][j][2] = 0
+    return without_skin
+
+
+def mask2svg(mask, filename, address):
+    # returns path to svg, and creates svg.
+    # filename doesn't need svg extension.
+    # mask needs name of bmp img + extension.
+    # address - address in which we want to look for mask and save svg.
+    os.chdir(address)
+    subprocess.call('potrace -s ' + mask + ' -o ' + filename + '.svg' , shell=True)
+    return address + filename + '.svg'
