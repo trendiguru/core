c9b0a075148996dc46953a1ef97258a21a76bada
Nadav <nadav@trendiguru.com>
2015-04-08 13:39:14 +0300

    1. constants uptate: added svg_address. 2. finesses @ find_similar_mongo

--- constants.py
+++ constants.py
@@ -33,7 +33,10 @@ classifier_to_category_dict = {"dressClassifier.xml": ["dresses", "bridal-mother
                                                        "mens-big-and-tall-coats-and-jackets",
                                                        "mens-big-and-tall-blazers"]}
 
-
 # for web bounding box interface
 #this is for going to the previous item, highest numbered image
 max_image_val = 666
+
+svg_address = '/var/www/static/svgs/'
+
+svg_url_prefix = 'http://extremeli.trendi.guru/static/svgs/'
