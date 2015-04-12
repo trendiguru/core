85b414f6a66a24184886a61a303615d8b93b8a8f
Nadav <nadav@trendiguru.com>
2015-04-12 13:35:56 +0300

    face detect the small image

--- find_similar_mongo.py
+++ find_similar_mongo.py
@@ -88,7 +88,7 @@ def got_bb(image_url, post_id, bb=None, number_of_results=10, category_id=None):
     combined_mask = cv2.bitwise_and(fg_mask, bb_mask)                             # for sending the right mask to the fp
     gc_image = background_removal.get_masked_image(small_image, combined_mask)
     fp_vector, closest_matches = find_top_n_results(small_image, combined_mask, number_of_results, category_id)
-    face_rect = background_removal.find_face(image)
+    face_rect = background_removal.find_face(small_image)
     if len(face_rect) > 0:
         x, y, w, h = face_rect[0]
         face_image = image[y:y+h, x:x+w, :]
