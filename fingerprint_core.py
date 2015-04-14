f89b44d34384150fddbef8cb77c746154c7dda26
Nadav <nadav@trendiguru.com>
2015-04-14 13:29:08 +0300

    z

--- fingerprint_core.py
+++ fingerprint_core.py
@@ -12,6 +12,7 @@ import constants
 
 fingerprint_length = constants.fingerprint_length
 
+
 def find_color_percentages(img_array):
     """
 
@@ -90,6 +91,7 @@ def find_color_percentages(img_array):
     print('color percentages:' + str(dominant_color_percentages) + ' indices:' + str(dominant_color_indices))
     return(all_colors)
 
+
 def crop_image_to_bb(img, bb_coordinates_string_or_array):
     if isinstance(bb_coordinates_string_or_array, basestring):
         bb_array = [int(bb) for bb in string.split(bb_coordinates_string_or_array)]
@@ -108,51 +110,36 @@ def crop_image_to_bb(img, bb_coordinates_string_or_array):
 
     return cropped_img
 
-def fp(img, bounding_box=None, weights = np.ones(fingerprint_length) , histogram_length=25, use_intensity_histogram=False):
 
-    if (bounding_box is not None) and (bounding_box != np.array([0, 0, 0, 0])).all():
-        img = crop_image_to_bb(img, bounding_box)
-    #crop out the outer 1/s of the image for color/texture-based features
-    if img is None:
-        return None
-    s = 5
-    h = img.shape[1]
-    w = img.shape[0]
+def fp(img, mask, weights=np.ones(fingerprint_length), histogram_length=25, use_intensity_histogram=False):
+    n_pixels = cv2.countNonZero(mask)
 
-    if h==0 or w==0:
-        return None
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    r = [h / s, w / s, h - 2 * h / s, w - 2 * w / s]
-#    roi = np.zeros((r[3], r[2], 3), np.uint8)
-    roi = crop_image_to_bb(img,r)
-    n_pixels = roi.shape[0] * roi.shape[1]
-
-    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-    #OpenCV uses  H: 0 - 180, S: 0 - 255, V: 0 - 255
-    #histograms
+    # OpenCV uses  H: 0 - 180, S: 0 - 255, V: 0 - 255
+    # histograms
     bins = histogram_length
 
-    hist_hue = cv2.calcHist([hsv], [0], None, [bins], [0, 180])
-    hist_hue = [item for sublist in hist_hue for item in sublist]  #flatten nested
+    hist_hue = cv2.calcHist([hsv], [0], mask, [bins], [0, 180])
+    hist_hue = [item for sublist in hist_hue for item in sublist]  # flatten nested
     hist_hue = np.divide(hist_hue, n_pixels)
 
-    hist_sat = cv2.calcHist([hsv], [1], None, [bins], [0, 255])
+    hist_sat = cv2.calcHist([hsv], [1], mask, [bins], [0, 255])
     hist_sat = [item for sublist in hist_sat for item in sublist]
     hist_sat = np.divide(hist_sat, n_pixels)
 
-    hist_int = cv2.calcHist([hsv], [2], None, [bins], [0, 255])
-    hist_int = [item for sublist in hist_int for item in sublist]  #flatten nested list
+    hist_int = cv2.calcHist([hsv], [2], mask, [bins], [0, 255])
+    hist_int = [item for sublist in hist_int for item in sublist]  # flatten nested list
     hist_int = np.divide(hist_int, n_pixels)
 
-    #Uniformity  t(5)=sum(p.^ 2);
+    # Uniformity  t(5)=sum(p.^ 2);
     hue_uniformity = np.dot(hist_hue, hist_hue)
     sat_uniformity = np.dot(hist_sat, hist_sat)
     int_uniformity = np.dot(hist_int, hist_int)
 
-    #Entropy   t(6)=-sum(p. *(log2(p+ eps)));
+    # Entropy   t(6)=-sum(p. *(log2(p+ eps)));
     eps = 1e-15
-    max_log_value = np.log2(bins)  #this is same as sum of p log p
+    max_log_value = np.log2(bins)  # this is same as sum of p log p
     l_hue = -np.log2(hist_hue + eps)/max_log_value
     hue_entropy = np.dot(hist_hue, l_hue)
     l_sat = -np.log2(hist_sat + eps)/max_log_value
@@ -163,9 +150,9 @@ def fp(img, bounding_box=None, weights = np.ones(fingerprint_length) , histogram
     result_vector = [hue_uniformity, sat_uniformity, int_uniformity, hue_entropy, sat_entropy, int_entropy]
     result_vector = np.concatenate((result_vector, hist_hue, hist_sat), axis=0)
     if use_intensity_histogram:
-        result_vector = np.concatenate((result_vector,hist_int), axis=0)
+        result_vector = np.concatenate((result_vector, hist_int), axis=0)
 
-    result_vector = result_vector * weights
+    result_vector = np.multiply(result_vector, weights)
     return result_vector
 
 
