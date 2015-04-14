e8a60d5ae02d6e5d281957481bbfe0a0e3fdd1c5
jeremy rutman <jeremy.rutman@gmail.com>
2015-04-14 16:56:15 +0300

    arrgh2

--- fingerprint_stuff/unit_test_lookfor_next.py
+++ fingerprint_stuff/unit_test_lookfor_next.py
@@ -7,34 +7,21 @@ import Utils
 
 class OutcomesTest(unittest.TestCase):
 
-#examples of things to return
-#    def testPass(self):
-#        return
-
-#    def testFail(self):
-#        self.failIf(True)
-
-#    def testError(self):
-#        raise RuntimeError('Test error!')
 
     def setUp(self):
-	import pymongo
-	db=pymongo.MongoClient().mydb
+        import pymongo
 
-self.training_collection_cursor = db.training.find()  # The db with multiple figs of same item
-
-assert (self.training_collection_cursor)  # make sure training collection exists
+        db = pymongo.MongoClient().mydb
+        self.training_collection_cursor = db.training.find()  # The db with multiple figs of same item
+        assert (self.training_collection_cursor)  # make sure training collection exists
 
 #    def tear_down(self):
 #        shutil.rmtree(self.temp_dir)
 
     def test_lookfor_next(self):
-
-
-print('path=' + str(sys.path))
+        print('path=' + str(sys.path))
     
     #get image and bounding box if it exists
-        print('reached GET function in default.py: vars are '+str(vars))
         resultDict = {}  #return empty dict if no results found
         prefixes = ['Main Image URL angle ', 'Style Gallery Image ']
 
@@ -57,14 +44,13 @@ print('path=' + str(sys.path))
                     print('human bb absent has been checked')
                     self.assertTrue(resultDict)
                     return resultDict
-            self.assertTrue(False, msg='did not find the url in the document')
-            print('imageslist:' + str(imagelist))
 # add assert that bounding box is empty , along lines of doc[images][url]['bb'] is not null
-            else:
-            logging.debug("no unbounded image found for string:" + str(prefix))
-            self.assertTrue(False, msg='did not receive url from lookfor_next_unbounded_image')
+                else:
+                    self.assertTrue(False, msg='did not receive url from lookfor_next_unbounded_image')
+                    self.assertTrue(False, msg='did not find the url in the document')
+                    print('imageslist:' + str(imagelist))
 
-#		strN='Style Gallery Image '+str(n)
+# strN='Style Gallery Image '+str(n)1111
 #		print('looking for string:'+str(strN))
 
 
