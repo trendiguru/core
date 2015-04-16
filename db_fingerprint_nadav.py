f0b09fd5ac982ebd015f81e0b892b1ef9a94f729
Lior Sabag <sabag.lior@gmail.com>
2015-04-16 12:14:55 +0300

    testing for loop instead of pool.map

--- db_fingerprint_nadav.py
+++ db_fingerprint_nadav.py
@@ -126,9 +126,11 @@ def fingerprint_db(fp_version, category_id=None, num_processes=None):
     num_processes = num_processes or multiprocessing.cpu_count() - 2
     pool = multiprocessing.Pool(num_processes)
 
-    pool.map(run_fp, product_cursor[0:10])
-    pool.close()
-    pool.join()
+    for doc in product_cursor[0:10]:
+        run_fp(doc)
+    # pool.map(run_fp, product_cursor[0:10])
+    # pool.close()
+    # pool.join()
 
     print "All done!!"
 
