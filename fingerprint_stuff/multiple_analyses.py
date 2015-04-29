0f00fe3805497f153235d3d85d12779d90ecfa6e
jeremy rutman <jeremy.rutman@gmail.com>
2015-04-30 02:19:35 +0300

    multiple ratings done in separate file

---
+++ fingerprint_stuff/multiple_analyses.py
@@ -0,0 +1,36 @@
+__author__ = 'jeremy'
+import numpy as np
+
+import rate_fp
+import fingerprint_core as fp_core
+import NNSearch
+import constants
+
+max_items = 50
+fingerprint_length = constants.fingerprint_length
+
+rate_fp.analyze_fingerprint(fingerprint_function=fp_core.gc_and_fp, weights=np.ones(fingerprint_length),
+                            distance_function=NNSearch.distance_1_k,
+                            distance_power=0.5, n_docs=max_items, use_visual_output1=False,
+                            use_visual_output2=False, image_sets=None, self_reporting=None, comparisons_to_make=None,
+                            filename='regular_fp')
+
+max_items = 70
+rate_fp.analyze_fingerprint(fingerprint_function=fp_core.gc_and_fp, weights=np.ones(fingerprint_length),
+                            distance_function=NNSearch.distance_1_k,
+                            distance_power=0.5, n_docs=max_items, use_visual_output1=False,
+                            use_visual_output2=False, image_sets=None, self_reporting=None, comparisons_to_make=None,
+                            filename='regular_fp')
+
+max_items = 50
+rate_fp.analyze_fingerprint(fingerprint_function=fp_core.gc_and_fp, weights=np.ones(fingerprint_length),
+                            distance_function=NNSearch.distance_1_k,
+                            distance_power=0.7, n_docs=max_items, use_visual_output1=False,
+                            use_visual_output2=False, image_sets=None, self_reporting=None, comparisons_to_make=None,
+                            filename='gcfp.k0.7')
+
+rate_fp.analyze_fingerprint(fingerprint_function=fp_core.gc_and_fp, weights=np.ones(fingerprint_length),
+                            distance_function=NNSearch.distance_1_k,
+                            distance_power=0.9, n_docs=max_items, use_visual_output1=False,
+                            use_visual_output2=False, image_sets=None, self_reporting=None, comparisons_to_make=None,
+                            filename='gcfp.k0.9')
