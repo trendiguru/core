5893365deebee3a79cf694185e27c5d7c5610a69
Nadav <nadav@trendiguru.com>
2015-05-03 19:55:55 +0300

    added MATLAB get_boxes.m function

---
+++ get_boxes.m
@@ -0,0 +1,21 @@
+function boxes = get_boxes(im)
+
+load('PARSE_model');
+max_side = max(size(im, 1), size(im, 2));
+if max_side > 400
+im = imresize(im, 400 / max_side);
+end
+% call detect function
+boxes = detect_fast(im, model, min(model.thresh, -1));
+boxes = nms(boxes, .1); % nonmaximal suppression
+% choosing the right figure, assuming it's the biggest
+chosen = 0; i_chosen = 1;
+% i = number of figures
+for i = 1 : size(boxes, 1) % for every figure found
+if boxes(i, 104)-boxes(i, 2) > chosen
+chosen = boxes(i, 104)-boxes(i, 2);
+i_chosen = i;
+end
+end
+boxes = boxes(i_chosen, : );
+end
