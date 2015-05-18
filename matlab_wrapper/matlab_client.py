6e4b95e8174ef987c8609d14523c6b70dc39c83c
jeremy rutman <jeremy.rutman@gmail.com>
2015-05-18 12:26:15 +0300

    rearranged func order

--- matlab_wrapper/matlab_client.py
+++ matlab_wrapper/matlab_client.py
@@ -8,6 +8,21 @@ class Engine(object):
         self.conn = rpyc.connect("localhost", 18861)
         self.obtain_all = obtain_all
 
+    def rpyc_obtain_wrapper(self, remote_func):
+        """
+        RPyC usually returns proxy object - this is safer but causes problems, especially with numpy.
+        Calling obtain fixes this. It makes a copy of the remote object (pickles and sends it over).
+        Object has to be picklable...
+        see: http://rpyc.readthedocs.org/en/latest/api/utils_classic.html
+        :param remote_func:
+        :return:
+        """
+
+        def wrapped_func(*args, **kwargs):
+            return rpyc.utils.classic.obtain(remote_func(*args, **kwargs))
+
+        return wrapped_func
+
     def __getattr__(self, name):
         try:
             # first check if this is an explicitly defined attribute on the server
@@ -21,15 +36,3 @@ class Engine(object):
         d = self.conn.root.get_pose_boxes_dict(path_to_image_or_url)
         return rpyc.utils.classic.obtain(d) if d else None
 
-    def rpyc_obtain_wrapper(self, remote_func):
-        """
-        RPyC usually returns proxy object - this is safer but causes problems, especially with numpy.
-        Calling obtain fixes this. It makes a copy of the remote object (pickles and sends it over).
-        Object has to be picklable...
-        see: http://rpyc.readthedocs.org/en/latest/api/utils_classic.html
-        :param remote_func:
-        :return:
-        """
-        def wrapped_func(*args, **kwargs):
-            return rpyc.utils.classic.obtain(remote_func(*args, **kwargs))
-        return wrapped_func
