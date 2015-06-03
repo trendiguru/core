19b09f6ecc0f9111406724595298fe7080ea5eb5
Nadav <nadav@trendiguru.com>
2015-06-03 17:49:23 +0300

    new scripts file, with first function: dl_keyword_images

--- scripts.py
+++ scripts.py
@@ -1,6 +1,7 @@
 __author__ = 'Nadav Paz'
 
 import urllib
+import os
 
 import pymongo
 
@@ -10,10 +11,12 @@ from find_similar_mongo import get_all_subcategories
 
 
 def dl_keyword_images(keyword, category_id):
+    path = '/home/ubuntu/Dev/' + keyword
+    if not os.path.exists(path):
+        os.makedirs(path)
     db = pymongo.MongoClient().mydb
     query = {"categories": {"$elemMatch": {"id": {"$in": get_all_subcategories(db.categories, category_id)}}}}
     keyword_cursor = db.products.find({'$and': [{"description": {'$regex': keyword}}, query]})
-    path = '/home/ubuntu/Dev/' + keyword
     for dress in keyword_cursor:
         dress_image = Utils.get_cv2_img_array(dress['image']['sizes']['XLarge']['url'])
         if background_removal.image_is_relevant(background_removal.standard_resize(dress_image, 400)[0]):
