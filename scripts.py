cb17ecb9b33334fed1ec5d52509d58d9eef20090
Nadav <nadav@trendiguru.com>
2015-06-04 11:17:39 +0300

    dl_keyword_images update: - now you can download into dir also by category and by searching keyword in the description of items from the category.

--- scripts.py
+++ scripts.py
@@ -10,13 +10,17 @@ import background_removal
 from find_similar_mongo import get_all_subcategories
 
 
-def dl_keyword_images(keyword, category_id):
-    path = '/home/ubuntu/Dev/' + keyword
-    if not os.path.exists(path):
-        os.makedirs(path)
+def dl_keyword_images(category_id, keyword=None):
     db = pymongo.MongoClient().mydb
     query = {"categories": {"$elemMatch": {"id": {"$in": get_all_subcategories(db.categories, category_id)}}}}
-    keyword_cursor = db.products.find({'$and': [{"description": {'$regex': keyword}}, query]})
+    if keyword is None:
+        path = '/home/ubuntu/Dev/' + category_id
+        keyword_cursor = db.products.find({'$and': [{"description": {'$regex': keyword}}, query]})
+    else:
+        path = '/home/ubuntu/Dev/' + keyword
+        keyword_cursor = db.products.find({query})
+    if not os.path.exists(path):
+        os.makedirs(path)
     for dress in keyword_cursor:
         dress_image = Utils.get_cv2_img_array(dress['image']['sizes']['XLarge']['url'])
         if background_removal.image_is_relevant(background_removal.standard_resize(dress_image, 400)[0]):
