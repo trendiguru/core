06d9c4053078c36ba45dfc711949f105707356b6
jeremy rutman <jeremy.rutman@gmail.com>
2015-06-30 17:06:27 +0300

    use wrapper to avoid ValueError: Functions from the __main__ module cannot be processed by workers

--- test_rq.py
+++ test_rq.py
@@ -11,12 +11,15 @@ def count_words_at_url(url):
     return len(resp.text.split())
 
 
-if __name__ == '__main__':
-    print('starting')
-    # kill_images_collection()
-    # verify_hash_of_image('wefwfwefwe', 'http://resources.shopstyle.com/pim/c8/af/c8af6068982f408205491817fe4cad5d.jpg')
+def wrap():
     url = 'http://www.cnn.com'
     q = Queue(connection=Redis())
     result = q.enqueue(count_words_at_url, 'http://nvie.com')
 
     print(str(result) + ' words at ' + url)
+
+if __name__ == '__main__':
+    print('starting')
+    # kill_images_collection()
+    # verify_hash_of_image('wefwfwefwe', 'http://resources.shopstyle.com/pim/c8/af/c8af6068982f408205491817fe4cad5d.jpg')
+    wrap()
