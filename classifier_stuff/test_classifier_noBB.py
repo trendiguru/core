d3dbf0840fab3e12e1a940f1769c3534f22649a6
jeremy rutman <jeremy.rutman@gmail.com>
2015-07-05 11:39:59 +0300

    passing use_visual up the chain

--- classifier_stuff/test_classifier_noBB.py
+++ classifier_stuff/test_classifier_noBB.py
@@ -39,7 +39,8 @@ BLUE = [255,0,0]        # rectangle color
 def show_positives_file(positives_file):
     pass
 
-def detect_no_bb(classifier, img_array, use_visual_output=True):
+
+def detect_no_bb(classifier, img_array, use_visual_output=False):
     bbs_computer = classifier.detectMultiScale(img_array)
     if bbs_computer is () or bbs_computer is None:
         n_bbs_computer=0
@@ -205,7 +206,7 @@ def plot_confusion_matrix(m, image_dirs, classifier_names, use_visual_output=Fal
     return (date_string)
 
 
-def test_classifier(classifier, imagesDir, max_files_to_try=10000):
+def test_classifier(classifier, imagesDir, max_files_to_try=10000, use_visual_output=False):
     '''
     run classifier on all images in dict - assume only one or no target items per image
     :param classifier: the classifier xml
@@ -230,7 +231,8 @@ def test_classifier(classifier, imagesDir, max_files_to_try=10000):
         if use_visual_output is True:
             cv2.imshow('input', img_array)
             k = 0xFF & cv2.waitKey(10)
-        nMatches = detect_no_bb(classifier, img_array)
+
+        nMatches = detect_no_bb(classifier, img_array, use_visual_output=use_visual_output)
         n_extra = 0
         if nMatches:
             n_extra = nMatches - 1  # any more than 1 match is assumed wrong here
@@ -250,7 +252,9 @@ def test_classifier(classifier, imagesDir, max_files_to_try=10000):
 ###########
 #classifierNames.append("dressClassifier013.xml")
 #TODO allow specific directories, and alow recursive directory traverse for test_classifiers
-def test_classifiers(classifierDir='../classifiers/', imageDir='images', use_visual_output=True):
+
+
+def test_classifiers(classifierDir='../classifiers/', imageDir='images', use_visual_output=False):
     resultsDir='classifier_results'
     results_filename=os.path.join(resultsDir,'classifier_results_'+trainDir+'.txt')
     max_files_to_try = 1000
@@ -296,7 +300,8 @@ def test_classifiers(classifierDir='../classifiers/', imageDir='images', use_vis
         for j in range(0, len(imagedirs)):
             imagedir = imagedirs[j]
             print('classifier:' + classifier + ' image directory:' + imagedir)
-            totTargets, totMatches, totExtraMatches = test_classifier(cascade_classifier, imagedir)
+            totTargets, totMatches, totExtraMatches = test_classifier(cascade_classifier, imagedir,
+                                                                      use_visual_output=use_visual_output)
             print('totTargets:' + str(totTargets) + ' totMatches:' + str(totMatches) + ' tot ExtraMatches:' + str(
                 totExtraMatches) + '           ')
             if totTargets:
