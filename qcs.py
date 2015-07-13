73c5951ec4aab92c6b15a3fb69231fead90b6a20
Nadav <nadav@trendiguru.com>
2015-07-13 14:27:51 +0300

    bug fix

--- qcs.py
+++ qcs.py
@@ -8,9 +8,10 @@ import cv2
 import background_removal
 import Utils
 
+db = pymongo.MongoClient().mydb
+
 
 def from_image_url_to_task1(image_url):
-    db = pymongo.MongoClient().mydb
     images = db.images
     image = background_removal.standard_resize(Utils.get_cv2_img_array(image_url), 400)[0]
     if image is None:
@@ -22,13 +23,25 @@ def from_image_url_to_task1(image_url):
         # if image_dict['relevant'] and (image_dict is None or image_dict["faces"] is None):
         # do something
     relevance = background_removal.image_is_relevant(image)
-    image_obj = images.insert({'image_url': image_url,
-                               'relevant': relevance.is_relevant,
-                               'faces': relevance.faces.tolist()})
-    image_dict = images.find_one({'_id': image_obj})
-    for idx, face in enumerate(image_dict['faces']):
+    image_obj_id = images.insert({'image_url': image_url,
+                                  'relevant': relevance.is_relevant,
+                                  'people': []})
+    image_dict = images.find_one({'_id': image_obj_id})
+    for idx, face in enumerate(relevance.faces):
         x, y, w, h = face
+        image_dict['people'].append({'face': face.tolist()})
         copy = image.copy()
         cv2.rectangle(copy, (x, y), (x + w, y + h), [0, 255, 0], 2)
         cv2.imwrite('/home/ubuntu/Dev/qcs' + '/' + str(idx) + '.jpg', copy)
     return image_dict
+
+
+def validate_cats_and_send_to_bb(cats, obj_id):
+    for cat in cats:
+        if cat is None:
+            logging.warning("category is None, check the strings coming back from the QCs")
+            # cat = recall to the task1 process
+        if cat not in db.categories:
+            logging.warning("category isn't in our categories")
+            # cat = recall to the task1 process
+    return
