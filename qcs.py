78209cb55205cec9c5e7e6d0ff3e3a1f8070d3d8
Nadav <nadav@trendiguru.com>
2015-07-14 15:36:57 +0300

    z

--- qcs.py
+++ qcs.py
@@ -6,12 +6,15 @@ import binascii
 
 import pymongo
 import cv2
+from redis import Redis
+from rq import Queue
 
 import background_removal
 import Utils
 
 
 db = pymongo.MongoClient().mydb
+q = Queue(connection=Redis())
 
 
 def from_image_url_to_task1(image_url):
@@ -23,25 +26,31 @@ def from_image_url_to_task1(image_url):
             logging.warning("There's no image in the url!")
             return None
         relevance = background_removal.image_is_relevant(image)
-        image_obj_id = images.insert({'image_url': image_url,
-                                      'relevant': relevance.is_relevant,
-                                      'people': []})
-        image_dict = images.find_one({'_id': image_obj_id})
-        for idx, face in enumerate(relevance.faces):
-            x, y, w, h = face
-            image_dict['people'].append({'face': face.tolist(), 'person_id': binascii.hexlify(os.urandom(32))})
-            copy = image.copy()
-            cv2.rectangle(copy, (x, y), (x + w, y + h), [0, 255, 0], 2)
-            cv2.imwrite('/home/ubuntu/Dev/qcs' + '/' + str(idx) + '.jpg', copy)
-        return image_dict
-    else:
-        if not image_dict['relevant']:
-            logging.warning("image already exists and not relevant!")
+        if relevance.is_relevant:
+            image_dict = {'image_url': image_url,
+                          'relevant': relevance.is_relevant,
+                          'people': []}
+            for face in relevance.faces:
+                x, y, w, h = face
+                person = {'face': face, 'person_id': binascii.hexlify(os.urandom(32))}
+                copy = image.copy()
+                cv2.rectangle(copy, (x, y), (x + w, y + h), [0, 255, 0], 2)
+                image_s3_url = push_to_bucket(person['person_id'], copy)
+                person['url'] = image_s3_url
+                image_dict['people'].append(person)
+                q.enqueue(send_task1, image_s3_url, image_dict)
+            image_obj_id = images.insert(image_dict)
+            image_dict = images.find_one({'_id': image_obj_id})
             return image_dict
+        else:
+            logging.warning("image is not relevant!")
+            return image_dict
+    else:
+        logging.warning('image already exists..')
+        return image_dict
 
 
-
-
+# def send_task1(image, image_dict):
 
 def validate_cats_and_send_to_bb(category, image_id, person_id):
     image = db.images.find_one({'_id': image_id})
