00d70f58e13089b7168f6e893980675035f81e9e
Nadav <nadav@trendiguru.com>
2015-07-16 10:25:23 +0300

    z

--- qcs.py
+++ qcs.py
@@ -3,31 +3,52 @@ __author__ = 'Nadav Paz'
 import logging
 import os
 import binascii
-import boto3
+
 import pymongo
 import cv2
-from redis import Redis
+import redis
 from rq import Queue
 
+import boto3
 import background_removal
 import Utils
 
 
-db = pymongo.MongoClient().mydb
-q = Queue(connection=Redis())
+images = pymongo.MongoClient().mydb.images
+r = redis.Redis()
+q = Queue(connection=r)
+
+
+class Image(object):
+    image_url = ""
+    relevant = ""
+    people = []
+
+
+class Person(object):
+    face = []
+    id = binascii.hexlify(os.urandom(32))
+    items = []
+
+
+class Item(object):
+    id = binascii.hexlify(os.urandom(32))
+    bb = []
+    category = ""
+    fingerprint = []
+    similar_items = {}
 
 
 def from_image_url_to_task1(image_url):
-    images = db.images
-    image_dict = db.images.find_one({"image_url": image_url})
-    if not image_dict:
+    image_obj = images.find_one({"image_url": image_url})
+    if not image_obj:
         image = background_removal.standard_resize(Utils.get_cv2_img_array(image_url), 400)[0]
         if image is None:
             logging.warning("There's no image in the url!")
             return None
         relevance = background_removal.image_is_relevant(image)
         if relevance.is_relevant:
-            image_dict = {'image_url': image_url,
+            image_obj = {'image_url': image_url,
                           'relevant': relevance.is_relevant,
                           'people': []}
             for face in relevance.faces:
@@ -37,23 +58,21 @@ def from_image_url_to_task1(image_url):
                 cv2.rectangle(copy, (x, y), (x + w, y + h), [0, 255, 0], 2)
                 image_s3_url = upload_image(copy, person['person_id'])
                 person['url'] = image_s3_url
-                image_dict['people'].append(person)
-                q.enqueue(send_task1, image_s3_url, image_dict)
-            image_obj_id = images.insert(image_dict)
-            image_dict = images.find_one({'_id': image_obj_id})
-            return image_dict
+                image_obj['people'].append(person)
+                q.enqueue(send_task1, image_s3_url, image_obj)
+            image_obj_id = images.insert(image_obj)
+            image_obj = images.find_one({'_id': image_obj_id})
+            return image_obj
         else:
             logging.warning("image is not relevant!")
-            return image_dict
+            return image_obj
     else:
         logging.warning('image already exists..')
-        return image_dict
+        return image_obj
 
-
-# def send_task1(image, image_dict):
 
 def validate_cats_and_send_to_bb(category, image_id, person_id):
-    image = db.images.find_one({'_id': image_id})
+    image = images.find_one({'_id': image_id})
     if image is None:
         logging.warning('image_id inValid')
         # do something with it
@@ -73,3 +92,11 @@ def upload_image(image, name, bucket_name=None):
     bucket = s3.Bucket(name=bucket_name)
     bucket.put_object(Key=name, Body=image_string, ACL='public-read', ContentType="image/jpg")
     return "{0}/{1}/{2}.jpg".format("https://s3.eu-central-1.amazonaws.com", bucket_name, name)
+
+
+def from_obj_to_dict(obj):
+    dict = obj.__dict__
+    for key, value in dict.iteritems():
+        if type(key) is Person or type(key) is Item:
+            from_obj_to_dict(key)
+    return
