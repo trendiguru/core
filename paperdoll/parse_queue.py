304e72b598eafa1b4cd8d0162efd9f82140ff3d8
jeremy rutman <jeremy.rutman@gmail.com>
2015-07-19 16:54:08 +0300

    rq tests

--- paperdoll/parse_queue.py
+++ paperdoll/parse_queue.py
@@ -1,76 +1,23 @@
-__author__ = 'jeremy'
-__author_really_is__ = 'Nadav Paz'
+import requests
+import time
 
-import logging
-import os
-import binascii
-
-import pymongo
-import cv2
-import redis
 from rq import Queue
-
-import boto3
-import background_removal
-import Utils
+from redis import Redis
 
 
-images = pymongo.MongoClient().mydb.images
-r = redis.Redis()
-q = Queue(connection=r)
+def count_words_at_url(url):
+    resp = requests.get(url)
+    return len(resp.text.split())
 
 
-def from_image_url_to_task1(image_url):
-    image_obj = images.find_one({"image_url": image_url})
-    if not image_obj:
-        image = background_removal.standard_resize(Utils.get_cv2_img_array(image_url), 400)[0]
-        if image is None:
-            logging.warning("There's no image in the url!")
-            return None
-        relevance = background_removal.image_is_relevant(image)
-        if relevance.is_relevant:
-            image_obj = {'image_url': image_url,
-                         'relevant': relevance.is_relevant,
-                         'people': []}
-            for face in relevance.faces:
-                x, y, w, h = face
-                person = {'face': face.tolist(), 'person_id': binascii.hexlify(os.urandom(32))}
-                copy = image.copy()
-                cv2.rectangle(copy, (x, y), (x + w, y + h), [0, 255, 0], 2)
-                image_s3_url = upload_image(copy, person['person_id'])
-                person['url'] = image_s3_url
-                image_obj['people'].append(person)
-                # q.enqueue(send_task1, image_s3_url, image_obj)
-            image_obj_id = images.insert(image_obj)
-            image_obj = images.find_one({'_id': image_obj_id})
-            return image_obj
-        else:
-            logging.warning("image is not relevant!")
-            return image_obj
-    else:
-        logging.warning('image already exists..')
-        return image_obj
+# Tell RQ what Redis connection to use
+redis_conn = Redis()
+q = Queue('jeremyTest', connection=redis_conn)  # no args implies the default queue
 
+# Delay execution of count_words_at_url('http://nvie.com')
+job = q.enqueue(count_words_at_url, 'http://nvie.com')
+print job.result  # => None
 
-def validate_cats_and_send_to_bb(category, image_id, person_id):
-    image = images.find_one({'_id': image_id})
-    if image is None:
-        logging.warning('image_id inValid')
-        # do something with it
-    for person in image['people']:
-        if person['person_id'] == person_id:
-            current_person = person
-    if len(current_person['items']) == 0:
-        current_person['items'] = []
-    current_person['items'].append({'category': category})
-    return image
-
-
-def upload_image(image, name, bucket_name=None):
-    image_string = cv2.imencode(".jpg", image)[1].tostring()
-    bucket_name = bucket_name or "tg-boxed-faces"
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(name=bucket_name)
-    bucket.put_object(Key="{0}.jpg".format(name), Body=image_string, ACL='public-read', ContentType="image/jpg")
-    return "{0}/{1}/{2}.jpg".format("https://s3.eu-central-1.amazonaws.com", bucket_name, name)
-
+# Now, wait a while, until the worker is finished
+time.sleep(2)
+print job.result  # => 889
