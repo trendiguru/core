ed231d34dee4b21d1756f3b829d028471d1ce7c4
yonti <yontilevin@gmail.com>
2015-07-26 15:14:23 +0300

    max to min

--- background_removal.py
+++ background_removal.py
@@ -83,7 +83,6 @@ def choose_faces(image, faces_list, max_num_of_faces):
     else:
         return relevant_faces
 
-
 def face_is_relevant(image, face):
     x, y, w, h = face
     # threshold = face + 4 faces down = 5 faces
