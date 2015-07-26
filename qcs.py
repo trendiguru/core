b9f8431af0db75ac501f77d5392eeeb402cf1ad2
jeremy rutman <jeremy.rutman@gmail.com>
2015-07-26 14:27:01 +0300

    bb voting - take avg of legal boxes dump anything with iou<threshold(0.5) return avg of remaining

--- qcs.py
+++ qcs.py
@@ -2,29 +2,22 @@ __author__ = 'Nadav Paz'
 
 # theirs
 import logging
-import os
-import binascii
-import requests
 
 import pymongo
 import cv2
 import redis
 from rq import Queue
+import bson
 import boto3
 import numpy as np
 
-
-
-
-# ours
+import find_similar_mongo
 import background_removal
 import Utils
 import constants
-import find_similar_mongo
 
 
 QC_URL = 'http://www.clickworkers.com'
-# Is there only one URL for the whole shebang or different urls for diffrent tasks?
 db = pymongo.MongoClient().mydb
 images = pymongo.MongoClient().mydb.images
 r = redis.Redis()
@@ -37,7 +30,7 @@ q7 = Queue('receive_20s_results', connection=r)
 q8 = Queue('send_last_20', connection=r)
 q9 = Queue('receive_final_results', connection=r)
 
-#I propose to combine q6 and q8 into one q, and q7 and q9 into another, and they take arguments of how many results to show each worker and how many workers JR
+
 def upload_image(image, name, bucket_name=None):
     image_string = cv2.imencode(".jpg", image)[1].tostring()
     bucket_name = bucket_name or "tg-boxed-faces"
@@ -45,6 +38,21 @@ def upload_image(image, name, bucket_name=None):
     bucket = s3.Bucket(name=bucket_name)
     bucket.put_object(Key="{0}.jpg".format(name), Body=image_string, ACL='public-read', ContentType="image/jpg")
     return "{0}/{1}/{2}.jpg".format("https://s3.eu-central-1.amazonaws.com", bucket_name, name)
+
+
+def get_person_by_id(person_id):
+    image = images.find_one({'people.person_id': person_id})
+    for person in image['people']:
+        if person['person_id'] == person_id:
+            return person
+
+
+def get_item_by_id(item_id):
+    image = images.find_one({'people.items.item_id': item_id})
+    for person in image['people']:
+        for item in person['items']:
+            if item['item_id'] == item_id:
+                return item
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -60,96 +68,62 @@ def from_image_url_to_task1(image_url):
             logging.warning("There's no image in the url!")
             return None
         relevance = background_removal.image_is_relevant(image)
-        image_dict = {'image_url': image_url, 'relevant': relevance.is_relevant}
+        image_dict = {'image_url': image_url, 'relevant': relevance.is_relevant, '_id': bson.ObjectId()}
         if relevance.is_relevant:
             image_dict['people'] = []
             for face in relevance.faces:
                 x, y, w, h = face
-                person = {'face': face.tolist(), 'person_id': binascii.hexlify(os.urandom(32))}
+                person = {'face': face.tolist(), 'person_id': bson.ObjectId()}
                 copy = image.copy()
                 cv2.rectangle(copy, (x, y), (x + w, y + h), [0, 255, 0], 2)
-                image_s3_url = upload_image(copy, person['person_id'])
-                person['url'] = image_s3_url
+                person['url'] = upload_image(copy, str(person['person_id']))
                 image_dict['people'].append(person)
-                q2.enqueue(send_image_to_qc_categorization, image_s3_url, image_dict)
+                q2.enqueue(send_image_to_qc_categorization, person['url'], str(image_dict['_id']), str(person['id']))
         else:
             logging.warning('image is not relevant, but stored anyway..')
         images.insert(image_dict)
-        image_obj = images.find_one({'image_url': image_url})
-        return image_obj
+        return
     else:
         # TODO - understand which details are already stored and react accordingly
         return image_obj
-        # END OF FUNCTION 1
 
+
+# END OF FUNCTION 1
 
 # q2
-# FUNCTION 2
-def send_image_to_qc_categorization(image_s3_url):
-    payload = {'image_url': image_s3_url}
-    req = requests.post(QC_URL, data=payload)
-    return req.ok
-# END OF FUNCTION 2
+
+# FUNCTION 2 - Web2py send_image_to_qc_categorization
 
 # q3 - Web2Py
+
+
 # FUNCTION 3
-def get_categorization_from_qc(items_list, person_id):
-    person = db.images.find_one({'people.person_id': person_id})
-    if person is None:
-        logging.warning("Person wasn't found in DataBase!")
-        return None
+def from_categories_to_bb_task(person_url, items_list, image_id, person_id):
     if len(items_list) == 0:
         logging.warning("No items in items' list!")
-        q2.enqueue(send_image_to_qc_categorization, QC_URL, person['url'])
         return None
     items = determine_final_categories(items_list)
+    items_list = []
     for item in items:
-        item_dict = {'category': item, 'item_id': binascii.hexlify(os.urandom(32))}
-        person.append(item_dict)
-
-
-def determine_final_categories(items_list):
-    pass
-
+        item_dict = {'category': item, 'item_id': bson.ObjectId()}
+        items_list.append(item_dict)
+        q4.enqueue(send_item_to_qc_bb, person_url, image_id, person_id, item_dict)
+    images.update_one({'people.person_id': person_id}, {'$set': {'people.$.items': items_list}}, upsert=True)
+    return
 
 # END OF FUNCTION 3
 
-# q4 - 'send_to_bb'
+# q4
+
 # FUNCTION 4
-def send_bb_task_to_qc(img_url, item_category):
-    q4.enqueue(send_bb_to_qc_categorization, img_url, item_category)
+# Web2Py - send_item_to_qc_bb(person_url, image_id, person_id, item_dict)
+# END
 
+# q5 - Web2Py
 
-# QUEUE FUNCTION FOR FUNCTION 4
-def send_bb_to_qc_categorization(image_s3_url, category):
-    payload = {'image_url': image_s3_url, 'category': category}
-    req = requests.post(QC_URL, data=payload)
-    return req.ok
-
-
-# END OF QUEUE FUNCTION FOR FUNCTION 4
-# END OF FUNCTION 4
-
-# q5 - receive_bb
 # FUNCTION 5
-def receive_bb_from_qc(image_url, image_id, item_id, bb):
-    '''
-    Get bb from qc, see if there's enough to do voting. If so, call find_similar and send
-    those results to the next stage
-    :param image_url:
-    :param image_id:
-    :param item_id:
-    :param bb:
-    :return:
-    '''
-    # bb_list = get_bb_list_from_qc()
-    n_bbs_received_so_far, bb_list = put_bb_into_db(image_url, bb)
-    if n_bbs_received_so_far >= constants.N_bb_votes_required:  # probably no need for >= instead of ==
-        final_bb = determine_final_bb(bb_list)
-    n_results = constants.N_top_results_to_show[0]
-    item_category = None  # how do we get this info....
-    fp, results, svg = find_similar_mongo.got_bb(image_url, image_id, item_id, final_bb, n_results, item_category)
-    send_100_results_to_qc_in_20s(image_url, results)
+# Web2Py - bb_list = get_bb_list_from_qc()
+# END
 
 
 def determine_final_bb(bb_list):
@@ -182,7 +156,29 @@ def determine_final_bb(bb_list):
     else:
         logging.warning('no good intersections found')
         return None
+    bb = determine_final_bb(bb_list)  # Yonti's function
+    image = images.find_one({'people.items.item_id': item_id}, {})
+    category = get_item_by_id(item_id)['category']
+    fp, results, svg = find_similar_mongo.got_bb(image['image_url'], person_id, item_id, bb, N_top_results_to_show,
+                                                 category)
 
+# q6 - send_20s_resuqlts
+# FUNCTION 6
+def send_100_results_to_qc_in_20s(original_image_url, results):
+    for i in range(0, constants.N_workers[0]):  #divide results into chunks for N workers
+        final_image_index = min(i + constants.N_pics_per_worker - 1,
+                                len(results) - 1)  # the min deals with case where there's fewer images for last worker
+        chunk_of_results = results[i:final_image_index]
+        q6.enqueue(send_many_results_to_qcs, original_image_url, chunk_of_results)
+
+# QUEUE FUNC FOR FUNCTION 6
+def send_many_results_to_qcs(original_image, chunk_of_results):
+    payload = {'original_image': original_image, 'results_to_sort': chunk_of_results}
+    req = requests.post(QC_URL, data=payload)
+    return req.ok
+
+# END OF QUEUE FUNC FOR FUNCTION 6
+# END  FUNCTION 6
 
 def average_bbs(bblist):
     avg_box = [0, 0, 0, 0]
@@ -193,55 +189,62 @@ def average_bbs(bblist):
         avg_box = np.add(avg_box, bb)
         # print('avg after'+str(avg_box))
         n = n + 1
-    avg_box = np.int_(np.divide(avg_box, n))
+    avg_box = np.int(np.divide(avg_box, n))
     return avg_box
 
 
-def put_bb_into_db(image_url, bb):
-    pass
-
-
-# END
-
-
-# q6 - send_20s_resuqlts
-# FUNCTION 6
-# constants.N_top_results_to_show[i]
-# constants.N_workers[j]
-# constants.N_pics_per_worker[k]constants
-def send_100_results_to_qc_in_20s(original_image_url, results):
-    for i in range(0, constants.N_workers[0]):  #divide results into chunks for N workers
-        final_image_index = min(i + constants.N_pics_per_worker - 1,
-                                len(results) - 1)  #deal with case where there's fewer images for last worker
-        chunk_of_results = results[i:final_image_index]
-        q6.enqueue(send_many_results_to_qcs, original_image_url, chunk_of_results)
-
-
-# QUEUE FUNC FOR FUNCTION 6
-def send_many_results_to_qcs(original_image, chunk_of_results):
-    payload = {'original_image': original_image, 'results_to_sort': chunk_of_results}
-    req = requests.post(QC_URL, data=payload)
-    return req.ok
-
-
-# END OF QUEUE FUNC FOR FUNCTION 6
-# END  FUNCTION 6
-
+# q7
 # q7 - receive_20s_results
 # FUNCTION 7
 # assumption
-def get_20s_results(chunk_of_results, ratings):
-    #final_20_results = rearrange_results(sorted_results)
-    pass
+def get_20s_results(chunk_of_results, ratings, item_id):
+    final_20_results = None
+    image = images.find_one({'people.items.item_id': item_id})
+    for person in image['people']:
+        for item in person['items']:
+            if item['item_id'] == item_id:
+                if 'votes' in item:
+                    item['votes'].append([chunk_of_results, ratings])
+                    n_votes_so_far = len(item['votes']) / 2
+                    if n_votes_so_far >= constants.N_workers[0]:  # enough votes rec'd
+                        final_20_results = rearrange_results(item['votes'])
+                else:
+                    item['votes'] = [[chunk_of_results, ratings]]
+                # SOMEONE PLS REVIEW THE LINE BELOW - i've never used the mongodot notation before
+                write_result = images.update({"people.items.item_id": item_id},
+                                             {"$set": {"people.items.votes": item['votes']}})
+    return final_20_results
 
 
-# HELPER FUNCTION FOR FUNCTION 6
+def rearrange_results(votes):
+    '''
+    Take a bunch of votes. CHeck for duplicate votes (two or more dudes voting on same item).
+    Tote up all the results and send back in order
+    :param votes:
+    :return:
+    '''
+    all_results = []
+    all_ratings = []
+    for results, ratings in votes:
+        all_results = all_results.append(results)
+        all_ratings = all_ratings.append(ratings)
+
+    # combine multiple votes on same item
+    combined_results = [all_results[0]]
+    combined_ratings = [all_ratings[0]]
+    for i in range(0, len(all_results)):
+        for j in range(i + 1, all_results):
+            if all_results[i] != all_results[j]:  # different results being voted on by 2 dudes
+                combined_results = combined_results.append(all_results[j])
+                combined_ratings = combined_ratings.append(all_ratings[j])
+            else:  # same result being voted on by 2 dudes
+                combined_ratings[i] = combined_ratings[j]
+
 def send_many_results_to_qcs(original_image, chunk_of_results):
     payload = {'original_image': original_image, 'results_to_sort': chunk_of_results}
     req = requests.post(QC_URL, data=payload)
     return req.ok
 
-# END OF HELPER FOR FUNCTION 6
 # END
 
 """
@@ -257,5 +260,3 @@ send_final_20_results_to_qc_in_10s(copy, final_20_results)
 final_results = get_final_results_from_qc()
 insert_final_results(item.id, final_results)
 """
-
-
--- unit_test_qcs.py
+++ unit_test_qcs.py
@@ -2,7 +2,7 @@ __author__ = 'jeremy'
 import unittest
 
 import qcs
-
+import constants
 
 class OutcomesTest(unittest.TestCase):
     # examples of things to return
@@ -34,6 +34,19 @@ class OutcomesTest(unittest.TestCase):
         self.assertTrue(final_bb[2] == 101)
         self.assertTrue(final_bb[3] == 102)
 
+    def test_at_least_one_vote(self):
+        # N_bb_votes_required = 2
+        # N_category_votes_required = 2
+        self.assertTrue(len(constants.N_workers) == len(constants.N_pics_per_worker))
+        self.assertTrue(len(constants.N_workers) == len(constants.N_top_results_to_show))
+        for i in range(0, len(constants.N_workers)):
+            print('stage ' + str(i) + ':N results to show:' + str(constants.N_top_results_to_show[i]))
+            print('N workers:' + str(constants.N_workers[i]))
+            print('N pics per worker:' + str(constants.N_pics_per_worker[i]))
+            n_votes = constants.N_workers[i] * constants.N_pics_per_worker[i]
+            n_votes_per_picture = n_votes / constants.N_top_results_to_show[i]
+            print('n_votes:' + str(n_votes) + ', votes per picture:' + str(n_votes_per_picture) + '\n')
+            self.assertTrue(n_votes_per_picture > 1)
 
 if __name__ == '__main__':
     unittest.main()
