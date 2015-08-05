fcd0187c94cb48cbec9d41556c5fdad5901582c0
Nadav <nadav@trendiguru.com>
2015-08-05 17:06:25 +0300

    update

--- qcs.py
+++ qcs.py
@@ -64,6 +64,7 @@ def get_item_by_id(item_id):
 
 def decode_task(args, vars, data):  # args(list) = person_id, vars(dict) = task, data(dict) = QC results
     print "Arrived to decode_task successfully!"
+    print vars
     if vars["task_id"] is 'categorization':
         # from_categories_to_bb_task(data['items'], args[0])
         print "Arrived to decode_task with {0}".format(data)
