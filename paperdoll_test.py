6fa8f056a0a1f7de8def2a75396926ee09f4ddbc
Nadav <nadav@trendiguru.com>
2015-08-10 19:25:55 +0300

    new file paperdoll_test.py meanwhile contains only test function fot paperdoll results

--- paperdoll_test.py
+++ paperdoll_test.py
@@ -16,7 +16,7 @@ def color_paperdoll_mask(paperdoll_mask):
     for item in items_list:
         for k in range(0, 3):
             color_mask[:, :, k] = color_mask[:, :, k] + random.randint(0, 255) * np.array(
-                paperdoll_mask[:, :, k] == item)
+                paperdoll_mask == item)
     return color_mask
 
 
@@ -25,7 +25,8 @@ def pd_test(image_url):
     mask, labels, pose = paperdoll.paperdoll_parse_enqueue.paperdoll_enqueue(image_url, async=False)
     cv2.imshow('image', image)
     cv2.imshow('color_mask', color_paperdoll_mask(mask))
-    cv2.waitKey(0)
+    cv2.waitKey(0
+    C)
     label_list = []
     for num in np.unique(mask):
         # convert numbers to labels
