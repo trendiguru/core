a2f558ff5fd0843bbb157139dca00b545eadf248
root <root@pp-2.c.test-paper-doll.internal>
2015-08-17 18:05:04 +0000

    modified pd.py saving labels, pose and verifying Urls downloaded

--- paperdoll/pd.py
+++ paperdoll/pd.py
@@ -24,15 +24,23 @@ import imghdr
 def get_parse_from_matlab(image_filename):
     eng = matlab.engine.start_matlab('-nodesktop')
     mask, label_names, pose = eng.pd("inputimg.jpg", nargout=3)
+    label_dict = dict(zip(label_names, range(0, len(label_names))))
+    print('label dict'+str(label_dict))
     stripped_name = image_filename.split('.jpg')[0]
     outfilename = 'pd_output/' + stripped_name + '.png'
+    savedlabels = 'pd_output/' + stripped_name + '.lbls'
+    savedpose = 'pd_output/' + stripped_name + '.pos'
+    
     print('outfilename:' + outfilename)
     subprocess.Popen("scp -i /home/jeremy/first_aws.pem  output.png ubuntu@extremeli.trendi.guru:" + outfilename,
                      shell=True, stdout=subprocess.PIPE).stdout.read()
     subprocess.Popen("cp output.png " + outfilename, shell=True, stdout=subprocess.PIPE).stdout.read()
     subprocess.Popen("cp inputimg.jpg " + outfilename, shell=True, stdout=subprocess.PIPE).stdout.read()
+    subprocess.Popen("cp savedlabels.p " + savedlabels, shell=True, stdout=subprocess.PIPE).stdout.read()
+    subprocess.Popen("cp savedpose.p " + savedpose, shell=True, stdout=subprocess.PIPE).stdout.read()
+     
     #scp -i ~/first_aws.pem  img.jpg ubuntu@extremeli.trendi.guru:.
-    return mask, label_names, pose
+    return mask, label_dict, pose
 
 
 def get_parse_mask(image_url=None, image_filename=None):
@@ -40,7 +48,7 @@ def get_parse_mask(image_url=None, image_filename=None):
         subprocess.Popen("cp " + image_filename + " inputimg.jpg", shell=True, stdout=subprocess.PIPE).stdout.read()
         time.sleep(0.05)  # give some time for file to write
         get_parse_from_matlab(image_filename)
-        return [[],[],[]]
+        return 
     if image_url is not None:
         response = requests.get(image_url, stream=True)
 
@@ -50,16 +58,14 @@ def get_parse_mask(image_url=None, image_filename=None):
     time.sleep(0.1)  # give some time for file to write
     # img_array = imdecode(np.asarray(bytearray(response.content)), 1)
     if 'jpeg' != imghdr.what('inputimg.jpg'):
-        return
+        return [[],[],[]]
     stripped_name = image_url.split('//')[1]
     modified_name = stripped_name.replace('/', '_')
     print('stripped name:' + stripped_name)
     print('modified name:' + modified_name)
     #        cv2.imwrite(img_array,stripped_name)
-    mask, label_names, pose = get_parse_from_matlab(modified_name)
-    print('labels:' + str(label_names))
-    label_dict = dict(zip(label_names, range(0, len(label_names))))
-    print('label dict'+str(label_dict))
+    mask, label_dict, pose = get_parse_from_matlab(modified_name)
+    print('labels:' + str(label_dict))
     mask_np = np.array(mask, dtype=np.uint8)
     pose_np = np.array(pose, dtype=np.uint8)
     return mask_np, label_dict, pose_np
