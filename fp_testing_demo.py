6afe9b2a2f00eb4be67745a574fc1e5a35a09491
yonti <yontilevin@gmail.com>
2015-09-06 10:55:58 +0300

    changed the fingerprint related functions

--- fp_testing_demo.py
+++ fp_testing_demo.py
@@ -95,12 +95,13 @@ def from_svg_to_similar_results(svg_url, image_url, fp_length=fingerprint_length
             curr_item['fp'], curr_item['similar_results'] = \
                 find_similar_mongo.find_top_n_results(image, item_mask, 30, curr_item['category'], collection_name,
                                                       fp_category, fp_length, distance_func, bins)
-            curr_item['similar_results'] = [db.products.find_one({"_id": result["_id"]}, projection_dict)
+
+            top_matches = [db.products.find_one({"_id": result["_id"]}, projection_dict)
                                             for result in curr_item["similar_results"]]
             # return db.fp_testing.find_one_and_update({'items.svg_url': curr_item["svg_url"]},
             #                                          {'$set': {'items.$': curr_item}},
             #                                          return_document=pymongo.ReturnDocument.AFTER)
-            return curr_item['similar_results']
+            return top_matches
 
 def create_gc_mask(image, pd_mask, bgnd_mask):
     item_bb = bb_from_mask(pd_mask)
