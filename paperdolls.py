8a63071e75212a83e742d4258edf2e7ea8f4fd86
Nadav <nadav@trendiguru.com>
2015-09-12 18:51:10 +0300

    after pd conclusions

--- paperdolls.py
+++ paperdolls.py
@@ -255,17 +255,19 @@ def from_paperdoll_to_similar_results(person_id, mask, labels):
     for num in np.unique(mask):
         # convert numbers to labels
         category = list(labels.keys())[list(labels.values()).index(num)]
+        if category == 'null':
+            bgnd_mask = 255 * np.array(mask == num, dtype=np.uint8)
         if category in constants.paperdoll_shopstyle_women.keys():
             item_mask = 255 * np.array(mask == num, dtype=np.uint8)
-            # item_gc_mask = create_gc_mask(image, item_mask, bgnd_mask)  # (255, 0) mask
+            item_gc_mask = create_gc_mask(image, item_mask, bgnd_mask)  # (255, 0) mask
             item_dict = {"category": constants.paperdoll_shopstyle_women[category],
                          'item_id': str(bson.ObjectId()), 'item_idx': idx, 'saved_date': datetime.datetime.now()}
             svg_name = find_similar_mongo.mask2svg(
-                item_mask,
+                item_gc_mask,
                 str(image_obj['_id']) + '_' + person['person_id'] + '_' + item_dict['category'],
                 constants.svg_folder)
             item_dict["svg_url"] = constants.svg_url_prefix + svg_name
-            item_dict['fp'], item_dict['similar_results'] = find_similar_mongo.find_top_n_results(image, item_mask,
+            item_dict['fp'], item_dict['similar_results'] = find_similar_mongo.find_top_n_results(image, item_gc_mask,
                                                                                                   100,
                                                                                                   item_dict['category'])
             items.append(item_dict)
