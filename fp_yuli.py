5e37960b8030ee1d9ba0db307c0066298d64bb79
yuli <yuliha888@gmail.com>
2015-09-24 17:39:20 +0300

    commited to an institution

--- fp_yuli.py
+++ fp_yuli.py
@@ -1,14 +1,21 @@
+__author__ = 'yuli'
+
 import Utils
 import background_removal
 import kassper
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 def run_fp(image_url, bb=None):
-	image = Utils.get_cv2_img_array(image_url)
-	small_image, resize_ratio = background_removal.standard_resize(image, 400)
-	mask = get_mask(small_image, bb)
-	fp_vector = yuli_fp(small_image, mask, whaterver=63)
-	return fp_vector
+    image = Utils.get_cv2_img_array(image_url)
+    small_image, resize_ratio = background_removal.standard_resize(image, 400)
+    mask = get_mask(small_image )
+    print mask.shape
+    #cv2.imshow('mask', mask)
+    #cv2.waitKey(0)
+    fp_vector = yuli_fp(small_image, mask, whaterver=63)
+    return fp_vector
 
 # Returned mask is resized to max_side_lenth 400 
 def get_mask(small_image, bb=None):
@@ -24,5 +31,45 @@ def get_mask(small_image, bb=None):
 
 def yuli_fp(small_image, mask, whaterver=45):
 	# Write awesome function here
-	pass
+    from itertools import product, chain
+    import MR8filters
+
+    sigmas = [1, 2, 4]
+    n_sigmas = len(sigmas)
+    n_orientations = 6
+
+    edge, bar, rot = MR8filters.makeRFSfilters(sigmas=sigmas,
+            n_orientations=n_orientations)
+
+    n = n_sigmas * n_orientations
+
+    #multiply small_img with mask
+    img = cv2.cvtColor(small_image, cv2.COLOR_BGR2GRAY)
+    mask = mask
+    img = np.multiply(mask, img)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    img = img.astype(np.float)
+    #apply filters
+    filterbank = chain(edge, bar, rot)
+    n_filters = len(edge) + len(bar) + len(rot)
+    response = MR8filters.apply_filterbank(img, filterbank)
+    #fp_vector = MRFfilters(small_image, mask)
+
+    # plot responses
+
+    # plot responses
+    fig2, ax2 = plt.subplots(3, 3)
+    for axes, res in zip(ax2.ravel(), response):
+        axes.imshow(res, cmap=plt.cm.gray)
+        axes.set_xticks(())
+        axes.set_yticks(())
+    ax2[-1, -1].set_visible(False)
+    plt.show()
+
+    return response #return fp_vector
+
+
+if __name__ == "__main__":
+    run_fp('10796178-sexy-short-gown.jpg')
 
