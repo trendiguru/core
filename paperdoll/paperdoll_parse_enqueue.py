e76d5db09024464141e985bcb382982a7362183c
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-08 17:10:47 +0300

    bad comma

--- paperdoll/paperdoll_parse_enqueue.py
+++ paperdoll/paperdoll_parse_enqueue.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             'http://www.wantdresses.com/wp-content/uploads/2015/09/group-of-vsledky-obrzk-google-pro-httpwwwoblectesecz-awesome-prom-dresses.jpg',\
             'http://www.wantdresses.com/wp-content/uploads/2015/09/gowns-blue-picture-more-detailed-picture-about-awesome-strapless-awesome-prom-dresses.jpg']
     for url in urls:
-        img, labels, pose = paperdoll_enqueue(url, async=False)
+        img, labels, pose = paperdoll_enqueue(url, async = False)
         print('url:'+url)
         print('labels:'+labels)
 #        show_max(img, labels)
