301fe4f1d1b408fce5ee0a7f93dd01d47756051e
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-08 16:07:52 +0300

    \xe2

--- scorer/scorer.py
+++ scorer/scorer.py
@@ -22,8 +22,7 @@ def classification_rating(goldenset_classes,testset_classes,weights_dictionary):
     7. perform the rating calculation as follows, and return value:
         return_value = {(3.2) - (4) - (5)}/(6); if return_value < 0 -> return_value = 0
     '''
-​
-​
+
     # initial check (1):
     # for golden_class in goldenset_classes:
     #     if not isinstance(golden_class,str):
@@ -34,54 +33,45 @@ def classification_rating(goldenset_classes,testset_classes,weights_dictionary):
     #     if not isinstance(test_class,str):
     #         print "testset_classes must be strings in all list type!"
     #         return
-​
-​
+
     # initial check (2):
     if len(goldenset_classes)==0:
             print "goldenset_classes must not be empty!"
             return
-​
     if len(testset_classes)==0:
             print "testset_classes must not be empty!"
             return
-​
-​
+
     # finding matches to the goldenset (3):
     goldenset_classes = set(goldenset_classes)
     testset_classes = set(testset_classes)
     set_of_class_matches = goldenset_classes.intersection(testset_classes)
-​
-​
+
     # summing the weights of the matched classes in testset (3.1, 3.2):
     sum_weights_of_test_matches = 0
     for class_match in set_of_class_matches:
         sum_weights_of_test_matches = sum_weights_of_test_matches + weights_dictionary[class_match]
     PC = sum_weights_of_test_matches
-​
-​
+
     # how many of the goldenset are not included in the testset (4):
     NC = len(goldenset_classes) - len(set_of_class_matches)
-​
-​
+
     # how many of the testset are not included in the goldenset (5):
     PWC = len(testset_classes) - len(set_of_class_matches)
-​
-​
+
     # summing the weights of the weights for goldenset classes (6):
     sum_weights_of_goldenset_matches = 0
     for class_match in goldenset_classes:
         sum_weights_of_goldenset_matches = sum_weights_of_goldenset_matches + weights_dictionary[class_match]
     NWgolden = sum_weights_of_goldenset_matches
-​
-​
+
     # classes rating calculation (7):
     class_rating = float(PC)/NWgolden - float(NC)/NWgolden * float(PWC)/len(testset_classes)
     if class_rating < 0.0:
         class_rating = 0.0
-​
+
     return class_rating
-​
-​
+
 def results_rating(goldenset_images,testset_images):
     '''
     calculates the rating of the ordered images set of the test in comparison to the
@@ -106,8 +96,7 @@ def results_rating(goldenset_images,testset_images):
     5. calculate: Nne = sum(goldenset_image that are not in testset_images)
     6. return_value = {Ngolden - Nco - Nnco - Nne}/Ngolden; if return_value < 0 -> return_value = 0
     '''
-​
-​
+
     # initial check (1):
     # for golden_class in goldenset_images:
     #     if not isinstance(golden_class,str):
@@ -118,18 +107,16 @@ def results_rating(goldenset_images,testset_images):
     #     if not isinstance(test_class,str):
     #         print "testset_images must be strings in all list type!"
     #         return
-​
-​
+
     # initial check (2):
     if len(goldenset_images)==0:
             print "goldenset_images must not be empty!"
             return
-​
+
     if len(testset_images)==0:
             print "testset_images must not be empty!"
             return
-​
-​
+
     # find matching image names of golden and test, and find order of them (3):
     # we assume each image is listed only once in each list.
     index_of_ordered_at_goldenset = []
@@ -144,13 +131,12 @@ def results_rating(goldenset_images,testset_images):
                ordered_images.append(golden_image_name)
                testset_images_tag = testset_images[index_of_ordered_at_testset[-1]:]
                break
-​
+
     X = []
     for i in range(len(index_of_ordered_at_goldenset)):
         X.append(index_of_ordered_at_testset[i] - index_of_ordered_at_goldenset[i])
     Nco = sum(X)
-​
-​
+
     # find matching image names of golden and test, and find un-order of them (4):
     index_of_unordered_at_goldenset = []
     index_of_unordered_at_testset = []
@@ -163,40 +149,40 @@ def results_rating(goldenset_images,testset_images):
                 index_of_unordered_at_goldenset.append(goldenset_images.index(golden_image_name))
                 index_of_unordered_at_testset.append(testset_images.index(golden_image_name))
                 unordered_images.append(golden_image_name)
-​
-                # finding the weighted distance from the last ordered image:
+
+             # finding the weighted distance from the last ordered image:
                 for index in index_of_ordered_at_goldenset:
                     if (index - index_of_unordered_at_goldenset[-1]) > 0:
                         last_ordered_distance.append(index-1)
                         # break
                 weighted_unordered_distances.append(last_ordered_distance[-1] - index_of_unordered_at_testset[-1])
-​
+
     Y = []
     for i in range(len(index_of_unordered_at_goldenset)):
         Y.append(float((index_of_unordered_at_goldenset[i] - index_of_unordered_at_testset[i]) * \
                weighted_unordered_distances[i]))
     Nnco = sum(Y)
-​
+
     # finds how many of the goldenset are not included in the testset (5):
     Nne = len(set(goldenset_images).difference(testset_images))
     print [len(goldenset_images),Nco,Nnco,Nne]
-​
-​
+
+
     # classes rating calculation (6):
     images_rating = float(Nco)/len(goldenset_images) + 0.5*float(Nnco)/len(goldenset_images) - \
                     float(Nne)/len(goldenset_images)
     if images_rating < 0.0:
         images_rating = 0.0
-​
+
     return images_rating
-​
-​
+
+
 def scorer(goldenset_classes,testset_classes,weights_dictionary,goldenset_images,testset_images):
     test_classes_score = classification_rating(goldenset_classes,testset_classes,weights_dictionary)
     test_results_score = results_rating(goldenset_images,testset_images)
     return test_classes_score, test_results_score
-​
-​
+
+
 '''
 #  ##################################################################################
 #   LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB
@@ -220,8 +206,8 @@ print results_rating(goldenset_images,testset_images)
 #   LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB LAB
 ###################################################################################
 '''
-​
-​
+
+
 # Tgfunctions:
 #
 # from trendi_guru_modules..
@@ -230,27 +216,27 @@ import background_removal
 import Utils
 import paperdolls
 import constants
-​
+
 from trendi_guru_modules.paperdoll import paperdoll_parse_enqueue
-​
+
 # resize image:
 image = background_removal.standard_resize(image, 400)[0]
-​
+
 # activate paperdoll on image:
 mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image, False)
-​
+
 # task 1: get categories from image
-​
+
 # face:
 relevance = background_removal.image_is_relevant(image)
 face = relevance.faces[0]
-​
+
 final_mask = paperdolls.after_pd_conclusions(mask, labels, face)
-​
+
 #---------------------
 goldenset_classes = []
 #---------------------
-​
+
 testset_classes = []
 weights_dictionary = {}
 for num in np.unique(final_mask):
@@ -259,16 +245,14 @@ for num in np.unique(final_mask):
         testset_classes.append(category)
         # only because of this being a test, and weights (for category) dictionary is not set yet:
         weights_dictionary[category] = 1
-​
+
 print classification_rating(goldenset_classes,testset_classes,weights_dictionary)
-​
-​
+
+
 # task 2: get similar results
-​
+
 '''
 ​
 '''
     # scoring:
     # test_classes_score, test_results_score = scorer(goldenset_classes,testset_classes,weights_dictionary,goldenset_images,testset_images)
-​
-​
