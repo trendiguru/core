31f4fa14ffa519500d30c43ba68a4bb3de592ef4
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-08 17:09:17 +0300

    bad comma

--- paperdoll/paperdoll_parse_enqueue.py
+++ paperdoll/paperdoll_parse_enqueue.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
             'http://www.wantdresses.com/wp-content/uploads/2015/07/skvdfw-l.jpg',\
             'http://www.wantdresses.com/wp-content/uploads/2015/09/xoxo-prom-dresses-awesome-prom-dresses.jpg',\
             'http://www.wantdresses.com/awesome-prom-dresses/prom-dresses-on-pinterest-orange-prom-dresses-pink-prom-dresses-awesome-prom-dresses',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/fun-prom-dresses-2013-look-awesome-in-ombre-blog-at-the-prom-awesome-prom-dresses.jpg'\,
+            'http://www.wantdresses.com/wp-content/uploads/2015/09/fun-prom-dresses-2013-look-awesome-in-ombre-blog-at-the-prom-awesome-prom-dresses.jpg',\
             'http://www.wantdresses.com/wp-content/uploads/2015/09/dress-jewellery-picture-more-detailed-picture-about-hot-sale-awesome-prom-dresses.jpg',\
             'http://www.wantdresses.com/wp-content/uploads/2015/07/rs_634x926-140402114112-634-8Prom-Dress-ls.4214.jpg',\
             'http://www.wantdresses.com/wp-content/uploads/2015/09/group-of-vsledky-obrzk-google-pro-httpwwwoblectesecz-awesome-prom-dresses.jpg',\
