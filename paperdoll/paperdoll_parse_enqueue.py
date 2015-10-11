026d1017dc8a4242150899220a7bb6b21522c8c4
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-11 17:38:18 +0300

    parallel again

--- paperdoll/paperdoll_parse_enqueue.py
+++ paperdoll/paperdoll_parse_enqueue.py
@@ -71,14 +71,7 @@ def show_max(parsed_img, labels):
 #run a timing test
 if __name__ == "__main__":
     urls = ['http://i.imgur.com/ahFOgkm.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/sexy-plus-sized-prom-dresses-at-peaches-boutique-peaches-awesome-prom-dresses.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/07/skvdfw-l.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/xoxo-prom-dresses-awesome-prom-dresses.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/prom-dresses-on-pinterest-orange-prom-dresses-pink-prom-dresses-awesome-prom-dresses.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/fun-prom-dresses-2013-look-awesome-in-ombre-blog-at-the-prom-awesome-prom-dresses.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/dress-jewellery-picture-more-detailed-picture-about-hot-sale-awesome-prom-dresses.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/07/rs_634x926-140402114112-634-8Prom-Dress-ls.4214.jpg',\
-            'http://www.wantdresses.com/wp-content/uploads/2015/09/group-of-vsledky-obrzk-google-pro-httpwwwoblectesecz-awesome-prom-dresses.jpg',\
+           'http://www.wantdresses.com/wp-content/uploads/2015/09/group-of-vsledky-obrzk-google-pro-httpwwwoblectesecz-awesome-prom-dresses.jpg',\
             'http://www.wantdresses.com/wp-content/uploads/2015/09/gowns-blue-picture-more-detailed-picture-about-awesome-strapless-awesome-prom-dresses.jpg']
     i = 0
     start_time = time.time()
@@ -89,7 +82,7 @@ if __name__ == "__main__":
         print('url #'+str(i)+' '+url)
     #    img, labels, pose = paperdoll_enqueue(url, async = True,queue=queue)
        # n = paperdoll_enqueue(url, async = True,queue=queue)
-        img, labels, pose = paperdoll_enqueue(url, async = False)
+        img, labels, pose = paperdoll_enqueue(url, async = False,use_tg_worker=True)
 #        print('labels:'+str(labels))
 #        print('')
     elapsed_time = time.time() - start_time
