e8cd59b36ea791fa50458e2b387785f7cf3c2aff
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-11 06:40:18 +0300

    @#% indents

--- paperdoll/unit_test_paperdoll_parse_enqueue.py
+++ paperdoll/unit_test_paperdoll_parse_enqueue.py
@@ -28,12 +28,12 @@ class OutcomesTest(unittest.TestCase):
         print('url #'+str(i)+' '+url)
     #    img, labels, pose = paperdoll_enqueue(url, async = True,queue=queue)
          #n = paperdoll_enqueue(url, async = True,queue=queue)
-         img, labels, pose = paperdoll_enqueue_parallel(url, async = False)
-         print('labels:'+str(labels))
-         print('')
-         img, labels, pose = paperdoll_enqueue_parallel(url, async = True)
-         print('labels:'+str(labels))
-         print('')
+        img, labels, pose = paperdoll_enqueue_parallel(url, async = False)
+        print('labels:'+str(labels))
+        print('')
+        img, labels, pose = paperdoll_enqueue_parallel(url, async = True)
+        print('labels:'+str(labels))
+        print('')
 
     #run a timing test
     def test_time(self):
