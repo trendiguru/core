e8ff6169efa7008d7942970295891730528eb4b5
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-11 06:39:01 +0300

    unit tests for paperdoll

--- paperdoll/unit_test_paperdoll_parse_enqueue.py
+++ paperdoll/unit_test_paperdoll_parse_enqueue.py
@@ -26,11 +26,11 @@ class OutcomesTest(unittest.TestCase):
         queue = Queue('paperdoll', connection=redis_conn)
 
         print('url #'+str(i)+' '+url)
-        #    img, labels, pose = paperdoll_enqueue(url, async = True,queue=queue)
-            n = paperdoll_enqueue(url, async = True,queue=queue)
-           # img, labels, pose = paperdoll_enqueue_parallel(url, async = True)
-    #        print('labels:'+str(labels))
-    #        print('')
+    #    img, labels, pose = paperdoll_enqueue(url, async = True,queue=queue)
+         #n = paperdoll_enqueue(url, async = True,queue=queue)
+         img, labels, pose = paperdoll_enqueue_parallel(url, async = False)
+         print('labels:'+str(labels))
+         print('')
 
 
     #run a timing test
