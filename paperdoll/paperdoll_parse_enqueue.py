7648f89425f25e16980a0c52581fc44149147f9b
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-11 07:19:06 +0300

    test all combos

--- paperdoll/paperdoll_parse_enqueue.py
+++ paperdoll/paperdoll_parse_enqueue.py
@@ -44,6 +44,7 @@ def paperdoll_enqueue_parallel(img_url_or_cv2_array,async=True):
             if elapsed_time>constants.paperdoll_ttl:
                 print('timeout waiting for pd.get_parse_mask')
                 return [[],[],[]]
+        return job.result
     #the caller expects three results...
     return [job.result,None,None]
 
