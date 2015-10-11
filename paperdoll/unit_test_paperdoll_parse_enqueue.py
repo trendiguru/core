3eba99e6afec5e2691cf2080d4dc8f9f3282a137
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-11 06:08:31 +0300

    unit tests for paperdoll

---
+++ paperdoll/unit_test_paperdoll_parse_enqueue.py
@@ -0,0 +1,87 @@
+__author__ = 'jeremy'
+
+# TODO
+#add tests for:
+#make sure fingerprint of simlar images are more similar than fps of dvery different images
+#length is correct
+
+import unittest
+import math
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+import Utils
+import fingerprint_core
+import constants
+
+import time
+import numpy as np
+from rq import Queue
+from redis import Redis
+import cv2
+
+from trendi_guru_modules import constants
+
+redis_conn = Redis()
+
+
+class OutcomesTest(unittest.TestCase):
+    # examples of things to return
+    #    def testPass(self):
+    #        return
+
+    #    def testFail(self):
+    #        self.failIf(True)
+
+    #    def testError(self):
+    #        raise RuntimeError('Test error!')
+
+    #run a timing test
+    def test_bad_url():
+        url = 'http://notanimage.jpg'
+        queue = Queue('paperdoll_test', connection=redis_conn)
+
+        for url in urls:
+            i+=1
+            print('url #'+str(i)+' '+url)
+        #    img, labels, pose = paperdoll_enqueue(url, async = True,queue=queue)
+            n = paperdoll_enqueue(url, async = True,queue=queue)
+           # img, labels, pose = paperdoll_enqueue_parallel(url, async = True)
+    #        print('labels:'+str(labels))
+    #        print('')
+        elapsed_time = time.time() - start_time
+        print('tot elapsed:'+str(elapsed_time)+',per image:'+str(float(elapsed_time)/len(urls)))
+
+
+    #run a timing test
+    def test_time():
+        urls = ['http://i.imgur.com/ahFOgkm.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/sexy-plus-sized-prom-dresses-at-peaches-boutique-peaches-awesome-prom-dresses.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/07/skvdfw-l.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/xoxo-prom-dresses-awesome-prom-dresses.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/prom-dresses-on-pinterest-orange-prom-dresses-pink-prom-dresses-awesome-prom-dresses.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/fun-prom-dresses-2013-look-awesome-in-ombre-blog-at-the-prom-awesome-prom-dresses.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/dress-jewellery-picture-more-detailed-picture-about-hot-sale-awesome-prom-dresses.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/07/rs_634x926-140402114112-634-8Prom-Dress-ls.4214.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/group-of-vsledky-obrzk-google-pro-httpwwwoblectesecz-awesome-prom-dresses.jpg',\
+                'http://www.wantdresses.com/wp-content/uploads/2015/09/gowns-blue-picture-more-detailed-picture-about-awesome-strapless-awesome-prom-dresses.jpg']
+        i = 0
+        start_time = time.time()
+        queue = Queue('paperdoll_test', connection=redis_conn)
+
+        for url in urls:
+            i+=1
+            print('url #'+str(i)+' '+url)
+        #    img, labels, pose = paperdoll_enqueue(url, async = True,queue=queue)
+            n = paperdoll_enqueue(url, async = True,queue=queue)
+           # img, labels, pose = paperdoll_enqueue_parallel(url, async = True)
+    #        print('labels:'+str(labels))
+    #        print('')
+        elapsed_time = time.time() - start_time
+        print('tot elapsed:'+str(elapsed_time)+',per image:'+str(float(elapsed_time)/len(urls)))
+
+
+if __name__ == '__main__':
+    unittest.main()
