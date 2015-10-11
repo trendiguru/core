a50484cc37d4fb9276d9a78971145a6ff41d1fbb
nate <ndor123@gmail.com>
2015-10-11 18:43:13 +0300

    moved lab to above scorer

--- scorer/TGscorer.py
+++ scorer/TGscorer.py
@@ -269,7 +269,7 @@ def run_scorer(test_case_image_path,goldenset_classes,goldenset_images,weights_d
     return test_classes_score, test_results_score
 
 
-def lab()
+def lab():
     weights_dictionary = {'vest': 1, 'jeans': 1, 'sweatshirt': 1, 'skirt': 1, 'blouse': 1, 'cardigan': 1, 'shirt': 1, 'dress': 1, 'top': 1, 'suit': 1, 'pants': 1, 'shorts': 1, 't-shirt': 1, 'leggings': 1, 'blazer': 1, 'tights': 1, 'bodysuit': 1, 'jacket': 1, 'coat': 1, 'stockings': 1, 'jumper': 1, 'sweater': 1}
     goldenset_classes = ['cardigan','shirt','skirt','top','suit','jumper','shorts','t-shirt','leggings','blazer','tights','bodysuit']
     goldenset_images = '../images/img.jpg'
