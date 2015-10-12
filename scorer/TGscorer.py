4c9623700cd126bc68caf6015b5f73ebe91e32fe
nate <ndor123@gmail.com>
2015-10-12 16:56:23 +0300

    moved lab to above scorer

--- scorer/TGscorer.py
+++ scorer/TGscorer.py
@@ -82,7 +82,7 @@ def classification_rating(goldenset_classes,testset_classes,weights_dictionary):
     print PWC
     class_rating = float(PC)/NWgolden - float(NC)/NWgolden * float(PWC)/len(testset_classes)
     if class_rating < 0.0:
-        class_rating = 0.0
+        class_rating = 0.0 #
 
     return class_rating
 
