0e612a57e3cd8b0de5ba32d1e471e4367047bef5
Lior Sabag <sabag.lior@gmail.com>
2015-10-14 17:25:06 +0300

    db_nam - > db

--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ min_bb_to_image_area_ratio = 0.95  # if bb takes more than this fraction of imag
 #########
 #DB stuff
 #########
-db_name = pymongo.MongoClient().mydb
+db = pymongo.MongoClient().mydb
 from redis import Redis
 redis_conn = Redis()
 parallel_matlab_queuename = 'pd'
