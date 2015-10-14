88671d36874be18594c69d9be19140f336de1c33
nate <ndor123@gmail.com>
2015-10-14 16:18:01 +0300

    moved lab to above scorer

--- scorer/TGscorer.py
+++ scorer/TGscorer.py
@@ -179,7 +179,7 @@ def classification_rating(goldenset_classes,testset_classes,weights_dictionary):
     elif PWC == 0:
         class_rating = 1.0
     else:
-        class_rating = sigmoid((float(PC)/NWgolden) / (float(PWC)/len(testset_classes))) #- float(NC)/NWgolden
+        class_rating = sigmoid((float(PC)/NWgolden) / (float(PWC)))#/len(testset_classes))) #- float(NC)/NWgolden
         if class_rating < 0.0:
             class_rating = 0.0
 
