5a1bb3c9bd3ff0a0067ec335b479948c56e8c08f
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-20 19:00:57 +0300

    no vis output so i can run on server instead of crashing poor laptop

--- classifier_stuff/ccv_facedetector.py
+++ classifier_stuff/ccv_facedetector.py
@@ -19,7 +19,7 @@ def ccv_facedetect(filename):
     #subprocess.check_output([command,arg1,arg2])
 #    output = subprocess.Popen(fcommand, stdout=subprocess.PIPE).communicate()[0]
     retvals = commands.getstatusoutput(fcommand)
-    print retvals
+#    print retvals
     rects = []
     for rectstr in retvals[1:]:
         newrect = [int(s) for s in rectstr.split() if s.isdigit()]
@@ -27,7 +27,7 @@ def ccv_facedetect(filename):
             rects.append(newrect)
     return rects
 
-def check_lfw():
+def check_lfw(use_visual_output=False):
     BASE_PATH = os.getcwd()
     BASE_PATH2 = os.path.join(BASE_PATH, 'male-female/female')
     print('basepath:' + BASE_PATH2)
@@ -45,14 +45,48 @@ def check_lfw():
                 print('faces:'+str(faces))
                 n_images = n_images + 1
                 if len(faces)>1:
-                    n_extras = n_extra + 1
+                    n_extra = n_extra + 1
                 if len(faces)==1:
                     n_single_detections = n_single_detections + 1
-                show_rects(abs_path,faces)
-                print('n_images:'+str(n_images)+'n_images:'+str(n_images)+'n_detections:'+str(n_single_detections))
-    print('n_images:'+str(n_images))
-    print('n_extras:'+str(n_extra))
-    print('n_detections:'+str(n_single_detections))
+                if use_visual_output:
+                    show_rects(abs_path,faces)
+                print('n_images:'+str(n_images)+' n_extra:'+str(n_extra)+' n_detections:'+str(n_single_detections))
+
+    true_positives = n_single_detections + n_extra
+    true_pos_rate = float(true_positives)/n_images
+    false_neg_rate = float(n_images-true_positives)/n_images
+    print('n_images:'+str(n_images)+' n_extra:'+str(n_extra)+' n_detections:'+str(n_single_detections))
+    print('true pos:'+str(true_positives)+' false_neg:'+str(false_neg_rate))
+
+    BASE_PATH = os.getcwd()
+    BASE_PATH2 = os.path.join(BASE_PATH, 'male-female/male')
+    print('basepath:' + BASE_PATH2)
+    n_images = 0
+    n_extra = 0
+    n_single_detections = 0
+    for dirname, dirnames, filenames in os.walk(BASE_PATH2):
+        dirnames.sort()
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                abs_path = "%s/%s" % (subject_path, filename)
+                print('path:' + abs_path)
+                faces = ccv_facedetect(abs_path)
+                print('faces:'+str(faces))
+                n_images = n_images + 1
+                if len(faces)>1:
+                    n_extra = n_extra + 1
+                if len(faces)==1:
+                    n_single_detections = n_single_detections + 1
+                if use_visual_output:
+                    show_rects(abs_path,faces)
+                print('n_images:'+str(n_images)+' n_extra:'+str(n_extra)+' n_detections:'+str(n_single_detections))
+
+    true_positives = n_single_detections + n_extra
+    true_pos_rate = float(true_positives)/n_images
+    false_neg_rate = float(n_images-true_positives)/n_images
+    print('n_images:'+str(n_images)+' n_extra:'+str(n_extra)+' n_detections:'+str(n_single_detections))
+    print('true pos:'+str(true_positives)+' false_neg:'+str(false_neg_rate))
 
 def show_rects(abs_path,faces):
     img_arr = cv2.imread(abs_path)
