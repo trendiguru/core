86bf5ed5e002c0901b1943d2915aebf976791033
jeremy rutman <jeremy.rutman@gmail.com>
2015-10-21 18:17:04 +0300

    so close to monkeys and llamas

--- classifier_stuff/ccv_facedetector.py
+++ classifier_stuff/ccv_facedetector.py
@@ -11,7 +11,8 @@ import os
 import sys
 import os.path
 import logging
-import Utils
+
+from trendi_guru_modules import Utils
 
 def ccv_facedetect(filename):
  #   Utils.get_cv2_img_array(url_or_path_to_image_file_or_cv2_image_array, download=True,)
