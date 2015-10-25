87832052086c68c5284876ae70f63fc3d80c1741
yonti <yontilevin@gmail.com>
2015-10-25 11:00:45 +0200

    changed the fingerprint related functions

--- dailyDBupdate.py
+++ dailyDBupdate.py
@@ -77,6 +77,15 @@ def email(stats):
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+    }
+    </style>
 </head>
 <body>
 <div class="container">"""
