849cf7c7d687a8e69dcfed0aabfd3cc514315ff2
nate <ndor@bitbucket.org>
2015-10-27 10:15:45 +0200

    minor adjustments...

--- collar_neural_net/collar_CNN.py
+++ collar_neural_net/collar_CNN.py
@@ -60,8 +60,8 @@ Y_train = output_vector
 
 
 
-batch_size = 10000
-epoches_number = 1000
+batch_size = 32
+epoches_number = 100
 overwrite_weights = True
 
 model = Sequential()
@@ -69,21 +69,21 @@ model = Sequential()
 # this applies 32 convolution filters of size 3x3 each.
 model.add(Convolution2D(32, 3, 3, border_mode='full', input_shape=(3, 32, 32)))
 model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3))
-model.add(Activation('relu'))
+# model.add(Convolution2D(32, 3, 3))
+# model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(64, 3, 3, border_mode='valid'))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
+# model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+# model.add(Activation('relu'))
+# model.add(Convolution2D(64, 3, 3))
+# model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
 # Note: Keras does automatic shape inference.
-model.add(Dense(256))
+model.add(Dense(10))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 
