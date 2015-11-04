40284d3ffc7fe4e0664da7c01f1bfa2770156d20
jeremy rutman <jeremy.rutman@gmail.com>
2015-11-04 05:49:26 +0200

    more  caffe_general worker

--- caffe_general.py
+++ caffe_general.py
@@ -6,25 +6,26 @@ import cv2
 import logging
 from . import constants
 import Utils
+import os
 redis_conn = constants.redis_conn
 
 # Tell RQ what Redis connection to use
 
 def caffe_general(url_or_img_array, async=True,filename=None):
     """
-        this is supposed to take an image and give a deep nn result. make sure someone has run
+        this is supposed to take an image and give a caffe nn result. make sure someone has run
         rqworker -u redis://redis1-redis-1-vm:6379 caffe_general
     :param img_url_or_cv2_array: the image/url
     :param async: whether to run async or sync
     :return: dict like {class1:probability1, class2:probability2}
     """
-
+    delete_when_done = False
     img_arr = Utils.get_cv2_img_array(url_or_img_array)
-    filename = filename or rand_string()+'.jpg'
-    if img_arr is not None and cv2.imwrite(filename, img):
+    filename = filename or (rand_string()+'.jpg')
+    if img_arr is not None and cv2.imwrite(filename, img_arr):
         queue_name = 'caffe_general'
         queue = Queue(queue_name, connection=redis_conn)
-        job = queue.enqueue(ask_caffe,img_arr)
+        job = queue.enqueue(ask_caffe,filename)
         print('started caffe job on queue:'+str(queue))
         start = time.time()
         if not async:
@@ -32,16 +33,17 @@ def caffe_general(url_or_img_array, async=True,filename=None):
             while job.result is None:
                 time.sleep(0.5)
                 print('.'),
-                elapsed_time = time.time()-start
-                if elapsed_time>constants.caffe_general_ttl :
+                time_elapsed = time.time()-start
+                if time_elapsed>constants.caffe_general_ttl :
                     print('timeout waiting for caffe_general')
-                    return
+                    return None
             print('')
             if delete_when_done:
                 try:
                     os.remove(filename)
                 except Exception as e:
-                    logging.warning("ccv_facedetect could not delete file {0} because of exception: \n{1}".format(filename, e))
+                    logging.warning("caffe_general could not delete file {0} because of exception: \n{1}".format(filename, e))
+            print(job.result)
             return job.result
         else:
             print('running asynchronously (not waiting for result)')
@@ -49,30 +51,28 @@ def caffe_general(url_or_img_array, async=True,filename=None):
             try:
                 os.remove(filename)
             except Exception as e:
-                logging.warning("ccv_facedetect could not delete file {0} because of exception: \n{1}".format(filename, e))
+                logging.warning("caffe_general could not delete file {0} because of exception: \n{1}".format(filename, e))
         return job
 
-
-
-
-
-    #        ./build/examples/cpp_classification/classification.bin ./models/bvlc_reference_caffenet/deploy.prototxt ./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel ./data/ilsvrc12/imagenet_mean.binaryproto ./data/ilsvrc12/synset_words.txt ./examples/images/cat.jpg
             return
     else:
         raise ValueError("either image is empty or problem writing")
 
 
-
-
-def ask_caffe():
-    caffe_path=constants.caffe_path_in_container
-    bin_path = caffe_path
-    detect_command = "{bin} {prototxt} {caffemodel} {binproto} {words} {img}" \
-        .format(bin=bin_path, filename=filename, path_to_ccvface_db=path_to_ccvface_db)
-
+def ask_caffe(filename):
+    caffe_path = constants.caffe_path_in_container
+    bin_path = os.path.join(caffe_path,'build/examples/cpp_classification/classification.bin'
+    prototxt = os.path.join(caffe_path,'models/bvlc_reference_caffenet/deploy.prototxt')
+    caffemodel = os.path.join(caffe_path,'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel')
+    binproto = os.path.join(caffe_path,'/data/ilsvrc12/imagenet_mean.binaryproto')
+    words = os.path.join(caffe_path,'/data/ilsvrc12/synset_words.txt')
+#./build/examples/cpp_classification/classification.bin ./models/bvlc_reference_caffenet/deploy.prototxt ./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel ./data/ilsvrc12/imagenet_mean.binaryproto ./data/ilsvrc12/synset_words.txt ./examples/images/cat.jpg
+    detect_command = "{bin_path} {prototxt} {caffemodel} {binproto} {words} {img}" \
+        .format(bin_path=bin_path, prototxt=prototxt, caffemodel=caffemodel, binproto=binproto, words=words, img=filename)
+    print('trying command:'+str(detect_command))
     retvals = commands.getstatusoutput(detect_command)
     logging.debug('return from command ' + detect_command + ':' + str(retvals), end="\n")
-
+    return retvals
 
 #run a parallelization test
 if __name__ == "__main__":
