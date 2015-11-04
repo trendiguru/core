7cba141fe4dab482ded108eee8046e5bf9159442
yonti <yontilevin@gmail.com>
2015-11-04 16:26:47 +0200

    changed the fingerprint related functions

--- getShopStyleDB.py
+++ getShopStyleDB.py
@@ -76,7 +76,7 @@ class ShopStyleDownloader():
         cats_to_dl = [anc["id"] for anc in ancestors]
         for cat in cats_to_dl:
             self.download_category(cat, collection)
-        self.wait_for(60, collection)
+        self.wait_for(90, collection)
         self.db.download_data.find_one_and_update({"criteria": collection},
                                                   {'$set': {"end_time": datetime.datetime.now()}})
         tmp = self.db.download_data.find({"criteria": collection})[0]
