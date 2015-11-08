d7adef6374b2c53ef2b94b3155e57359f002e0aa
yonti <yontilevin@gmail.com>
2015-11-08 19:05:57 +0200

    changed the fingerprint related functions

---
+++ caffeDocker.py
@@ -0,0 +1,29 @@
+__author__ = 'yonatan'
+
+import time
+
+import constants
+
+db = constants.db
+relevantCaffeLabels = constants.caffeRelevantLabels
+
+
+def isImgRelevant(url):
+    '''
+
+    :param url: in string format!!!
+    :return: True if relevant / False o.w
+    '''
+    db.caffeQ.insert_one({"url": url})
+
+    while (db.caffeResults.find({"url": url}).count() == 0):
+        time.sleep(1)
+
+    results = db.caffeResults.find_one({"url": url})
+
+    catID = results["results"]
+    intersection = [i for i in catID if i in relevantCaffeLabels]
+    if intersection is None:
+        return False
+
+    return True
