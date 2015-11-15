e7a1b6c545309351d96f273592e43c870cd28366
yonti <yontilevin@gmail.com>
2015-11-15 16:11:47 +0200

    changed the fingerprint related functions

--- caffeDocker_test.py
+++ caffeDocker_test.py
@@ -2,8 +2,7 @@ __author__ = 'yonatan'
 
 import time
 
-import skimage
-import numpy as np
+import cv2
 
 import constants
 
@@ -22,7 +21,8 @@ def is_person_in_img(method, src):
     if method == "url":
         db.caffeQ.insert_one({"method": method, "src": src})
     else:
-        src = load_image(src)
+        src = cv2.imread(src)
+        src = float(src)
         src = src.tolist()
         db.caffeQ.insert_one({"method": method, "src": src})
     while db.caffeResults.find({"src": src}).count() == 0:
@@ -37,29 +37,3 @@ def is_person_in_img(method, src):
         return False
     return True
 
-
-def load_image(filename, color=True):
-    """
-    Load an image converting from grayscale or alpha as needed.
-
-    Parameters
-    ----------
-    filename : string
-    color : boolean
-        flag for color format. True (default) loads as RGB while False
-        loads as intensity (if image is already grayscale).
-
-    Returns
-    -------
-    image : an image with type np.float32 in range [0, 1]
-        of size (H x W x 3) in RGB or
-        of size (H x W x 1) in grayscale.
-    """
-    img = skimage.img_as_float(skimage.io.imread(filename)).astype(np.float32)
-    if img.ndim == 2:
-        img = img[:, :, np.newaxis]
-        if color:
-            img = np.tile(img, (1, 1, 3))
-    elif img.shape[2] == 4:
-        img = img[:, :, :3]
-    return img
