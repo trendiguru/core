f77d4b73d9a278ac4c16ee4f8058645e45e7fd06
jr <jeremy@trendiguru.com>
2015-11-16 21:04:17 +0200

    parse_enqueue not in git for some reason

--- paperdoll/kwiktest.py
+++ paperdoll/kwiktest.py
@@ -1,6 +1,18 @@
 __author__ = 'jeremy'
-import paperdoll_parse_enqueue
-
-url = 'http://i.imgur.com/ahFOgkm.jpg'
-retval = paperdoll_parse_enqueue.paperdoll_enqueue(url, async=False)
-print('retval:' + str(retval.result))
+from trendi import paperdoll_parse_enqueue
+import time
+import numpy as np
+urls=[]
+dt=[]
+urls.append('http://i.imgur.com/ahFOgkm.jpg')
+urls.append('https://img1.etsystatic.com/019/1/5682424/il_570xN.555916317_ebv0.jpg')
+for url in urls
+    start_time = time.time()
+    retval = paperdoll_parse_enqueue.paperdoll_enqueue(url, async=False)
+    end_time = time.time()
+    dt=end_time-start_time
+    dts.append(dt)
+    print('retval:' + str(retval.result)+' time:'+str(dt))
+means=np.mean(dt)
+std=np.std(dt)
+print('mean:' + str(means)+' std:'+str(std))
