f2528e368a66a6cf9b67dd70252076d893254de8
yonti <yontilevin@gmail.com>
2015-11-16 12:31:19 +0200

    changed the fingerprint related functions

--- caffeDocker_test.py
+++ caffeDocker_test.py
@@ -11,21 +11,26 @@ db = constants.db
 relevant_caffe_labels = constants.caffeRelevantLabels
 
 
-def is_person_in_img(method, src):
+def is_person_in_img(method, path, k):
     '''
 
     :param type: what is the input type -path or url
             src: the exctual path/urlin string format!!!
+            k: numner of results
     :return: True if relevant / False o.w
     '''
     tic = time.time()
     if method == "url":
-        db.caffeQ.insert_one({"method": method, "src": src})
-    else:
-        src = np.array(cv2.imread(src))
+        src = path
+    elif method == "img":
+        src = np.array(cv2.imread(path))
         src = src.astype(float) / 255
         src = src.tolist()
-        db.caffeQ.insert_one({"method": method, "src": src})
+    else:
+        print "bad input"
+        return ("bad input", [])
+
+    db.caffeQ.insert_one({"method": method, "src": src, "k": k})
     while db.caffeResults.find({"src": src}).count() == 0:
         time.sleep(0.25)
     toc = time.time()
@@ -34,7 +39,10 @@ def is_person_in_img(method, src):
     catID = results["results"]
     intersection = [i for i in catID if i in relevant_caffe_labels]
     db.caffeResults.delete_one({"src": src})
-    if len(intersection) == 0:
-        return False
-    return True
 
+    if len(intersection) == 0:
+        retTuple = (False, catID)
+    else:
+        retTuple = (True, catID)
+
+    return retTuple
