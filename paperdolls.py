a332c3a892deac2e30aae63732948476527f619b
Nadav <nadav@trendiguru.com>
2015-11-18 12:07:38 +0200

    always use caffe is_person

--- paperdolls.py
+++ paperdolls.py
@@ -342,7 +342,7 @@ def get_results_now(page_url, image_url, collection='products_jp'):
 
     # NEW_IMAGE !!
     clean_image = copy.copy(image)
-    relevance = background_removal.image_is_relevant(image, page_url, image_url)
+    relevance = background_removal.image_is_relevant(image, True, image_url)
     image_dict = {'image_urls': [image_url], 'relevant': relevance.is_relevant,
                   'image_hash': image_hash, 'page_urls': [page_url], 'people': []}
     if relevance.is_relevant:
