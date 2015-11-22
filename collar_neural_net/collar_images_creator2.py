e95009210099fe5d905c6e765a1f1024bd6c31c2
nate <nate@trendiguru.com>
2015-11-22 15:02:00 +0200

    optimized net

--- collar_neural_net/collar_images_creator2.py
+++ collar_neural_net/collar_images_creator2.py
@@ -9,8 +9,8 @@ face_cascade = cv2.CascadeClassifier('/home/nate/Desktop/core/classifier_stuff/c
 
 image_file_types = ['.jpg','.png','.bmp','.gif']
 a = 1.25 # scalar for increasing collar box in relation to face box (1==100%)
-max_angle = 20 # tilt angle of the image for diversification
-angle_offset = 10 # tilt angle of the image for diversification
+max_angle = 15 # tilt angle of the image for diversification
+angle_offset = 7 # tilt angle of the image for diversification
 # max_offset = 0.01 # maximum horizontal movement (% (out of box X) of the collar box for diversification
 # delta_offset = max_offset # horizontal movement increments(%)
 output_images_size = (32, 32) # pixels^2
@@ -91,8 +91,8 @@ for image_file_name in only_image_files:
         # cv2.waitKey(500)
         # ###########################################################
         # writing a false, i.e. [0, 0, 0]:
-        image_of_collar = image_of_rotated_collar[(offsetted_face[1]+2.1*offsetted_face[3])*(1-a):
-                            (offsetted_face[1]+3.1*offsetted_face[3])*(1+a),
+        image_of_collar = image_of_rotated_collar[(offsetted_face[1]+offsetted_face[3])*(1-a)+offsetted_face[3]:
+                            (offsetted_face[1]+2.1*offsetted_face[3])*(1+a)+offsetted_face[3],
                             (offsetted_face[0])*(1-a):(offsetted_face[0]+offsetted_face[2])*(1+a)]
         resized_image_of_collar = cv2.resize(image_of_collar, output_images_size)
         image_call = str(image_number) + '_0'
@@ -104,9 +104,9 @@ for image_file_name in only_image_files:
         image_number += 1
         rotated_image_matrix = cv2.getRotationMatrix2D(flipped_collar_image_center_point, angle, 1.0)
         image_of_rotated_collar = cv2.warpAffine(image, rotated_image_matrix,(row, col))
-        image_of_collar = image_of_rotated_collar[(offsetted_face[1]+2.1*offsetted_face[3])*(1-a):
-                            (offsetted_face[1]+3.1*offsetted_face[3])*(1+a),
-                            col-((offsetted_face[0]+offsetted_face[2])*(1+a)):col-((offsetted_face[0])*(1-a))]
+        image_of_collar = image_of_rotated_collar[(offsetted_face[1]+offsetted_face[3])*(1-a):
+                            (offsetted_face[1]+2.1*offsetted_face[3])*(1+a),
+                            (offsetted_face[0])*(1-a):(offsetted_face[0]+offsetted_face[2])*(1+a)]
         resized_image_of_collar = cv2.resize(image_of_collar, output_images_size)
         image_call = str(image_number)
         ###########################################################
@@ -137,8 +137,8 @@ for image_file_name in only_image_files:
         # cv2.waitKey(500)
         # ###########################################################
         # writing a false, i.e. [0, 0, 0]:
-        image_of_collar = image_of_rotated_collar[(offsetted_face[1]+offsetted_face[3])*(1-a):
-                            (offsetted_face[1]+2.1*offsetted_face[3])*(1+a),
+        image_of_collar = image_of_rotated_collar[(offsetted_face[1]+offsetted_face[3])*(1-a)+offsetted_face[3]:
+                            (offsetted_face[1]+2.1*offsetted_face[3])*(1+a)+offsetted_face[3],
                             (offsetted_face[0])*(1-a):(offsetted_face[0]+offsetted_face[2])*(1+a)]
         resized_image_of_collar = cv2.resize(image_of_collar, output_images_size)
         image_call = str(image_number) + '_0'
