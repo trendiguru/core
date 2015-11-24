53e843771c58b8226133e12401a3a2f40e0afd30
jr <jeremy@trendiguru.com>
2015-11-24 13:14:05 +0200

    fashionista->pixel annotated bitmaps

--- paperdoll/fashionista_reader.m
+++ paperdoll/fashionista_reader.m
@@ -1,7 +1,7 @@
 %this is a matlab file that takes the unzipped fashionista dataset of 685 pixel-level-annotated images and saves as bitmaps with
 %a consistent labelling (shown in 'categories' below)
 
-categories=    {
+pd_categories=    {
     'null',
     'tights',
     'shorts',
@@ -58,6 +58,17 @@ categories=    {
     'loafers',
     'hair',
     'skin'}
+
+%to translate these to shopstyle categories you can use constants.paperdoll_shopstyle_women
+%where the key is pd label and value is shopstyle equivalent
+%reproduced below for your reading pleasure
+%paperdoll_shopstyle_women = {'top': 'womens-tops', 'pants': 'womens-pants', 'shorts': 'shorts', 'jeans': 'jeans',
+%                             'jacket': 'jackets', 'blazer': 'blazers', 'shirt': 'womens-tops', 'skirt': 'skirts',
+%                             'blouse': 'womens-tops', 'dress': 'dresses', 'sweater': 'sweaters',
+%                             't-shirt': 'tees-and-tshirts', 'cardigan': 'cardigan-sweaters', 'coat': 'coats',
+%                             'suit': 'womens-suits', 'vest': 'vests', 'sweatshirt': 'sweatshirts',
+%                             'jumper': 'v-neck-sweaters', 'bodysuit': 'shapewear', 'leggings': 'leggings',
+%                             'stockings': 'hosiery', 'tights': 'leggings'}
 
 for k=1:685
     pd_pred=predictions_paperdoll(k);
