7455d922b55cc15a58778a7fe84a8301d7660a53
yonti <yontilevin@gmail.com>
2015-11-25 14:45:41 +0200

    auto download

---
+++ autoDownloader.py
@@ -0,0 +1,25 @@
+__author__ = 'yonatan'
+
+from datetime import datetime
+
+from rq_scheduler import Scheduler
+
+from constants import redis_conn
+
+scheduler = Scheduler(connection=redis_conn)
+
+# remove old jobs
+jobs = scheduler.get_jobs()
+print jobs
+for job in jobs:
+    scheduler.cancel(job)
+
+# set new jobs
+scheduler.schedule(scheduled_time=datetime.now(),
+                   func="developer.test",
+                   repeat=5,
+                   interval=60)
+
+print ("new job list:")
+jobs = scheduler.get_jobs()
+print jobs
