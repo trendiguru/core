84860ec6386a0d8406f6aea2105e77a819ad50ef
jr <jeremy@trendiguru.com>
2015-11-29 18:12:37 +0200

    word frequencies

--- downloaders/db_description_histogram.py
+++ downloaders/db_description_histogram.py
@@ -84,7 +84,8 @@ def collect_description(search_string='pants',category_id='dresses'):
                 words = s.encode('utf-8')
                 # print(unicode(s.strip(codecs.BOM_UTF8), 'utf-8'))
                 # print(unicode(s.strip(codecs.BOM_UTF8), 'utf-8'))
-        for word in words:
+        individual_words = words.split():
+        for word in individual_words:
             if word in word_frequencies:
                 word_frequencies[word] += 1
             else:
