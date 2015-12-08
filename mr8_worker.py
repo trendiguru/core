6f719972c773e9582fa15772ea02055b9834fd62
yuli <yuliha888@gmail.com>
2015-12-08 15:21:40 +0000

    update mr8_worker.py, fp_yuli_MR8.py

--- mr8_worker.py
+++ mr8_worker.py
@@ -7,6 +7,7 @@ import logging
 import numpy as np
 import cv2
 
+import paperdoll.paperdoll_parse_enqueue
 import background_removal
 import Utils
 import constants
@@ -20,30 +21,11 @@ def add_new_field(doc, x):
     image_url = doc["images"]["XLarge"]
 
     image = Utils.get_cv2_img_array(image_url)
+    print "image high, width:", image.shape[0], image.shape[1]
     if not Utils.is_valid_image(image):
         logging.warning("image is None. url: {url}".format(url=image_url))
         return
-    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = background_removal.image_is_relevant(image)
-    print faces
-    if faces[0] is False:
-        return
-    fc = faces[1]
-    print fc
-    if len(fc) == 0:
-        return
-    x0, y0, w, h = fc[0]
 
-    s_size = np.amin(np.asarray(w, h))
-
-    sample = gray_img[y0 + 3 * s_size:y0 + 4 * s_size, x0:x0 + s_size]
-    print sample.shape
-    d = 40
-    if s_size < d:
-        return
-    resized_sample = cv2.resize(sample, (d, d))
-    response = fp_yuli_MR8.yuli_fp(resized_sample, d / 2)
-    print len(response)
 
     ms_response = []
     for idx, val in enumerate(response):
@@ -59,23 +41,30 @@ def add_new_field(doc, x):
     # return x
 
 
-def mr8_4_demo(img, fc):
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print fc
-    if len(fc) == 0:
-        return
-    x0, y0, w, h = fc
+def mr8_4_demo(img, fc = None):
 
-    s_size = min(w, h)
+    mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(img, async=False).result[:3]
+    final_mask = paperdolls.after_pd_conclusions(mask, labels, person['face'])
+    for num in np.unique(final_mask):
+	category = list(labels.keys())[list(labels.values()).index(num)]
+	if category in constants.paperdoll_shopstyle_women.keys():
+		item_mask = 255 * np.array(final_mask == num, dtype=np.uint8)
 
-    sample = gray_img[y0 + 3 * s_size:y0 + 4 * s_size, x0:x0 + s_size]
-    print sample.shape
-    d = 40
-    if s_size < d:
-        return
-    resized_sample = cv2.resize(sample, (d, d))
-    response = fp_yuli_MR8.yuli_fp(resized_sample, d / 2)
+    faces = background_removal.image_is_relevant(image)
+
+    if faces[0] is True and len(faces[1]) != 0:
+        x0, y0, w, h = faces[1][0]
+        s_size = np.amin(np.asarray(w, h))
+        print "s_size:", s_size
+	#sample = gray_img[y0 + 3 * s_size:y0 + 4 * s_size, x0:x0 + s_size]
+    else:
+        s_size = np.asarray(0.1*image.shape[0])
+        print "s_size:", s_size
+         
+    trimmed_mask = fp_yuli_MR8.trim_mask(image, mask) 
+    response = fp_yuli_MR8.yuli_fp(trimmed_mask, s_size)
     print len(response)
+
 
     ms_response = []
     for idx, val in enumerate(response):
