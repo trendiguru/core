59b779d933d48f7ac3e7803a1207ebb9fd0e220b
yonti <yontilevin@gmail.com>
2015-12-09 15:37:50 +0200

    flipkart

--- all_in_one.py
+++ all_in_one.py
@@ -91,7 +91,7 @@ def distance_function(entry, target_dict, fp_weights, hist_length, wing, weight)
         entry_mr8 = entry["mr8"]
         target_mr8 = target_dict["mr8"]
         mr8_distance = NNSearch.distance_1_k(entry_mr8, target_mr8)
-        w0 = abs(1 - float(weight))
+        w0 = abs(1 - weight)
         return w0 * bhat + weight * mr8_distance
 
 def find_n_nearest_neighbors(target_dict, entries, number_of_matches, fp_weights, hist_length, wing, weight):
