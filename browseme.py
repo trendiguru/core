771af10007e097ef9dcf2da05b61b0bd44bd3869
yonti <yontilevin@gmail.com>
2015-12-16 19:27:25 +0200

    flipkart

---
+++ browseme.py
@@ -0,0 +1,19 @@
+__author__ = 'yonatan'
+import time
+
+from selenium import webdriver
+
+
+def runExt(url):
+    # enable browser logging
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1120, 1050)
+    driver.get(url)
+    driver.execute_script(open("/var/www/latest/b_main.js").read())
+    time.sleep(1)
+    for x in range(10):
+        script = "scroll(" + str(x * 500) + "," + str(x * 500 + 500) + ")"
+        driver.execute_script(script)
+        time.sleep(0.5)
+
+    driver.quit()
