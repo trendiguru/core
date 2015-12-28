93840d3979fd0475b30f4c5256e39e4c0851176c
jr <jeremy@trendiguru.com>
2015-12-28 13:10:07 +0200

    rq imports

--- constants.py
+++ constants.py
@@ -22,7 +22,7 @@ min_bb_to_image_area_ratio = 0.95  # if bb takes more than this fraction of imag
 ##############
 
 pd_worker_command =  'cd /home/jeremy/paperdoll3/paperdoll-v1.0/ && /usr/bin/python /usr/local/bin/rqworker -w rq.tgworker.TgWorker -u redis://redis1-redis-1-vm:6379 pd &'
-pd_worker_command_braini1 =  'cd /home/pd_user/paperdoll  && /usr/bin/python /usr/local/bin/rqworker  -w trendi.matlab_wrapper.tgworker.TgWorker -u redis://redis1-redis-1-vm:6379 pd &'
+pd_worker_command_braini1 =  'cd /home/pd_user/paperdoll  && /usr/bin/python /usr/local/bin/rqworker  -w trendi.matlab_wrapper.tgworker.TgWorker  pd &'
 
 N_expected_pd_workers_per_server = 15
 N_expected_pd_workers_per_server_braini1 = 41
