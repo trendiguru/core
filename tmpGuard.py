0b8e7c3df52963f0792cbf9e598db9898187e6c1
yonti <yontilevin@gmail.com>
2016-01-01 17:51:19 +0200

    tmpG

--- tmpGuard.py
+++ tmpGuard.py
@@ -16,45 +16,45 @@ from termcolor import colored
 
 def main():
     current = (time.ctime(time.time())).split(" ")
-    print current
     current_date = current[-3]
     current_time = current[-2].split(":")
     current_hour = current_time[0]
     current_min = current_time[1]
+    print colored("\ntmpGuard : %s\n" % current[-2], "red", "on_yellow")
     files2erase = []
-    for file in os.listdir("/tmp"):
-        (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat("/tmp/" + file)
-        if file[:3] == "tmp":
+    for f in os.listdir("/tmp"):
+        (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat("/tmp/" + f)
+        if f[:3] == "tmp":
             modified = time.ctime(mtime)
-            print (file + "last modified: %s" % modified)
+            print (f + "last modified: %s" % modified)
             modified_list = modified.split(" ")
             modified_date = modified_list[-3]
             modified_time = modified_list[-2].split(":")
             modified_hour = modified_time[0]
             modified_min = modified_time[1]
-            min_diff = int(modified_min) - int(current_min)
-            hour_diff = int(modified_hour) - int(current_hour)
-            date_diff = int(modified_date) - int(current_date)
+            min_diff = int(current_min) - int(modified_min)
+            hour_diff = int(current_hour) - int(modified_hour)
+            date_diff = int(current_date) - int(modified_date)
             if date_diff == 0:
                 if hour_diff == 0:
                     if min_diff > 10:
-                        files2erase.append(file)
+                        files2erase.append(f)
                 elif hour_diff == 1:
                     if min_diff + 60 > 10:
-                        files2erase.append(file)
+                        files2erase.append(f)
                 else:
-                    files2erase.append(file)
+                    files2erase.append(f)
             elif date_diff == 1:
                 if hour_diff + 24 == 1:
                     if min_diff + 60 > 10:
-                        files2erase.append(file)
+                        files2erase.append(f)
                 else:
-                    files2erase.append(file)
+                    files2erase.append(f)
             else:
-                files2erase.append(file)
+                files2erase.append(f)
     count = 0
-    for file in files2erase:
-        ret = subprocess.call(["sudo rm -r /tmp/" + file], shell=True)
+    for f in files2erase:
+        ret = subprocess.call(["sudo rm -r /tmp/" + f], shell=True)
         if not ret:
             print colored("removed X succeeded", "yellow")
             count += 1
@@ -65,4 +65,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        time.sleep(600)
