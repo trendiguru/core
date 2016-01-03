6f02e18717b5ef05feb5b9f395b9c4d857c61353
yonti <yontilevin@gmail.com>
2016-01-03 17:59:25 +0200

    specio

--- fingerprint_stuff/fp_tests/nate_fp_testing.py
+++ fingerprint_stuff/fp_tests/nate_fp_testing.py
@@ -4,8 +4,6 @@ __author__ = 'yonatan'
 same as all the other testers
 '''
 
-import time
-
 from rq import Queue
 
 from nate_worker import add_new_field
@@ -30,18 +28,18 @@ def create_new_collection():
     for x, doc in enumerate(category_stack):
         # if x < 5000:
         #     continue
-        job = q.enqueue_call(func=add_new_field, args=(doc, x), ttl=1000, result_ttl=1000, timeout=1000)
-        jobs.append(job)
-
-    current = db.nate_testing.count()
-    time.sleep(1)
-    future = db.nate_testing.count()
-    while current < future:
-        time.sleep(60)
-        current = future
-        future = db.nate_testing.count()
-        print future
-
+        # job = q.enqueue_call(func=add_new_field, args=(doc, x), ttl=1000, result_ttl=1000, timeout=1000)
+        # jobs.append(job)
+        add_new_field(doc, x)
+    # current = db.nate_testing.count()
+    # time.sleep(1)
+    # future = db.nate_testing.count()
+    # while current < future:
+    #     time.sleep(60)
+    #     current = future
+    #     future = db.nate_testing.count()
+    #     print future
+    print ("finished")
 
 if __name__ == "__main__":
     create_new_collection()
