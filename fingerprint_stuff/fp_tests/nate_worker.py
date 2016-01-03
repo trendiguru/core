bd221cdbc886f7df57ae94f267f56ff77869401f
yonti <yontilevin@gmail.com>
2016-01-03 18:14:28 +0200

    specio

--- fingerprint_stuff/fp_tests/nate_worker.py
+++ fingerprint_stuff/fp_tests/nate_worker.py
@@ -68,7 +68,7 @@ def add_new_field(doc, x):
     item_mask = mask
     try:
         specio = new_finger_print.spaciogram_finger_print(image, item_mask)
-        doc["specio"] = specio
+        doc["specio"] = specio.tolist()
     except:
         print("specio specio specio scpecio failed")
         return
