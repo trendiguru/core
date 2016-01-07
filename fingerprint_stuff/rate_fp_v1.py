4af04297831e36644853cf176fd8eb0495bc2c32
Your Name <you@example.com>
2016-01-07 16:17:31 +0200

    update rate_fp_v1

--- fingerprint_stuff/rate_fp_v1.py
+++ fingerprint_stuff/rate_fp_v1.py
@@ -28,9 +28,9 @@ import sys
 import matplotlib
 from scipy.spatial import distance as dist
 from trendi import constants
-from trendi.constants import db
+#from trendi.constants import db
 import pymongo
-#db = pymongo.MongoClient().mydb
+db = pymongo.MongoClient().mydb
 
 
 matplotlib.use('Agg')  # prevents problems generating plots on server where no display is defined
