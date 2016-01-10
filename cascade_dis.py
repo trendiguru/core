f364fbca2a64f4f94c8cb24fae781a1f294642d0
yonti <yontilevin@gmail.com>
2016-01-10 20:08:18 +0200

    spec

--- cascade_dis.py
+++ cascade_dis.py
@@ -6,7 +6,12 @@ from new_finger_print import spaciograms_distance_rating
 
 def distance_function_nate(entry, target_dict, rank):
     # a = time.time()
-    dist = spaciograms_distance_rating(entry["sp_one"], target_dict["sp_one"], rank)
+    if rank == 1:
+        dist = spaciograms_distance_rating(entry["sp_one"], target_dict["sp_one"], rank)
+    elif rank == 2:
+        dist = spaciograms_distance_rating(entry["sp_two"], target_dict["sp_two"], rank)
+    elif rank == 3:
+        dist = spaciograms_distance_rating(entry["specio"], target_dict["specio"], rank)
     # b = time.time()
     # print ("specio time = %s" % str(b - a))
 
@@ -30,8 +35,7 @@ def stage_one(target_dict, entries, rank, stopme):
                 nearest_n.sort(key=lambda tup: tup[1])
                 # last item in the list (index -1, go python!)
                 farthest_nearest = nearest_n[-1][1]
-            if i == 2 * stopme:
-                break
+
             # Loop through remaining entries, if one of them is better, insert it in the correct location and remove last item
             d = distance_function_nate(entry, target_dict, rank)
 
@@ -48,4 +52,5 @@ def stage_one(target_dict, entries, rank, stopme):
     end_time = time.time()
     total_time = end_time - start_time
     print ("total time = %s" % (str(total_time)))
-    return nearest_n
+    id_list = [item['id'] for item in nearest_n]
+    return id_list, nearest_n
