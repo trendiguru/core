9a1f80ca36d3edf4956f19916cd4de641b536044
yonti <yontilevin@gmail.com>
2016-01-10 13:08:36 +0200

    scrap

--- Yonti/screen.py
+++ Yonti/screen.py
@@ -1,1 +1,17 @@
 __author__ = 'yonatan'
+
+import subprocess
+from time import sleep
+
+from termcolor import colored
+
+
+def master():
+    print colored("starting master...", "green", attrs=["bold"])
+    subprocess.call(["screen -S test1"], shell=True)
+    sleep(2)
+    print colored("closing master...", "green", attrs=["bold"])
+
+
+if __name__ == "__main__":
+    master()
