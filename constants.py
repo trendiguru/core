83b3ee7799c3843c715e868298afacf00a5757e0
jr <jeremy@trendiguru.com>
2016-01-12 16:42:21 +0200

    comma got in there somehow

--- constants.py
+++ constants.py
@@ -25,7 +25,7 @@ minimum_im_height = 50
 ##############
 
 pd_worker_command =  'cd /home/jeremy/paperdoll3/paperdoll-v1.0/ && /usr/bin/python /usr/local/bin/rqworker -w trendi.matlab_wrapper.tgworker.TgWorker -u redis://redis1-redis-1-vm:6379 pd &'
-pd_worker_command_braini1 =  'cd /home/pd_user/paperdoll  && /usr/bin/python /usr/local/bin/rqworker  -w trendi.matlab_wrapper.tgworker.TgWorker  pd &',
+pd_worker_command_braini1 =  'cd /home/pd_user/paperdoll  && /usr/bin/python /usr/local/bin/rqworker  -w trendi.matlab_wrapper.tgworker.TgWorker  pd &'
 string_to_look_for_in_pd_command = 'tgworker'
 
 N_expected_pd_workers_per_server = 15
