89dc00fa1416667ceb1ead5c97e7d98f9c579810
jr <jeremy@trendiguru.com>
2016-01-12 17:28:49 +0200

    threshold changes

--- utils/rq_info.py
+++ utils/rq_info.py
@@ -3,17 +3,16 @@ from redis import StrictRedis
 from rq import push_connection, get_failed_queue, Queue
 from rq.job import Job
 
-from trendi import constants
+from . import constants
 
 con = constants.redis_conn
-#con = StrictRedis()
-push_connection(con)
+
 
 def failed_info():
-    fq = get_failed_queue()
+    fq = Queue('failed', con)
     count = fq.count
     print('count:'+str(count))
-#    fq.requeue(job.id)
+
 
 if __name__ == "__main__":
     failed_info()
