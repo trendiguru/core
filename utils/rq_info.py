6b21b4275733c6f24e003f8652848f619a4fd67c
jr <jeremy@trendiguru.com>
2016-01-12 16:25:16 +0200

    rqinfo

--- utils/rq_info.py
+++ utils/rq_info.py
@@ -1,7 +1,23 @@
 
-import subprocess
+from redis import StrictRedis
+from rq import push_connection, get_failed_queue, Queue
+from rq.job import Job
 
-    def info():
-        command = 'rqinfo -u redis://redis1-redis-1-vm:6379''
-        retval = subprocess.call(command, shell=True)  # create the svg
-        print retval
+
+con = StrictRedis()
+push_connection(con)
+
+def div_by_zero(x):
+    return x / 0
+
+job = Job.create(func=div_by_zero, args=(1, 2, 3))
+job.origin = 'fake'
+job.save()
+fq = get_failed_queue()
+fq.quarantine(job, Exception('Some fake error'))
+assert(fq.count == 1)
+
+fq.requeue(job.id)
+
+assert(fq.count == 0)
+assert(Queue('fake').count == 1)
