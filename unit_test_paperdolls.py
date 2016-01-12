1c991a03634a38863270d2009752519a84be9954
jr <jeremy@trendiguru.com>
2016-01-12 14:41:37 +0200

    alphabetized

---
+++ unit_test_paperdolls.py
@@ -0,0 +1,57 @@
+
+
+class OutcomesTest(unittest.TestCase):
+    # examples of things to return
+    # def testPass(self):
+    # return
+    # def testFail(self):
+    # self.failIf(True)
+    # def testError(self):
+    # raise RuntimeError('Test error!')
+
+
+    def setUp(self):
+
+        pass
+
+
+    def test_blacklisted_term_in_url():
+        oklist=['nastygal.com','cnn.com',
+            'http://www.booking.com/
+            'http://ok.ru',            'http://www.youjizz.com',
+            'http://www.msn.com',
+            'http://www.netflix.com',
+            'http://vk.com',
+            'http://www.dasding.de',
+            'http://www.zara.com',
+            'http://www.alohatube.com',
+            'https://www.instagram.com',
+            'http://www.jabong.com',
+            'http://www.tagged.com',
+            'http://www.jollychic.com',
+            'http://www.aliexpress.com',
+            'http://www.sex.com',
+            'http://www.flipkart.com',
+            'http://www.wildfashion.ro',
+            'http://www.posthaus.com.br',
+            'http://www.stellaswardrobe.com',
+            'http://fallout4.2game.info',
+            'http://www.asos.com',
+            'http://www.gala.de',
+            'http://www.jizzhut.com',
+            'http://shop.mango.com',
+            'https://www.google.co.il',
+            'http://www.refinery29.com',
+            'https://vk.com',
+            'http://www.fashiondays.ro',
+            'http://www.voonik.com',
+            'http://lookbook.nu',
+            'http://beeg.com',
+            'http://www.freepik.com',
+            'http://www.3suisses.fr',
+            'http://www.clubmonaco.com',
+            'http://www.amazon.com',
+            'http://jerkoffer.com',
+            'http://globoesporte.globo.com/']
+
+
