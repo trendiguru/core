b6a177b69074070ba905156b65a8ef1a7b9d0b24
jr <jeremy@trendiguru.com>
2016-01-13 11:52:48 +0200

    connection-redis_conn

--- utils/rq_info.py
+++ utils/rq_info.py
@@ -28,7 +28,9 @@ def failed_info():
         lines = exception_info.split('\n')
         enqueue_time = dict['enqueued_at']
         end_time = dict['ended_at']
-        func = dict['func_name' ]
+        func = ''
+        if 'func_name'] in dict:
+           func = dict['func_name']
         args = dict['args']
         id = dict['id']
         reason = exception_info[-2]
