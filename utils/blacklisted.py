e60859fd46672cb19d1cf160c42c3630c2fc3ae5
jr <jeremy@trendiguru.com>
2016-01-13 13:40:56 +0200

    reasons

--- utils/blacklisted.py
+++ utils/blacklisted.py
@@ -2,8 +2,8 @@
 from rq import Queue
 from trendi import constants
 
-con = constants.redis_conn
-bl = Queue('blacklisted_urls', connection = con)
+db = constants.db
+bl = db.blacklisted_urls
 dicts = bl.find()
 for dict in dicts:
     url =  dict['page_url']
