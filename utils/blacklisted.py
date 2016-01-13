211835a41c2a41d906595f3f49bfc3c21df8eef2
jr <jeremy@trendiguru.com>
2016-01-13 13:42:41 +0200

    reasons

--- utils/blacklisted.py
+++ utils/blacklisted.py
@@ -5,6 +5,8 @@ from trendi import constants
 db = constants.db
 bl = db.blacklisted_urls
 dicts = bl.find()
+print('count:'+str(len(dicts)))
+raw_input()
 for dict in dicts:
     url =  dict['page_url']
     print('url:'+url)
