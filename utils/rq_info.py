13857880dc0fd0ae467516aea9a2f6c39edd0bdb
jr <jeremy@trendiguru.com>
2016-01-13 11:31:47 +0200

    connection-redis_conn

--- utils/rq_info.py
+++ utils/rq_info.py
@@ -18,10 +18,30 @@ def failed_info():
     failed_jobs = fq.jobs
     print('len:'+str(len(failed_jobs)))
     for job in failed_jobs:
-        exception_info = job.exc_info
+        dict = job.to_dict()
+        for key,val in dict:
+            print(key,val)
+        created_time = dict['created_at']
+        exception_info = dict['exc_info']
         #print('exception info:'+str(exception_info))
         lines = exception_info.split('\n')
-        print(lines[-2])
+        enqueue_time = dict['enqueued_at']
+        end_time = dict['ended_at']
+        func =dict['function_name']
+        args = dict['args']
+        id = dict['id']
+        print('id {0} created {1} ended {2} function {3} args {4}'.format(id,created_time,end_time,func))
 
 if __name__ == "__main__":
     failed_info()
+
+'''
+    job.args                 job.dependency           job.fetch                job.id                   job.kwargs               job.return_value
+job.cancel               job.dependents_key       job.func                 job.instance             job.meta                 job.save
+job.cleanup              job.dependents_key_for   job.func_name            job.is_failed            job.origin               job.set_id
+job.connection           job.description          job.get_call_string      job.is_finished          job.perform              job.set_status
+job.create               job.ended_at             job.get_id               job.is_queued            job.refresh              job.status
+job.created_at           job.enqueued_at          job.get_result_ttl       job.is_started           job.register_dependency  job.timeout
+job.data                 job.exc_info             job.get_status           job.key                  job.result               job.to_dict
+job.delete               job.exists               job.get_ttl              job.key_for              job.result_ttl           job.ttl
+'''
