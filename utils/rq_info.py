638eaf12d90edce775fef6dac7d564ea18e67b35
jr <jeremy@trendiguru.com>
2016-01-15 15:05:10 +0200

    funged up my nice func

--- utils/rq_info.py
+++ utils/rq_info.py
@@ -34,7 +34,7 @@ def failed_info():
  #       for key in dict:
 #            print(key,dict[key])
         if 'exc_info' in dict:
-            exception_info = dict['exc_info']:
+            exception_info = dict['exc_info']
             if exception_info == '' or exception_info is None:
                 exception_info = 'no info given\nno info given'
         else:
