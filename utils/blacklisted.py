08fdb4bf21491066ed2ffd08efc98b7ce9370180
jr <jeremy@trendiguru.com>
2016-01-16 23:05:08 +0200

    add time stamp anddump errs to file

--- utils/blacklisted.py
+++ utils/blacklisted.py
@@ -5,7 +5,7 @@ from trendi import constants
 db = constants.db
 bl = db.blacklisted_urls
 dicts = bl.find()
-print('count:'+dicts.count())
+print('count:'+str(dicts.count()))
 raw_input()
 dict = dicts.next()
 while dict is not None:
