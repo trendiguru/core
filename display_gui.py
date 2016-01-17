15f439adf62f106c651e261270e3044aace2da9a
yonti <yontilevin@gmail.com>
2016-01-17 20:27:19 +0200

    demo yonti

---
+++ display_gui.py
@@ -0,0 +1,23 @@
+__author__ = 'yonatan'
+
+from . import constants
+
+db = constants.db
+
+
+def getItems(idx):
+    items = db.images.find().batch_size(100)
+    start = int(idx)
+    batch = []
+    for i in range(start, 100 + start):
+        tmp_item = items[i]
+        tmp = {"item_urls": tmp_item["image_urls"],
+               "people": tmp_item["people"]}
+        for x, candidate in enumerate(tmp["people"]):
+            for y, cand_item in enumerate(candidate["items"]):
+                tmp["people"][x]["items"][y]["category"] = cand_item['category']
+                tmp["people"][x]["items"][y]["saved_date"] = cand_item['saved_date']
+                for w in range(10):
+                    tmp["people"][x]["items"][y]["top10"][w] = cand_item['similar_results'][w]["sizes.Xlarge.url"]
+        batch.append(tmp)
+    return batch
