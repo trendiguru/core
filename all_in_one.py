29c6ad6cf214935f87898656ca8acb277e400eb3
yonti <yontilevin@gmail.com>
2016-01-17 21:42:23 +0200

    demo yonti

--- all_in_one.py
+++ all_in_one.py
@@ -387,8 +387,7 @@ def get_svg_nate(image_url):
         else:
             # no faces, only general positive human detection
             person = {'face': [], 'person_id': str(bson.ObjectId()), 'person_idx': 1, 'items': []}
-            mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image, async=False,
-                                                                           queue_name='pd_yonti').result[:3]
+            mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image, async=False).result[:3]
             final_mask = after_pd_conclusions(mask, labels)
             item_idx = 0
             for num in np.unique(final_mask):
