bea6af8a6273817b4e9c5f22717efe63b88364ea
Nadav <nadav@trendiguru.com>
2016-01-17 15:51:47 +0200

    log

--- paperdolls.py
+++ paperdolls.py
@@ -253,7 +253,7 @@ def start_process(page_url, image_url, lang=None):
     # NEW_IMAGE !!
     print "Start process image shape: " + str(image.shape)
     relevance = background_removal.image_is_relevant(image, use_caffe=False, image_url=image_url)
-    image_dict = {'image_urls': [image_url], 'relevant': relevance.is_relevant,
+    image_dict = {'image_urls': [image_url], 'relevant': relevance.is_relevant, 'views': 1,
                   'image_hash': image_hash, 'page_urls': [page_url], 'people': []}
     if relevance.is_relevant:
         # There are faces
@@ -270,6 +270,7 @@ def start_process(page_url, image_url, lang=None):
             q1.enqueue_call(func=from_paperdoll_to_similar_results, args=(person['person_id'], paper_job.id, 100,
                                                                           products_collection, coll_name),
                             depends_on=paper_job, ttl=TTL, result_ttl=TTL, timeout=TTL)
+        logging.warning("trying to insert {0}".format(image_dict))
         iip.insert_one(image_dict)
     else:  # if not relevant
         logging.warning('image is not relevant, but stored anyway..')
