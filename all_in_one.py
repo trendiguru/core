be0251041acbcd8308b69f68362c20c4364bb315
yonti <yontilevin@gmail.com>
2016-01-17 18:09:39 +0200

    demo yonti

--- all_in_one.py
+++ all_in_one.py
@@ -344,8 +344,7 @@ def get_svg_nate(image_url):
                 image_copy = person_isolation(image, face)
                 person = {'face': face, 'person_id': str(bson.ObjectId()), 'person_idx': 1,
                           'items': []}
-                mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image_copy, person['person_id'],
-                                                                               queue_name=worker_name).result[:3]
+                mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image_copy).result[:3]
                 final_mask = after_pd_conclusions(mask, labels, person['face'])
                 # image = draw_pose_boxes(pose, image)
                 item_idx = 0
@@ -387,7 +386,7 @@ def get_svg_nate(image_url):
         else:
             # no faces, only general positive human detection
             person = {'face': [], 'person_id': str(bson.ObjectId()), 'person_idx': 1, 'items': []}
-            mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image, queue_name=worker_name).result[:3]
+            mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image).result[:3]
             final_mask = after_pd_conclusions(mask, labels)
             item_idx = 0
             for num in np.unique(final_mask):
