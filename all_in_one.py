4bc0f3e3630f3d5c92cddf1a29efdd47787d7b8c
yonti <yontilevin@gmail.com>
2016-01-17 21:33:07 +0200

    demo yonti

--- all_in_one.py
+++ all_in_one.py
@@ -344,8 +344,8 @@ def get_svg_nate(image_url):
                 image_copy = person_isolation(image, face)
                 person = {'face': face, 'person_id': str(bson.ObjectId()), 'person_idx': 1,
                           'items': []}
-                mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image_copy, async=False, at_front=True,
-                                                                               queue_name='pd_yonti').result[:3]
+                mask, labels, pose = paperdoll_parse_enqueue.paperdoll_enqueue(image_copy, async=False,
+                                                                               at_front=True).result[:3]
                 final_mask = after_pd_conclusions(mask, labels, person['face'])
                 # image = draw_pose_boxes(pose, image)
                 item_idx = 0
