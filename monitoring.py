106df88c47b31db99a8c67910399b70c88e5cc64
Nadav <nadav@trendiguru.com>
2016-01-25 12:12:10 +0200

    moni

--- monitoring.py
+++ monitoring.py
@@ -240,9 +240,16 @@ def download_last_x_logs(x):
     address = "/home/developer/logs"
     os.chdir(address)
     last_x_log = subprocess.check_output([command, 'ls ' + page]).split('\n')[-(x + 1):-1]
+    saved_logs = []
     for log in last_x_log:
         filename = log[len(page) + 1:] + ".csv"
         subprocess.call([command, 'cp ' + log + ' ' + filename])
+        saved_logs.append(address + '/' + filename)
+    return saved_logs
+
+
+# def save_log_to_mongo(log_file):
+
 
 
 if __name__ == "__main__":
