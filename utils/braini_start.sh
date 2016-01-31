4b39d020ec6f28e6a0c22e299560d07e99c2c9a1
jr <jeremy@trendiguru.com>
2016-01-31 13:18:25 +0200

    shell script for braini startup

--- utils/braini_start.sh
+++ utils/braini_start.sh
@@ -5,6 +5,7 @@ lsof -ti:27017 | xargs kill -9
 ssh -f -N -L 6379:redis1-redis-1-vm:6379 root@extremeli.trendi.guru
 ssh -f -N -L 27017:mongodb1-instance-1:27017 root@extremeli.trendi.guru
 
+#how to get out of screen without using ctrl-A d ?
 screen -dRR shepherd
 cd /home/pd_user/trendi/paperdoll
 python doll_shepherd.py --N=47
