c357be291cc7a01e29257bbe0ee730e87013bca4
yonti <yontilevin@gmail.com>
2016-02-02 10:14:33 +0200

    demo yonti

--- Yonti/screen.py
+++ Yonti/screen.py
@@ -8,8 +8,9 @@ from termcolor import colored
 
 def master():
     print colored("starting master...", "green", attrs=["bold"])
-    commands = "sudo bash ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe &"
-    subprocess.call(["screen -S test1 " + commands], shell=True)
+    # commands = "./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe &"
+    subprocess.call(["screen -S test1 'sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe &'"],
+                    shell=True)
     sleep(2)
     print colored("closing master...", "green", attrs=["bold"])
 
