d8d813044a4c6177e46b5a2d1a1b00b3e685843a
yonti <yontilevin@gmail.com>
2016-02-02 11:05:57 +0200

    demo yonti

--- printStuff.py
+++ printStuff.py
@@ -8,8 +8,13 @@ from termcolor import colored
 colors = ["red", "blue", "green", "magenta", "yellow"]
 
 if __name__ == "__main__":
+    rc_list = []
     for i in range(10):
-        sleep(1)
-        subprocess.call(["sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe &"], shell=True)
+        child = subprocess.Popen(["sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe"],
+                                 shell=True)
         r = random.randint(0, 4)
-        print colored(str(i) + str(i) + str(i), colors[r])
+        print colored("BroseMe %s is opened" % (str(i)), colors[r])
+        rc_list.append(child.returncode)
+
+    while all(rc is None for rc in rc_list):
+        sleep(30)
