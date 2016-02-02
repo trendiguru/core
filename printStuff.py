e524d0eb0d77ad600fdb99d441da3e83db249d1c
yonti <yontilevin@gmail.com>
2016-02-02 12:48:47 +0200

    demo yonti

--- printStuff.py
+++ printStuff.py
@@ -11,7 +11,8 @@ colors = ["red", "blue", "green", "magenta", "yellow"]
 if __name__ == "__main__":
     rc_list = []
     # for i in range(1):
-    child = subprocess.Popen(["sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe"])
+    child = subprocess.Popen(["sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe"],
+                             shell=True)
     r = random.randint(0, 4)
     print colored("BroseMe %s is opened" % (str(i)), colors[r])
     rc_list.append(child.returncode)
