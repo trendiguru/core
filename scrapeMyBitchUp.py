63fab94a166030d75d0786bf049960a5672f8d55
yonti <yontilevin@gmail.com>
2016-02-02 20:00:59 +0200

    scraper

--- scrapeMyBitchUp.py
+++ scrapeMyBitchUp.py
@@ -18,7 +18,7 @@ crawlMe_q = Queue('CrawlMe', connection=redis_conn)
 if __name__ == "__main__":
     urlid = time.time()
     print "start time: {0}".format(datetime.datetime.utcnow())
-    if mainScraper.screenCheck():
+    if not mainScraper.screenCheck():
         mainScraper.master()
     link_list = ['http://www.' + domain[4:] for domain in monitoring.get_top_x_whitelist(100).keys()]
     for link in link_list:
