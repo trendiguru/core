04b2dfb345464fd4c29291d06fbc8671ffbb1c71
yonti <yontilevin@gmail.com>
2016-02-02 12:37:04 +0200

    demo yonti

--- printStuff.py
+++ printStuff.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     rc_list = []
     for i in range(1):
         child = subprocess.Popen(["sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe"],
-                                 shell=True)
+                                 shell=True, preexec_fn=os.setsid)
         r = random.randint(0, 4)
         print colored("BroseMe %s is opened" % (str(i)), colors[r])
         rc_list.append(child.returncode)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         a += 1
         if a == 2:
             print colored("killllllllllllllllll", "green")
-            os.kill(child.pid, signal.SIGINT)
+            os.killpg(os.getpgid(child.pid), signal.SIGTERM)
         sleep(30)
 
         print colored("still working", 'yellow')
