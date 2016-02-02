b9689531a2d13783e1c739cd9f94b6db21f4cfd7
yonti <yontilevin@gmail.com>
2016-02-02 11:57:08 +0200

    demo yonti

--- Yonti/screen.py
+++ Yonti/screen.py
@@ -1,7 +1,6 @@
 __author__ = 'yonatan'
 
 import subprocess
-from time import sleep
 
 from termcolor import colored
 
@@ -10,10 +9,9 @@ def master():
     print colored("starting master...", "green", attrs=["bold"])
     # commands = "./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe &"
     subprocess.call(
-        ["screen -S test1 python -m trendi.printStuff"],
+        ["screen -S scraper python -m trendi.printStuff"],
         shell=True)
-    sleep(2)
-    print colored("closing master...", "green", attrs=["bold"])
+    print colored("sraper detached/terminated", "green", attrs=["bold"])
 
 
 if __name__ == "__main__":
--- printStuff.py
+++ printStuff.py
@@ -9,7 +9,7 @@ colors = ["red", "blue", "green", "magenta", "yellow"]
 
 if __name__ == "__main__":
     rc_list = []
-    for i in range(10):
+    for i in range(2):
         child = subprocess.Popen(["sudo ./xvfb-run-safe.sh rqworker -u redis://redis1-redis-1-vm:6379 BrowseMe"],
                                  shell=True)
         r = random.randint(0, 4)
@@ -18,3 +18,7 @@ if __name__ == "__main__":
 
     while all(rc is None for rc in rc_list):
         sleep(30)
+        print colored("still working", 'yellow')
+
+    print colored("exiting", 'red')
+    sleep(15)
