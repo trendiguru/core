e29c40dc92ab200df07dd6fb99d0610ed5690a75
jr <jeremy@trendiguru.com>
2016-02-04 11:38:46 +0200

    shell script for caffe - get everything ready then edit makefile by hand

--- docker_containers/caffe/caffe.sh
+++ docker_containers/caffe/caffe.sh
@@ -7,6 +7,8 @@ cd caffe
 # Install python dependencies
 cat python/requirements.txt | xargs -n1 pip install && \
 
+#prereqs for cuda
+sudo apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
 
 sudo apt-get update
 sudo apt-get install -y  bc
@@ -38,6 +40,7 @@ sudo apt-get install -y  wget
 #sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
 #sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 #sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libboost-all-dev
 
 # glog
 wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
@@ -45,6 +48,7 @@ tar zxvf glog-0.3.3.tar.gz
 cd glog-0.3.3
 ./configure
 make && make install
+
 # gflags
 wget https://github.com/schuhschuh/gflags/archive/master.zip
 unzip master.zip
@@ -52,22 +56,24 @@ cd gflags-master
 mkdir build && cd build
 export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
 make && make install
+
 # lmdb
 git clone git://gitorious.org/mdb/mdb.git
 cd mdb/libraries/liblmdb
 make && make install
 
 # Make and move into build directory
-mkdir build && cd build && \
+cp Makefile.config.example Makefile.config
+sed -i 's/# USE_CUDNN := 1/USE_CUDNN := 1/' Makefile.config
+sed -i 's/# OPENCV_VERSION := 3/OPENCV_VERSION := 3/' Makefile.config
+sed -i 's/CUDA_DIR := \/usr\/local\/cuda/CUDA_DIR := \/usr\/local\/cuda-7.5/' Makefile.config
+mkdir build && cd build
 
 # CMake
-cmake .. && \
+#cmake ..
 
 # Make
 #make -j"$(nproc)" all
-make -j45 all
+#make -j45 all
 
-
-
-#prereqs for cuda
-sudo apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev
+#make pycaffe
