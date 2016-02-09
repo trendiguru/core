00f9d8cf57fc7f146c21561fca7e76dca975e8d1
jr <jeremy@trendiguru.com>
2016-02-09 18:32:36 +0200

    lenet - digit detection

--- classifier_stuff/caffe_nns/lenet_ex.py
+++ classifier_stuff/caffe_nns/lenet_ex.py
@@ -1,14 +1,24 @@
 # coding: utf-8
+import os
+print os.getuid() # numeric uid
+import pwd
+import sys
+print pwd.getpwuid(os.getuid()) # full /etc/passwd info
+
+
+#sys.path.insert(0,'./python')
+##sys.path.insert(0,'/usr/lib/python2.7/dist-packages/trendi')
+#sys.path.insert(0,'/home/jr/sw/caffe/python')
+print(sys.path)
+
 import caffe
 import os
-import sys
 import socket
 from pylab import *
 from caffe import layers as L
 from caffe import params as P
-from matplotlib import *
-sys.path.insert(0,'./python')
-sys.path
+#from matplotlib import *
+from matplotlib import pyplot as plt
 
 #get_ipython().system(u'data/mnist/get_mnist.sh')
 #get_ipython().system(u'examples/mnist/create_mnist.sh')
@@ -45,7 +55,51 @@ solver = caffe.SGDSolver('examples/mnist/lenet_auto_solver.prototxt')
 solver.net.forward()  # train net
 solver.test_nets[0].forward()  # test net (there can be more than one)
 # we use a little trick to tile the first eight images
-imshow(solver.net.blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray')
+plt.imshow(solver.net.blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray')
 print solver.net.blobs['label'].data[:8]
-imshow(solver.test_nets[0].blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray')
+plt.imshow(solver.test_nets[0].blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray')
 print solver.test_nets[0].blobs['label'].data[:8]
+solver.step(1)
+plt.imshow(solver.net.params['conv1'][0].diff[:, 0].reshape(4, 5, 5, 5)
+       .transpose(0, 2, 1, 3).reshape(4*5, 5*5), cmap='gray')
+
+#%%time
+niter = 200
+test_interval = 25
+# losses will also be stored in the log
+train_loss = zeros(niter)
+test_acc = zeros(int(np.ceil(niter / test_interval)))
+output = zeros((niter, 8, 10))
+
+# the main solver loop
+for it in range(niter):
+    solver.step(1)  # SGD by Caffe
+
+    # store the train loss
+    train_loss[it] = solver.net.blobs['loss'].data
+
+    # store the output on the first test batch
+    # (start the forward pass at conv1 to avoid loading new data)
+    solver.test_nets[0].forward(start='conv1')
+    output[it] = solver.test_nets[0].blobs['ip2'].data[:8]
+
+    # run a full test every so often
+    # (Caffe can also do this for us and write to a log, but we show here
+    #  how to do it directly in Python, where more complicated things are easier.)
+    if it % test_interval == 0:
+        print 'Iteration', it, 'testing...'
+        correct = 0
+        for test_it in range(100):
+            solver.test_nets[0].forward()
+            correct += sum(solver.test_nets[0].blobs['ip2'].data.argmax(1)
+                           == solver.test_nets[0].blobs['label'].data)
+        test_acc[it // test_interval] = correct / 1e4
+
+
+_, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(arange(niter), train_loss)
+ax2.plot(test_interval * arange(len(test_acc)), test_acc, 'r')
+ax1.set_xlabel('iteration')
+ax1.set_ylabel('train loss')
+ax2.set_ylabel('test accuracy')
