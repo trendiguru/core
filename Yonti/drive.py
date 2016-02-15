8faaf9545196f44c65a7bcb30dad1d9a8b2195bc
yonti <yontilevin@gmail.com>
2016-02-15 16:58:36 +0200

    db download

---
+++ Yonti/drive.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import os
+
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/drive.file'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store, flags) \
+            if flags else tools.run(flow, store)
+DRIVE = build('drive', 'v2', http=creds.authorize(Http()))
+
+FILES = (
+    ('xl1.xlsx', False),
+    ('xl1.xlsx', True),
+)
+
+for filename, convert in FILES:
+    metadata = {'title': filename}
+    res = DRIVE.files().insert(convert=convert, body=metadata,
+            media_body=filename, fields='mimeType,exportLinks').execute()
+    if res:
+        print('Uploaded "%s" (%s)' % (filename, res['mimeType']))
+
+# if res:
+#     MIMETYPE = 'application/pdf'
+#     res, data = DRIVE._http.request(res['exportLinks'][MIMETYPE])
+#     if data:
+#         fn = '%s.pdf' % os.path.splitext(filename)[0]
+#         with open(fn, 'wb') as fh:
+#             fh.write(data)
+#         print('Downloaded "%s" (%s)' % (fn, MIMETYPE))
