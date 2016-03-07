745b321a71d050733edc672eaca1fb54e3ffa70c
yonti <yontilevin@gmail.com>
2016-03-07 17:05:43 +0200

    ebay

--- dl_excel.py
+++ dl_excel.py
@@ -2,6 +2,10 @@ import xlsxwriter
 from .Yonti import drive
 from . import constants
 from . import ebay_constants
+import datetime
+
+today_date = str(datetime.datetime.date(datetime.datetime.now()))
+
 db = constants.db
 
 filename = 'ebay'
@@ -13,8 +17,11 @@ worksheet_main = workbook.add_worksheet('main')
 categories = []
 print ("total count = %s" %(db.ebay_Female.count()))
 for cat in ebay_constants.categories_keywords:
-    count = db.ebay_Female.find({'categories': cat}).count()
-    categories.append([cat, count])
+    items = db.ebay_Female.find({'categories': cat}).count()
+    new_items = db.ebay_Female.find({'categories': cat, 'download_data.first_dl': today_date}).count()
+    instock = db.ebay_Female.find({'categories': cat, 'status.instock': True}).count()
+    out = db.ebay_Female.find({'categories': cat, 'status.instock': False}).count()
+    categories.append([cat, items,new_items, instock, out])
 
 # #create headers
 bold = workbook.add_format({'bold': True, })
@@ -25,12 +32,15 @@ bold = workbook.add_format({'bold': True, })
 # # Start from the first cell. Rows and columns are zero indexed.
 # row = 1
 # col = 0
-worksheet_main.set_column('B:C',20)
-worksheet_main.add_table('B2:C'+str(len(categories)+3),
+worksheet_main.set_column('B:E',20)
+worksheet_main.add_table('B2:E'+str(len(categories)+3),
                          {'data' : categories,
                           'total_row': True,
-                          'columns': [{'header': 'Categories', 'total_string': 'Total'},
-                                      {'header': 'Count',   'total_function' : 'sum'},],
+                          'columns': [{'header': 'Category', 'total_string': 'Total'},
+                                      {'header': 'items',    'total_function' : 'sum'},
+                                      {'header': 'new items','total_function' : 'sum'},
+                                      {'header': 'instock',  'total_function' : 'sum'},
+                                      {'header': 'out of stock', 'total_function' : 'sum'}],
                           'banded_columns': True,
                           'banded_rows': False,
                           'header_row': True})
@@ -50,6 +60,6 @@ print ('uploading to drive...')
 files = (filename,path2file, True)
 res = drive.upload2drive(files)
 if res:
-    print('files uploaded!')
+    print('file uploaded!')
 else:
     print ('error while uploading!')
