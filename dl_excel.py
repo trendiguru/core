f095b7f41b000ade64412e864f2facf463508fe2
yonti <yontilevin@gmail.com>
2016-03-07 16:20:39 +0200

    ebay

--- dl_excel.py
+++ dl_excel.py
@@ -25,7 +25,7 @@ bold = workbook.add_format({'bold': True})
 # # Start from the first cell. Rows and columns are zero indexed.
 # row = 1
 # col = 0
-worksheet_main.set_rows(70)
+worksheet_main.set_cols(1-2,70)
 worksheet_main.add_table('B2:C'+str(len(categories)),
                          {
                              'data' : categories,
