42bcddedf2d2fe9ee9c23fc3ad74dd35ef150c57
yonti <yontilevin@gmail.com>
2016-03-07 13:44:02 +0200

    ebay

--- Yonti/drive.py
+++ Yonti/drive.py
@@ -14,7 +14,7 @@ parent_folder = "0B-fDiFA73MH_N1ZCNVNYcW0tRFk"
 def is_file_in_folder(service, folder_id, file_name):
     # param={"name":='ebay'"}
     try:
-        children = service.files().list(folderId=folder_id, fields='files(name)').execute()
+        children = service.children().list(folderId=folder_id, fields='files(name)').execute()
         for child in children.get('name', []):
             print(child)
             # child_name = child['name']
