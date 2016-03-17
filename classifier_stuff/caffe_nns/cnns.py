629b036e2facc650f00470bbc637084c9669d949
jeremy <jeremy@trendiguru.com>
2016-03-17 19:44:55 +0200

    maybe implace=True unecessary for dropout

--- classifier_stuff/caffe_nns/cnns.py
+++ classifier_stuff/caffe_nns/cnns.py
@@ -708,14 +708,15 @@ def small_googLeNet(db, batch_size, n_classes=11, meanB=128, meanG=128, meanR=12
                             bias_filler=dict(type='constant',value=0.2))
     n.inception_3a_relu_pool_proj_25 = L.ReLU(n.inception_3a_pool_proj_24, in_place=True)
     n.inception_3a_output_26 = L.Concat(bottom=[n.inception_3a_1x1_13,n.inception_3a_3x3_17,n.inception_3a_5x5_21,n.inception_3a_pool_proj_24])
-    n.inception_3a_avg_pool = L.Pooling(n.inception_3a_output_26, kernel_size=7, stride = 1,pool=P.Pooling.AVE)
-    n.final_dropout = L.Dropout(n.inception_3a_avg_pool, dropout_ratio=dict(factor=0.4))
-
-    n.output_layer = L.InnerProduct(n.inception_3a_avg_pool,param=[dict(lr_mult=lr_mult1),dict(lr_mult=lr_mult2)],num_output=n_classes,weight_filler=dict(type='xavier'))
     return n.to_proto()
 
-
 '''
+
+    n.inception_3a_avg_pool = L.Pooling(n.inception_3a_output_26, kernel_size=7, stride = 1,pool=P.Pooling.AVE)
+    n.final_dropout = L.Dropout(n.inception_3a_avg_pool, dropout_ratio=dict(factor=0.4),in_place=True)
+    n.output_layer = L.InnerProduct(n.inception_3a_avg_pool,param=[dict(lr_mult=lr_mult1),dict(lr_mult=lr_mult2)],num_output=n_classes,weight_filler=dict(type='xavier'))
+
+
 
     n.loss = L.SoftmaxWithLoss(n.output_layer,n.label)
 #    n.accuracy = L.Accuracy(n.output_layer,n.label,include=[dict(phase=TEST)])
