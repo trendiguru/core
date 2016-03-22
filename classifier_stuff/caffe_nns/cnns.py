9a1460bfaab154e63fcf5cf52816c9d810a12c20
jeremy <jeremy@trendiguru.com>
2016-03-22 22:01:41 +0200

    150w 200h

--- classifier_stuff/caffe_nns/cnns.py
+++ classifier_stuff/caffe_nns/cnns.py
@@ -1349,8 +1349,8 @@ if __name__ == "__main__":
         db_name = 'multi_retrieval'
         use_visual_output = False
 #    h,w,d,B,G,R,n = imutils.image_stats_from_dir_of_ditestrs(dir_of_dirs)
-    resize_x = None
-    resize_y=None
+    resize_x = 150
+    resize_y= 200
     print('dir:'+dir_of_dirs)
     print('rgb:'+str(R)+','+str(G)+','+str(B))
     print('mode:'+solver_mode)
