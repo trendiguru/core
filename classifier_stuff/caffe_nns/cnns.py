1baf1a53d589652ebf94c9d14b8d9f4419c83e20
jeremy <jeremy@trendiguru.com>
2016-03-23 13:44:31 +0200

    small glnt

--- classifier_stuff/caffe_nns/cnns.py
+++ classifier_stuff/caffe_nns/cnns.py
@@ -1338,6 +1338,8 @@ if __name__ == "__main__":
         dir_of_dirs = '/home/jeremy/core/classifier_stuff/caffe_nns/dataset/cropped'  #b2
         dir_of_dirs = '/home/jeremy/core/classifier_stuff/caffe_nns/dataset/retrieval'  #b2
         nn_dir = '/home/jeremy/core/classifier_stuff/caffe_nns/alexnet12_multi_retrieval'  #b2
+        nn_dir = '/home/jeremy/core/classifier_stuff/caffe_nns/small_goog_multi_retrieval'  #b2
+
 #        nn_dir = '/home/jeremy/core/classifier_stuff/caffe_nns/vgg_'  #b2
         max_images_per_class = 15000
         pc = False
