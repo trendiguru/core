11e7851053b1abef1e08b02c711d95bb477ebc07
jeremy <jeremy@trendiguru.com>
2016-03-24 12:15:03 +0200

    2 inceptions

--- classifier_stuff/darknet_convert.py
+++ classifier_stuff/darknet_convert.py
@@ -3,6 +3,7 @@ import os
 import logging
 import cv2
 import random
+import socket
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -12,26 +13,33 @@ logging.basicConfig(level=logging.DEBUG)
 #these aparently correspond to filename lines in the train.txt file
 # so we'll step thru dirs and append the train.txt file as well as the bbox file
 
+#Note that each image corresponds to an annotation file. But we only need one single training list of images.
+# Remember to put the folder 'images' and folder 'annotations' in the same parent directory,
+# as the darknet code look for annotation files this way (by default).
 
-def dir_of_dirs_to_darknet(dir_of_dirs, trainfile,bbfile,positive_filter=None,maxfiles_per_dir=999999):
+
+
+def dir_of_dirs_to_darknet(dir_of_dirs, trainfile,annotations_dir, positive_filter=None,maxfiles_per_dir=999999,bbfile_prefix=None):
     initial_only_dirs = [dir for dir in os.listdir(dir_of_dirs) if os.path.isdir(os.path.join(dir_of_dirs,dir))]
  #   print(str(len(initial_only_dirs))+' dirs:'+str(initial_only_dirs)+' in '+dir_of_dirs)
     # txn is a Transaction object
     #prepare directories
     only_dirs = []
     category_number = 0
+    ensure_dir(annotations_dir)
+    print('outer dir of dirs:{} trainfile:{}'.format(dir_of_dirs,trainfile))
     for a_dir in initial_only_dirs:
         #only take 'test' or 'train' dirs, if test_or_train is specified
         if (not positive_filter or positive_filter in a_dir):
             print('doing directory {} '.format(a_dir))
             fulldir = os.path.join(dir_of_dirs,a_dir)
             only_dirs.append(fulldir)
-            n_files = dir_to_darknet(fulldir,trainfile,bbfile,category_number,maxfiles_per_dir=maxfiles_per_dir)
+            n_files = dir_to_darknet(fulldir,trainfile,category_number,annotations_dir,maxfiles_per_dir=maxfiles_per_dir,bbfile_prefix=bbfile_prefix)
             print('did {} files in {}'.format(n_files,a_dir))
             category_number += 1
 
 
-def dir_to_darknet(dir, trainfile,category_number,randomize=True,maxfiles_per_dir=999999,bbfile_prefix=None):
+def dir_to_darknet(dir, trainfile,category_number,annotations_dir,randomize=True,maxfiles_per_dir=999999,bbfile_prefix=None):
     only_files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
     new_length = min(len(only_files),maxfiles_per_dir)
     only_files = only_files[0:new_length]
@@ -41,10 +49,20 @@ def dir_to_darknet(dir, trainfile,category_number,randomize=True,maxfiles_per_di
     n_digits = 5
     with open(trainfile,'a') as fp_t:
         for a_file in only_files:
-            basedir = os.path.dirname(dir)
-            bbfile = basedir +"_{0:0>3}".format(n)+'.txt'
-            if bbfile_prefix:
-                bbfile = bbfile_prefix+"_{0:0>3}".format(n)+'.txt'
+            containing_dir = os.path.split(dir)[-1]
+            bbfilename_from_outerdir = False
+            if bbfilename_from_outerdir:
+                bbfile = containing_dir +"_{0:0>3}".format(n)+'.txt'  #dir_003.txt
+                bbfile = os.path.join(dir,bbfile)              #dir_of_dirs/dir/dir_003.txt
+                if bbfile_prefix:
+                    bbfile = bbfile_prefix+"_{0:0>3}".format(n)+'.txt'
+            else:
+                filebase = a_file[0:-4]   #file.jpg -> file
+                bbfile = filebase+"_{0:0>3}".format(n)+'.txt'
+                bbfile = os.path.join(dir,bbfile)              #dir_of_dirs/dir/dir_003.txt
+            filebase = a_file[0:-4]   #file.jpg -> file
+            bbfile = filebase+'.txt'
+            bbfile = os.path.join(annotations_dir,bbfile)              #dir_of_dirs/dir/dir_003.txt
             print('bbfilename:'+str(bbfile))
             with open(bbfile,'w') as fp_bb:
                 full_filename = os.path.join(dir,a_file)
@@ -58,8 +76,8 @@ def dir_to_darknet(dir, trainfile,category_number,randomize=True,maxfiles_per_di
                 fp_t.write(full_filename+'\n')
                 n = n + 1
 #                raw_input('enter for next')
+    #    fp_bb.flush()
         fp_bb.close()
-        fp_bb.flush()
     fp_t.close()
     return n
 
@@ -148,9 +166,38 @@ def convert(imsize, box):
     h = h*dh
     return (x,y,w,h)
 
+def ensure_dir(f):
+    '''
+
+    :param f: file or directory name
+    :return: no return val, creates dir if it doesnt exist
+    '''
+    logging.debug('f:' + f)
+    # d = os.path.dirname(f)
+    if not os.path.exists(f):
+        #        print('d:'+str(d))
+
+        os.makedirs(f)
+
+
+host = socket.gethostname()
+print('host:'+str(host))
 
 if __name__ == '__main__':
-    dir = '/home/jeremy/tg/berg_test/cropped/test_pairs_belts'
-    trainfile = '/home/jeremy/tg/trainjr.txt'
-    bbfile = '/home/jeremy/tg/bbjr.txt'
-    n_files = dir_to_darknet(dir,trainfile,bbfile,37)
+    if host == 'jr':
+        dir_of_dirs = '/home/jeremy/python-packages/trendi/classifier_stuff/caffe_nns/dataset'
+        images_dir = '/home/jeremy/tg/berg_test/cropped/'
+        dir = '/home/jeremy/tg/berg_test/cropped/test_pairs_belts'
+        trainfile = '/home/jeremy/tg/trainjr.txt'
+        bbfile = '/home/jeremy/tg/bbjr.txt'
+        annotations_dir = '/home/jeremy/annotations'
+    else:
+        dir_of_dirs = '/home/jeremy/core/classifier_stuff/caffe_nns/images'
+        images_dir = dir_of_dirs
+        dir = '/home/jeremy/tg/berg_test/cropped/test_pairs_belts'
+        trainfile =  '/home/jeremy/core/classifier_stuff/caffe_nns/trainfilejr.txt'
+        annotations_dir = '/home/jeremy/core/classifier_stuff/caffe_nns/annotations'
+
+
+    n_files = dir_of_dirs_to_darknet(images_dir,trainfile,annotations_dir,maxfiles_per_dir=10)
+#    n_files = dir_to_darknet(dir,trainfile,bbfile,37)
