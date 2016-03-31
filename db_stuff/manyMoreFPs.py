3ed99ec906efd1dcf86986f9749dc3de70c916f8
yonti <yontilevin@gmail.com>
2016-03-31 16:28:31 +0300

    eft

---
+++ db_stuff/manyMoreFPs.py
@@ -0,0 +1,43 @@
+import subprocess
+from time import sleep
+import argparse
+import sys
+from termcolor import colored
+
+def screen(workers):
+    print colored("######  starting to open FPs  ######", "green", attrs=["bold"])
+    cmd = "screen -S scraper python -m trendi.db_stuff.manyMoreFPs -f fps -w " + str(workers)
+    print colored("opening screen", "green", attrs=["bold"])
+    subprocess.call([cmd], shell=True)
+    print colored("screen detached", "yellow", attrs=["bold"])
+
+
+def fps(w):
+    sleep(1)
+    for i in range(int(w)):
+        sleep(1)
+        subprocess.Popen(["rqworker -u redis://redis1-redis-1-vm:6379 fingerprint_new"],shell=True)
+        print colored("fp_new #%s is opened" % (str(i)), 'green')
+
+    while True:
+        sleep(1000)
+
+def getUserInput():
+    parser = argparse.ArgumentParser(description='give me some more fingerprinters!')
+    parser.add_argument("-n", dest="workers", default="25", help="enter the number of workers to run simultaneously")
+    parser.add_argument("-f", dest="function", default = "screen", help="which function 2 run")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    user_input = getUserInput()
+    if user_input.function == "screen":
+        screen(user_input.workers)
+    elif user_input.function == "fps":
+        fps(user_input.workers)
+    else:
+        print("bad input")
+    sys.exit(0)
+
