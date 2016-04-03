f7dc2096d2996f5bd16e821998255abaf60f3da1
jeremy <jeremy@trendiguru.com>
2016-04-03 12:45:44 +0300

    extremeli not autopullling

--- classifier_stuff/bbox_web.py
+++ classifier_stuff/bbox_web.py
@@ -1,10 +1,13 @@
 __author__ = 'jeremy'
 import os
+import system
 from subprocess import call
 
 def do_delete(image_number):
-    command = 'ssh root@104.155.22.95 mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
-    call(command)
+    braini2_ip = 37.58.101.173
+    print('finished command')
+    command = 'ssh root@'+braini2_ip+' mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
+    os.system(command)
     print('finished command')
     with open('out.txt','a') as f:
         f.write('did '+command)
