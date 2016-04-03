113beb0ee0e6c5d0a2b7debf533ec6df5ae8cc5d
jeremy <jeremy@trendiguru.com>
2016-04-03 13:10:44 +0300

    trying cmd with sudo to avoid the fact that its run by www-data

--- classifier_stuff/bbox_web.py
+++ classifier_stuff/bbox_web.py
@@ -5,7 +5,7 @@ from subprocess import call
 def do_delete(image_number):
     braini2_ip = '37.58.101.173'
     print('finished command')
-    command = 'ssh root@'+braini2_ip+' mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
+    command = 'sudo ssh root@'+braini2_ip+' mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
     os.system(command)
     print('finished command')
     with open('out.txt','a') as f:
