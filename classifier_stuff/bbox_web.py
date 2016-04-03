942cc6ff7023fe1dd4375b54384c3cb3e11f2ca8
jeremy <jeremy@trendiguru.com>
2016-04-03 12:36:01 +0300

    def del -> syntax error

--- classifier_stuff/bbox_web.py
+++ classifier_stuff/bbox_web.py
@@ -3,8 +3,8 @@ import os
 from subprocess import call
 
 def do_delete(image_number):
-   # command = 'ssh root@104.155.22.95 mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
-   # call(command)
+    command = 'ssh root@104.155.22.95 mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
+    call(command)
     print('finished command')
     with open('out.txt','a') as f:
         f.write('did '+command)
