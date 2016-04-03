4426d38751337695203c1336dedc2a80d27910d6
jeremy <jeremy@trendiguru.com>
2016-04-03 10:51:52 +0300

    funcs in separate file

--- classifier_stuff/bbox_web.py
+++ classifier_stuff/bbox_web.py
@@ -5,3 +5,7 @@ from subprocess import call
 def del(image_number):
     command = 'ssh root@104.155.22.95 mv /home/jeremy/trainjr.txt /home/jeremy/MOVEDITBABY.txt'
     call(command)
+    print('finished command')
+    with open('out.txt','a') as f:
+        f.write('did '+command)
+        f.close()
