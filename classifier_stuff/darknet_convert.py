1a8b89615a98fff8577f4bf0f963769abeef21fd
jeremy <jeremy@trendiguru.com>
2016-04-04 14:30:21 +0300

    bbs to db

--- classifier_stuff/darknet_convert.py
+++ classifier_stuff/darknet_convert.py
@@ -70,7 +70,7 @@ def bbs_to_db(dir_of_bbfiles,dir_of_images,use_visual_output=True):
                     newx = int(bb[0]*factor)
                     newy = int(bb[0]*factor)
                     im2 = cv2.resize(img_arr,(dest_width,dest_height))
-                    cv2.putText(im2,tamara_berg_categories[classno], (newx+1,newy+5), cv2.FONT_HERSHEY_SIMPLEX, 10, [int(255.0/10*classno),100,100],3)
+                    cv2.putText(im2,tamara_berg_categories[classno], (newx+1,newy+5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [int(255.0/10*classno),100,100],3)
 
                     cv2.imshow(imgfile,im2)
                     cv2.waitKey(100)
