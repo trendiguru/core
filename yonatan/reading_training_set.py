5e0e7ac836c4a341d7b336c8ed9a7acb312a0434
yonatan <yonatanguy@gmail.com>
2016-04-05 11:41:14 +0300

    checking i now pull from github and not bitbucket

--- yonatan/reading_training_set.py
+++ yonatan/reading_training_set.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 sets = {'train', 'test'}
 total_gender_pics = 0
 total_gender_train_pics = 1500
-total_gender_test_pics = 235
+total_gender_test_pics = 225
 
 for set in sets:
     if set == 'train':
