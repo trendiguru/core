34d78969ea79553bee94768708709e23853037b4
jeremy <jeremy@trendiguru.com>
2016-04-08 01:11:41 +0300

    type

--- downloaders/categorical_dl.py
+++ downloaders/categorical_dl.py
@@ -237,9 +237,9 @@ def get_shopstyle_nadav(download_dir='./',max_images_per_cat = 1000):
                          .format(count))
             h,w = img_arr.shape[:2]
             rect = [20, 20, w-40, h-40]
-            grabmask = background_removal.simple_mask_grabcut(img_arr)
-            grabmask = cat_count * grabmask / 255
-            grabmask = grabmask.astype(np.uint8)
+            bgfgmask = cat_count * bgfgmask / 255
+            bgfgmask = bgfgmask.astype(np.uint8)
+            grabmask = background_removal.simple_mask_grabcut(img_arr,rect=None,mask=bgfgmask)
             maskname = "{0}_{1}_mask.png".format(cat, prod["id"])
             success = cv2.imwrite(maskname, grabmask)
             if not success:
