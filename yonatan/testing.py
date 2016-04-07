a0cd2dd8442433442d6626986b10126fbd5458a7
yonatan <yonatanguy@gmail.com>
2016-04-07 20:41:08 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -2,6 +2,6 @@
 
 from trendi import background_removal
 
-base_image = background_removal.get_image()
+base_image = "home/yonatan/test_set/female/Juljia_Vysotskij_0001.jpg"
 print base_image.shape
 face_image = background_removal.find_face_cascade(base_image)
