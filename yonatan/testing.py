a17ba30367f4cc86054dda7bf534787ca62139d2
yonatan <yonatanguy@gmail.com>
2016-04-07 19:27:08 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
-import sys
-#sys.path.insert(0, 'home/yonatan/core')
-from background_removal import get_image
+import string
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+import collections
+import os
+
+import cv2
+import numpy as np
+
+def get_image():
+    Tk().withdraw()
+    filename = askopenfilename()
+    big_image = cv2.imread(filename)
+    return big_image
 
 base_image = get_image()
 print base_image
