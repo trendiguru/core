e696ab86cff72d41ecfaab3363aa32b2b3b1bacf
yonatan <yonatanguy@gmail.com>
2016-04-07 19:37:10 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from trendi import background_removal.py
+from trendi import background_removal
 
-base_image = get_image()
+base_image = background_removal.get_image()
 print base_image
 #face_image = core/background_removal.find_face_cascade(base_image)
 #print face_image
