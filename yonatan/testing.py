2def022ee1d567aa920ae6d25130b847e1f7c819
yonatan <yonatanguy@gmail.com>
2016-04-07 20:44:15 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
+import caffe
 from trendi import background_removal
 
-base_image = "home/yonatan/test_set/female/Juljia_Vysotskij_0001.jpg"
+path = "home/yonatan/test_set/female/Juljia_Vysotskij_0001.jpg"
+base_image = [caffe.io.load_image(path)]
 print base_image.shape
 face_image = background_removal.find_face_cascade(base_image)
