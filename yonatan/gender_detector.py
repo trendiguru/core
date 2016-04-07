a8b3b6182009a7161c56c645398b131644e4e187
yonatan <yonatanguy@gmail.com>
2016-04-07 16:11:41 +0300

    upload first draft of my gender_detector

---
+++ yonatan/gender_detector.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+classify.py is an out-of-the-box image classifer callable from the command line.
+
+By default it configures and runs the Caffe reference ImageNet model.
+"""
+import numpy as np
+import os
+import sys
+import argparse
+import glob
+import time
+
+import caffe
+
+
+#def main(argv):
+def main(input_image):
+
+    model_def = "/home/yonatan/core/yonatan/deploy.prototxt"
+    pretrained_model = "/home/yonatan/network_5000_train_set/intermediate_output_iter_10000.caffemodel"
+    caffe.set_mode_gpu()
+    image_dims = [250, 250]
+    mean, input_scale = None, None
+    channel_swap = [2, 1, 0]
+    raw_scale = 255.0
+    ext = 'jpg'
+
+    # Make classifier.
+    classifier = caffe.Classifier(model_def=model_def, pretrained_model=pretrained_model,
+            image_dims=image_dims, mean=mean,
+            input_scale=input_scale, raw_scale=raw_scale,
+            channel_swap=channel_swap)
+
+    # Load numpy array (.npy), directory glob (*.jpg), or image file.
+    input_file = os.path.expanduser(input_image)
+    if input_file.endswith('npy'):
+        print("Loading file: %s" % input_file)
+        inputs = np.load(input_file)
+    elif os.path.isdir(input_file):
+        print("Loading folder: %s" % input_file)
+        inputs =[caffe.io.load_image(im_f)
+                 for im_f in glob.glob(input_file + '/*.' + ext)]
+    else:
+        print("Loading file: %s" % input_file)
+        inputs = [caffe.io.load_image(input_file)]
+
+    print("Classifying %d inputs." % len(inputs))
+
+    # Classify.
+    start = time.time()
+    predictions = classifier.predict(inputs)
+    print("Done in %.2f s." % (time.time() - start))
+
+    # Save
+    #print("Saving results into %s" % args.output_file)
+    #np.save(args.output_file, predictions)
+    if predictions[0][0] > predictions[0][1]:
+        print "it's a boy!"
+    else:
+        print "it's a girl!"
+
+if __name__ == '__main__':
+    main()
