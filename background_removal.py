1d4ec0b3dc3c9c0cc4cbf9bd9cd6b3e7249abdef
jeremy <jeremy@trendiguru.com>
2016-04-07 18:56:40 +0300

    type

--- background_removal.py
+++ background_removal.py
@@ -364,11 +364,12 @@ def face_skin_color_estimation(image, face_rect):
     return skin_hue_list
 
 
-def simple_mask_grabcut(image, mask):
-    rect = (0, 0, image.shape[1] - 1, image.shape[0] - 1)
+def simple_mask_grabcut(image, rect=None, mask=None):
+    if not rect:
+        rect = (0, 0, image.shape[1] - 1, image.shape[0] - 1)
     bgdmodel = np.zeros((1, 65), np.float64)
     fgdmodel = np.zeros((1, 65), np.float64)
-    cv2.grabCut(image, mask, rect, bgdmodel, fgdmodel, 1, cv2.GC_INIT_WITH_MASK)
+    cv2.grabCut(image, mask, rect, bgdmodel, fgdmodel, 1, cv2.GC_INIT_WITH_RECT)
     mask2 = np.where((mask == 1) + (mask == 3), 255, 0).astype('uint8')
     return mask2
 
