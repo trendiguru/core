c0c95315bc6a254affb7acab178665da1849ce4c
yonatan <yonatanguy@gmail.com>
2016-04-07 18:14:06 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import sys
 sys.path.insert(0, '/home/yonatan/core')
-from background_removal import get_image
+import background_removal.py
 
-base_image = get_image()
+base_image = background_removal.get_image()
 print base_image
 #face_image = core/background_removal.find_face_cascade(base_image)
 #print face_image
