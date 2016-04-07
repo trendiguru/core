9b23fb9163fff3cdc99dc893078b84ad98459a90
yonatan <yonatanguy@gmail.com>
2016-04-07 19:34:46 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
-import string
-from Tkinter import Tk
-from tkFileDialog import askopenfilename
-import collections
-import os
 
-import cv2
-import numpy as np
-
-def get_image():
-    Tk().withdraw()
-    filename = askopenfilename()
-    big_image = cv2.imread(filename)
-    return big_image
+from Trendi import background_removal
 
 base_image = get_image()
 print base_image
