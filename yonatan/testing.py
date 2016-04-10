163485686d7c33b949baa801824da5ad0dd9b7a0
yonatan <yonatanguy@gmail.com>
2016-04-10 13:12:10 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 import caffe
 import numpy as np
-from trendi import background_removal
+from trendi import background_removal, Utils
 from PIL import Image
 
 path = "/home/yonatan/test_set/female/Juljia_Vysotskij_0001.jpg"
-im = Image.open(path)
-print im.size
+image = Utils.get_cv2_img_array(path)
+#im = Image.open(path)
+print image.size
 
+'''
 im_rgb = Image.open(path).convert('RGB')
 im_arr_rgb = np.array(im_rgb)
 print im_arr_rgb.size
+
 
 def is_grey_scale(img_path):
     im = Image.open(img_path).convert('RGB')
@@ -22,11 +25,11 @@ def is_grey_scale(img_path):
     return True
 
 print is_grey_scale(path)
+'''
 
 
 
-
-base_image = np.array([caffe.io.load_image(path)])
+#base_image = np.array([caffe.io.load_image(path)])
 #print base_image.shape
-face_image = background_removal.find_face_cascade(im)
+face_image = background_removal.find_face_cascade(image)
 print face_image
