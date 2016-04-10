c512a634de7c85dc01e0369c44f8fb126420907d
yonatan <yonatanguy@gmail.com>
2016-04-10 16:34:46 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -5,6 +5,10 @@ import numpy as np
 from trendi import background_removal, Utils, constants
 import cv2
 import os
+import sys
+import argparse
+import glob
+import time
 
 path = "/home/yonatan/test_set/female/Juljia_Vysotskij_0001.jpg"
 image = Utils.get_cv2_img_array(path)
@@ -55,3 +59,53 @@ print face_image
 print type(face_image)
 
 
+def the_detector(image):
+
+    input_image = image
+    MODLE_FILE = "/home/yonatan/core/yonatan/deploy.prototxt"
+    PRETRAINED = "/home/yonatan/network_5000_train_set/intermediate_output_iter_10000.caffemodel"
+    caffe.set_mode_gpu()
+    image_dims = [250, 250]
+    mean, input_scale = None, None
+    channel_swap = [2, 1, 0]
+    raw_scale = 255.0
+    ext = 'jpg'
+
+    # Make classifier.
+    classifier = caffe.Classifier(MODLE_FILE, PRETRAINED,
+            image_dims=image_dims, mean=mean,
+            input_scale=input_scale, raw_scale=raw_scale,
+            channel_swap=channel_swap)
+
+    # Load numpy array (.npy), directory glob (*.jpg), or image file.
+    #input_file = os.path.expanduser(input_image)
+    if input_file.endswith('npy'):
+        print("Loading file: %s" % input_file)
+        inputs = np.load(input_file)
+    elif os.path.isdir(input_file):
+        print("Loading folder: %s" % input_file)
+        inputs =[caffe.io.load_image(im_f)
+                 for im_f in glob.glob(input_file + '/*.' + ext)]
+    else:
+        print("Loading file: %s" % input_file)
+        inputs = [caffe.io.load_image(input_file)]
+
+    print("Classifying %d inputs." % len(inputs))
+
+    # Classify.
+    start = time.time()
+    predictions = classifier.predict(inputs)
+    print("Done in %.2f s." % (time.time() - start))
+
+    # Save
+    #print("Saving results into %s" % args.output_file)
+    #np.save(args.output_file, predictions)
+    if predictions[0][0] > predictions[0][1]:
+        print "it's a boy!"
+    else:
+        print "it's a girl!"
+    print predictions
+    print np.array(inputs).shape
+
+
+the_detector(face_image)
