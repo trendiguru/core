27125b7f23d40f4c47f5bc9bc9d7507f8c625aa7
yonatan <yonatanguy@gmail.com>
2016-04-10 12:03:57 +0300

    add testing file

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -2,8 +2,14 @@
 import caffe
 import numpy as np
 from trendi import background_removal
+from PIL import Image
 
 path = "/home/yonatan/test_set/female/Juljia_Vysotskij_0001.jpg"
+im = Image.open(path)
+print im.size
+
+
+
 base_image = np.array([caffe.io.load_image(path)])
 #print base_image.shape
 face_image = background_removal.find_face_cascade(base_image)
