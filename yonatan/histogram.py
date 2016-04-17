4274d03bd25855c591e73a0c27441aa70b57b008
yonatan <yonatanguy@gmail.com>
2016-04-17 12:21:37 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/histogram.py
+++ yonatan/histogram.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from numpy.random import normal, uniform
 gaussian_numbers = normal(size=1000)
 uniform_numbers = uniform(low=-3, high=3, size=1000)
+fig = plt.figure()
 plt.hist(gaussian_numbers, bins=20, histtype='stepfilled', normed=True, color='b', label='Gaussian')
 plt.hist(uniform_numbers, bins=20, histtype='stepfilled', normed=True, color='r', alpha=0.5, label='Uniform')
 plt.title("Gaussian/Uniform Histogram")
