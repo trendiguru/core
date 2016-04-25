0447cb20b8e2c82d5477e6cafb8779757729fd11
jeremy <jeremy@trendiguru.com>
2016-04-25 23:10:49 +0300

    output filetype like input

--- classifier_stuff/caffe_nns/jrinfer.py
+++ classifier_stuff/caffe_nns/jrinfer.py
@@ -5,10 +5,9 @@ from PIL import Image
 import caffe
 import os
 
-def infer(images,prototxt,caffemodel)
+def infer(images,prototxt,caffemodel):
     # load net
     net = caffe.Net(prototxt,caffemodel, caffe.TEST)
-
     for imagename in images:
         # load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
         im = Image.open(imagename)
@@ -16,7 +15,6 @@ def infer(images,prototxt,caffemodel)
         in_ = in_[:,:,::-1]
         in_ -= np.array((104.00698793,116.66876762,122.67891434))
         in_ = in_.transpose((2,0,1))
-
         # shape for input (data blob is N x C x H x W), set data
         net.blobs['data'].reshape(1, *in_.shape)
         net.blobs['data'].data[...] = in_
@@ -26,7 +24,6 @@ def infer(images,prototxt,caffemodel)
         result = Image.fromarray(out.astype(np.uint8))
         outname = im.strip('.png')[0]+'out.bmp'
         result.save(outname)
-
         fullout = net.blobs['score'].data[0]
 
 
