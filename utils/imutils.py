ae4536bf9d28fce9052106c5fa472464f0022084
jeremy <jeremy@trendiguru.com>
2016-04-27 19:44:45 +0300

    combined map+colorbar

--- utils/imutils.py
+++ utils/imutils.py
@@ -674,15 +674,15 @@ def show_mask_with_labels_dir(dir,filter=None,labels=None,original_images_dir=No
 
 def show_mask_with_labels(mask_filename,labels,original_image=None):
     colormap = cv2.COLORMAP_JET
-    img_arr = Utils.get_cv2_img_array(mask_filename)
+    img_arr = Utils.get_cv2_img_array(mask_filename,cv2.IMREAD_GRAYSCALE)
     if img_arr is None:
         logging.warning('trouble with file '+mask_filename)
         return
-#    img_arr = cv2.imread(mask_filename, cv2.IMREAD_GRAYSCALE)
     print('file:'+mask_filename+',size'+str(img_arr.shape))
     if len(img_arr.shape) != 2:
         logging.warning('got a multichannel image, using chan 0')
         img_arr = img_arr[:,:,0]
+    print('hist'+str(np.histogram(img_arr,bins=57)))
     h,w = img_arr.shape[0:2]
     uniques = np.unique(img_arr)
     print('number of unique mask values:'+str(len(uniques)))
@@ -732,7 +732,6 @@ def show_mask_with_labels(mask_filename,labels,original_image=None):
     combined = np.zeros([h,w+w_colorbar,3])
     combined[:,0:w_colorbar,:]=dest_colorbar
     combined[:,w_colorbar:w_colorbar+w,:]=dest
-    print('hist'+str(np.histogram(combined)))
     cv2.imshow('map',dest)
     cv2.imshow('colorbar',dest_colorbar)
     cv2.imshow('combined',combined)
