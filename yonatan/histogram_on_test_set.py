594145d26ad5d3f425b053c798389110d7fbc946
yonatan <yonatanguy@gmail.com>
2016-04-27 19:50:24 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/histogram_on_test_set.py
+++ yonatan/histogram_on_test_set.py
@@ -26,6 +26,8 @@ counter = 0
 
 for line in text_file:
     counter += 1
+
+    path = line.split()
     #split line to link and label
     words = line.split("/")
 
@@ -34,7 +36,7 @@ for line in text_file:
 
     file_name = words[3].split()
 
-    predictions = gender_detector.genderator(line)
+    predictions = gender_detector.genderator(path)
 
     #if the gender_detector is right
     if (predictions[0][0] > predictions[0][1]) and (file_name[1] == 0):
