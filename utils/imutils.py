b2735b94c9d5fde73732ce7fabab0f2dfa958a01
jeremy <jeremy@trendiguru.com>
2016-04-30 15:46:00 +0300

    combine mask, labelkey, orig....  orig coming in at different res than mask, sheesh

--- utils/imutils.py
+++ utils/imutils.py
@@ -791,7 +791,7 @@ def show_mask_with_labels(mask_filename,labels,original_image=None):
     n_tot = h*w
     frac = float(n_nonzero)/n_tot
     uniques = np.unique(img_arr)
-    print('number of unique mask values:'+str(len(uniques))+' frac nonzero:'+str(frac))
+    print('number of unique mask values:'+str(len(uniques))+' frac nonzero:'+str(frac) +' hxw:'+str(h)+','+str(w))
     if len(uniques)>len(labels):
         logging.warning('number of unique mask values > number of labels!!!')
         return
@@ -823,7 +823,7 @@ def show_mask_with_labels(mask_filename,labels,original_image=None):
         colorbar[i*bar_height:i*bar_height+bar_height,:] = unique
 
 #        cv2.putText(colorbar,labels[unique],(5,i*bar_height+bar_height/2-10),cv2.FONT_HERSHEY_PLAIN,1,[i*255/len(uniques),i*255/len(uniques),100],thickness=2)
-        cv2.putText(colorbar,labels[unique],(5,i*bar_height+bar_height/2-5),cv2.FONT_HERSHEY_PLAIN,1,[50,200,200],thickness=2)
+        cv2.putText(colorbar,labels[unique],(5,i*bar_height+bar_height/2-5),cv2.FONT_HERSHEY_PLAIN,1,[10,200,200],thickness=2)
         i=i+1
 
     scaled_colorbar = np.uint8(np.multiply(colorbar, max_huelevel / maxVal))
