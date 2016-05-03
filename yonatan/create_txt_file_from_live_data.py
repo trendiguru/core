ca6b898ce05a407ef47d615bc6a9494cf014e91c
yonatan <yonatanguy@gmail.com>
2016-05-03 14:16:57 +0300

    changed last layer from Softmaxwithloss to Softmax

---
+++ yonatan/create_txt_file_from_live_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import numpy as np
+import skimage.io
+from scipy.ndimage import zoom
+from skimage.transform import resize
+import os
+import caffe
+from .. import background_removal, Utils, constants
+import cv2
+import sys
+import argparse
+import glob
+import time
+import skimage
+import urllib
+from PIL import Image
+import pymongo; db = constants.db
+
+male_count = db.yonatan_gender.count({'gender': 'Male'})
+female_count = db.yonatan_gender.count({'gender': 'Female'})
+
+male_curser = db.yonatan_gender.find({'gender': 'Male'})
+female_curser = db.yonatan_gender.find({'gender': 'Female'})
+
+
+text_file = open("live_data_set_links.txt", "w")
+
+for m_counter in xrange(male_count):
+    text_file.write(male_curser[m_counter]['url'][0] + ' 0' + male_curser[m_counter]['face'] + '\n')
+    print m_counter
+
+for f_counter in xrange(male_count):
+    text_file.write(female_curser[f_counter]['url'][0] + ' 0' + female_curser[f_counter]['face'] + '\n')
+    print f_counter
+
+text_file.flush()
+
