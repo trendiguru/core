35cda4b1c69882697b82d7685be763b15baae600
yonatan <yonatanguy@gmail.com>
2016-05-04 18:39:49 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/histogram_on_test_set.py
+++ yonatan/histogram_on_test_set.py
@@ -35,7 +35,7 @@ ext = 'jpg'
 
 # Make classifier.
 classifier = caffe.Classifier(MODLE_FILE, PRETRAINED,
-                              image_dims=image_dims, mean=120,
+                              image_dims=image_dims, mean=mean,
                               input_scale=input_scale, raw_scale=raw_scale,
                               channel_swap=channel_swap)
 
