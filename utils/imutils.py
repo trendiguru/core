a9db3fe4c0fcafeb37c5c8e92b941c8925c1a4e3
jeremy <jeremy@trendiguru.com>
2016-05-05 18:43:25 +0300

    no system for showing pd output with labels???

--- utils/imutils.py
+++ utils/imutils.py
@@ -875,9 +875,11 @@ def show_mask_with_labels(mask_filename,labels,original_image=None,cut_the_crap=
 
         #    cv2.imshow('original',orig_arr)
             colorbar_h,colorbar_w = dest_colorbar.shape[0:2]
-            print('dest colorbar w {} h {} shape {}'.format(colorbar_w,colorbar_h,colorbar.shape)
+            print('dest colorbar w {} h {} shape {}'.format(colorbar_w,colorbar_h,colorbar.shape))
             dest_h,dest_w = dest.shape[0:2]
+            print('dest w {} h {} shape {}'.format(dest_w,dest_h,dest.shape))
             orig_h,orig_w = orig_arr.shape[0:2]
+            print('orig w {} h {} shape {}'.format(orig_w,orig_h,orig.shape))
 #            print('colobar size {} masksize {} imsize {}'.format(dest_colorbar.shape,dest.shape,orig_arr.shape))
             combined = np.zeros([dest_h,dest_w+orig_w+colorbar_w,3],dtype=np.uint8)
             combined[:,0:colorbar_w]=dest_colorbar
