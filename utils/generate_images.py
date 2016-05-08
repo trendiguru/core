a5866e8b2cf6bc7bf1d89857e8c75228b73036b8
jeremy <jeremy@trendiguru.com>
2016-05-08 20:55:54 +0300

    #@$) matlab

--- utils/generate_images.py
+++ utils/generate_images.py
@@ -518,8 +518,7 @@ def generate_random_pair_mask_and_image_dir(imgdir,label_dir,max_angle=7,max_off
             for i in range(n_tot):
 #                p.map(generate_random_pair_mask_and_image,imgfiles,args=(imgfile,label_dir,max_angle=max_angle,max_offset_x=max_offset_x,
 #                                                    max_offset_y=max_offset_y,max_scale=max_scale,n_tot=n_tot,labels=labels)
-                p = multiprocessing.Process(target=generate_random_pair_mask_and_image, args=(imgfile,label_dir,max_angle=max_angle,max_offset_x=max_offset_x,
-                                                    max_offset_y=max_offset_y,max_scale=max_scale,n_tot=n_tot,labels=labels))
+                p = multiprocessing.Process(target=generate_random_pair_mask_and_image, args=(imgfile,label_dir,max_angle=max_angle,max_offset_x=max_offset_x,max_offset_y=max_offset_y,max_scale=max_scale,n_tot=n_tot,labels=labels))
 
                 jobs.append(p)
                 p.start()
