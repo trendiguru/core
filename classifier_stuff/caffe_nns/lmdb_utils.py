e0e4d9d7b6b446e27904fc519605feb55fe89fda
jeremy <jeremy@trendiguru.com>
2016-05-09 11:54:30 +0300

    random augments

--- classifier_stuff/caffe_nns/lmdb_utils.py
+++ classifier_stuff/caffe_nns/lmdb_utils.py
@@ -827,14 +827,14 @@ if __name__ == "__main__":
  #   kill_db('testdb.train')
     db_name = 'fcnn_fullsize_allcats'
     image_dir = '/home/jeremy/image_dbs/colorful_fashion_parsing_data/images/test'
-    label_dir = '/home/jeremy/image_dbs/colorful_fashion_parsing_data/labels'
+    label_dir = '/home/jeremy/image_dbs/colorful_fashion_parsing_data/labels_u21'
     image_dbname='/home/jeremy/image_dbs/lmdb/images_u21_test'
     label_dbname='/home/jeremy/image_dbs/lmdb/labels_u21_test'
     label_images_and_images_to_lmdb(image_dbname,label_dbname,image_dir,label_dir,resize_x=None,resize_y=None,avg_B=B,avg_G=G,avg_R=R,
                      use_visual_output=False,imgsuffix='.jpg',labelsuffix='.png',shuffle=False,maxfiles=10)
 
     image_dir = '/home/jeremy/image_dbs/colorful_fashion_parsing_data/images/train'
-    label_dir = '/home/jeremy/image_dbs/colorful_fashion_parsing_data/labels'
+    label_dir = '/home/jeremy/image_dbs/colorful_fashion_parsing_data/labels_u21'
     image_dbname='/home/jeremy/image_dbs/lmdb/images_u21_train'
     label_dbname='/home/jeremy/image_dbs/lmdb/labels_u21_train'
     label_images_and_images_to_lmdb(image_dbname,label_dbname,image_dir,label_dir,resize_x=None,resize_y=None,avg_B=B,avg_G=G,avg_R=R,
