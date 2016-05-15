cc86ba1b6a897305db76aff080d4bbfac548f332
yonatan <yonatanguy@gmail.com>
2016-05-15 15:02:30 +0300

    changed last layer from Softmaxwithloss to Softmax

---
+++ yonatan/go_over_db.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#mini - 0
+#maxi = 1
+
+from .. import constants
+import pymongo
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+db = constants.db
+
+mini_dresses = db.mini
+maxi_dresses = db.maxi
+
+text_file = open("mini_maxi_1800_dresses.txt", "w")
+
+for mini_dress in mini_dresses.find():
+    text_file.write(mini_dress({'image_url'}) + ' 0' + '\n')
+
+
+
+text_file.flush()
