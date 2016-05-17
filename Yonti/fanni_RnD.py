3c83f05cf1d37a5ad36822a46ff5cbb848b6487b
yonti <yontilevin@gmail.com>
2016-05-17 15:32:39 +0300

    fanni

---
+++ Yonti/fanni_RnD.py
@@ -0,0 +1,25 @@
+"""
+function fot testing fanni
+"""
+from .. import constants
+
+db = constants.db
+
+def create_test_collection(name, amount):
+    img_list = db.images.find({'num_of_people': 1})
+    collection = db[name]
+    collection.drop_collection()
+
+    count=0
+    for img in img_list:
+        if img['image_urls'][0][0:27] == 'http://www.fashionseoul.com':
+            for item in img['people'][1]['items']:
+                if item['catergory']=='dress':
+                    dict = {'img_url':item['image_urls'][0],
+                            'category': 'dress',
+                            'fp': item['fp']}
+                    count+=1
+                    collection.insert_one(dict)
+                    break
+        if count > amount:
+            break
