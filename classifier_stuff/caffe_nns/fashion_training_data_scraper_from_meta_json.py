d064a614e82ddd664e075436152b12ca956d4000
jeremy <jeremy@trendiguru.com>
2016-05-17 14:46:31 +0300

    multiclass labels

--- classifier_stuff/caffe_nns/fashion_training_data_scraper_from_meta_json.py
+++ classifier_stuff/caffe_nns/fashion_training_data_scraper_from_meta_json.py
@@ -344,7 +344,8 @@ def multi_class_labels_from_bbfiles(dir_of_bbfiles):
         print(str(len(files))+' files in '+dir_of_bbfiles)
         for a_file in files:
             outvec = np.zeros(len(constants.ultimate_21))
-            for line in a_file:
+            with open(a_file,'r') as lines:
+            for line in lines:
                 print('file:'+a_file+' line:'+str(line))
                 line_arr = line.split()
                 berg_class = int(line_arr[0])
