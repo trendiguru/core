43873000513027f5763b6bc6f8cff3bcd319d62d
yonti <yontilevin@gmail.com>
2016-05-25 14:49:53 +0300

    hashing

--- Yonti/hash_worker.py
+++ Yonti/hash_worker.py
@@ -7,10 +7,10 @@ from .. import Utils, constants
 import sys
 db = constants.db
 
-def get_hash(collection_name,item_count,item_id,url):
+def get_hash(collection_name,item_count,item_id,item_url):
     collection = db[collection_name]
 
-    image = Utils.get_cv2_img_array(url)
+    image = Utils.get_cv2_img_array(item_url)
     if image is None:
         print('bad image!')
         collection.delete_one({"_id":item_id})
