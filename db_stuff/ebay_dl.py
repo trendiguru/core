6862cf9146236bf96b3ae70dd6328e4e3052715f
yonti <yontilevin@gmail.com>
2016-05-25 19:18:44 +0300

    ebay

--- db_stuff/ebay_dl.py
+++ db_stuff/ebay_dl.py
@@ -52,7 +52,7 @@ def StoreInfo(ftp, files):
     last_modified =filter(lambda store: store['name'] ==  fullname, files)
     item = {'type':'store','id': store_id,'name': split2[2][7:-2],'link':split2[4][7:-2],
             'dl_duration':0,'items_downloaded':0, 'B/W': 'black','status':status,
-            'modified': last_modified[0]["last_modified"]}
+            'modified': last_modified["last_modified"]}
     db.ebay_download_info.insert_one(item)
     for line in split[1:]:
         split2 = re.split("<name><!|></name>|<url><!|></url>",  line)
@@ -61,7 +61,7 @@ def StoreInfo(ftp, files):
         last_modified = filter(lambda store: store['name'] == fullname, files)
         item = {'type':'store','id': store_id, 'name': split2[1][7:-2], 'link':split2[3][7:-2],
                 'dl_duration':0,'items_downloaded':0, 'B/W': 'black','status':status,
-                'modified': last_modified[0]["last_modified"]}
+                'modified': last_modified["last_modified"]}
         db.ebay_download_info.insert_one(item)
     # files = filter(lambda x: x.get('name') == "status.txt",files)
     # files = filter(lambda x: x.get('name') == "StoreInformation.xml",files)
