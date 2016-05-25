feb0c38926bc7e5c952399928078ef6308cb5a6e
yonti <yontilevin@gmail.com>
2016-05-25 14:36:20 +0300

    hashing

--- Yonti/hashAway.py
+++ Yonti/hashAway.py
@@ -20,6 +20,7 @@ def hashCollection(collection_name):
     collection = db[collection_name]
     items = collection.find({"img_hash":{"exists":0}},{'images.XLarge':1})
     total_count = items.count()
+    print (total_count)
     for x, item in enumerate(items):
         print (x)
         q.enqueue(hash_worker.get_hash, collection_name=collection_name, item_count = x, item_id = item["_id"],
