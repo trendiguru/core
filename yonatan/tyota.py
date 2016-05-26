40d605fc8e3bb88d11109ade4121ae7cb2e2fc32
yonatan <yonatanguy@gmail.com>
2016-05-26 15:45:08 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/tyota.py
+++ yonatan/tyota.py
@@ -9,37 +9,37 @@ import matplotlib.pyplot as plt
 dresses = {'mini', 'midi', 'maxi'}
 sets = {'train', 'cv', 'test'}
 
-dresses_train_set_from_each_catagory = 750
-dresses_cv_set_from_each_catagory = 150
-dresses_test_set_from_each_catagory = 150
-
-counter_train = 0
-counter_cv = 0
-counter_test = 0
+counter = 0
+counter_train = 750
+counter_cv = 150
+counter_test = 150
 
 for dress in dresses:
-
     source_dir = '/home/yonatan/resized_' + dress + '_dresses'
 
-    for set in sets:
+    counter = 0
 
-        destination_dir = '/home/yonatan/dresses_' + set + '_set'
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
 
-        for root, dirs, files in os.walk(source_dir):
-            for file in files:
+            old_file_location = source_dir + '/' + file
 
-                old_file_location = source_dir + '/' + file
-                new_file_location = destination_dir + '/' + file
+            if counter < counter_train:
+                new_file_location = '/home/yonatan/dresses_train_set/' + file
                 os.rename(old_file_location, new_file_location)
-
-                if set == 'train':
-                    counter_train += 1
-                    print counter_train
-                elif set == 'cv':
-                    counter_cv += 1
-                    print counter_cv
-                elif set == 'test':
-                    counter_test += 1
-                    print counter_test
+                counter_train += 1
+                print counter_train
+            elif counter >= counter_train and counter < counter_train + counter_cv:
+                new_file_location = '/home/yonatan/dresses_cv_set/' + file
+                os.rename(old_file_location, new_file_location)
+                counter_cv += 1
+                print counter_cv
+            elif counter >= counter_train + counter_cv and counter < counter_train + counter_cv + counter_test:
+                new_file_location = '/home/yonatan/dresses_test_set/' + file
+                os.rename(old_file_location, new_file_location)
+                counter_test += 1
+                print counter_test
+            else:
+                break
 
 print 'counter_mini = {0}, counter_midi = {1}, counter_maxi = {2}'.format(counter_train, counter_cv, counter_test)
