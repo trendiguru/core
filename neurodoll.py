4236d54e7d8b47ed868c0253bbe9552838ce0c72
jeremy <jeremy@trendiguru.com>
2016-05-30 20:25:57 +0300

    results out door

--- neurodoll.py
+++ neurodoll.py
@@ -117,6 +117,7 @@ def infer_one(url_or_np_array,net,required_image_size=(256,256)):
 #    im = im.resize(required_imagesize,Image.ANTIALIAS)
 
     in_ = image.astype(float)
+    in_ = in_.astype(float)
     in_ = imutils.resize_keep_aspect(in_,output_size=required_image_size,output_file=None)
     if len(in_.shape) != 3:
         print('got 1-chan image, skipping')
