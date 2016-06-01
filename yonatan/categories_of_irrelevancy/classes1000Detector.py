62ecaa34db1719eda22b77650c189e4cef67f468
yonatan <yonatanguy@gmail.com>
2016-06-01 15:49:35 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/categories_of_irrelevancy/classes1000Detector.py
+++ yonatan/categories_of_irrelevancy/classes1000Detector.py
@@ -18,7 +18,8 @@ import skimage
 MODLE_FILE = "/home/yonatan/trendi/yonatan/categories_of_irrelevancy/Alexnet_1000_deploy.prototxt"
 PRETRAINED = "/home/yonatan/bvlc_alexnet.caffemodel"
 caffe.set_mode_gpu()
-image_dims = [227, 227]
+#image_dims = [227, 227]
+image_dims = None
 mean, input_scale = np.array([120, 120, 120]), None
 channel_swap = [2, 1, 0]
 raw_scale = 255.0
@@ -1064,9 +1065,9 @@ def theDetector(url_or_np_array):
     else:
         return None
 
-    #resized_image = cv2.resize(full_image, (227, 227))
+    resized_image = cv2.resize(full_image, (227, 227))
 
-    img_for_caffe = [cv2_image_to_caffe(full_image)]
+    img_for_caffe = [cv2_image_to_caffe(resized_image)]
     #face_for_caffe = [caffe.io.load_image(face_image)]
 
     if img_for_caffe is None:
