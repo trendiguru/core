203350eabef91102c341de52597f8a072ef6cd6d
yonti <yontilevin@gmail.com>
2016-06-13 10:10:58 +0300

    ebay

--- db_stuff/dl_status.py
+++ db_stuff/dl_status.py
@@ -90,7 +90,7 @@ def checkStatus():
     workbook = xlsxwriter.Workbook(path2file)
     bold = workbook.add_format({'bold': True})
 
-    lasts_days_info = dl_status.find({'date':{'$gte':last2weeks}}).sort({'_id':-1})
+    lasts_days_info = dl_status.find({'date':{'$gte':last2weeks}}).sort({'_id': -1})
     todays = workbook.add_worksheet('today')
     for daily_info in lasts_days_info:
         dl_date = daily_info['date']
