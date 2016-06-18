4809a43cec673d8584e42640afb001aac3c3d1bd
jeremy <jeremy@trendiguru.com>
2016-06-18 15:04:50 +0300

    sort pd output

--- db_stuff/choose_good_parses.py
+++ db_stuff/choose_good_parses.py
@@ -1,14 +1,19 @@
 __author__ = 'jeremy'
 import os
 import shutil
+import time
+import random
 
 from trendi.utils import imutils
 from trendi.constants import fashionista_categories
 from trendi import Utils
 
 def choose_good_parses(dir):
+    n_done = 0
     parse_list = [f for f in os.listdir(dir) if '.bmp' in f]
+    random.shuffle(parse_list)
     img_list = []
+    start_time = time.time()
     for parse in parse_list:
         full_parsename = os.path.join(dir,parse)
         img_filename = full_parsename[:-4]+'.jpg'
@@ -19,6 +24,8 @@ def choose_good_parses(dir):
             move_the_rest(posename,k)
             urlname = full_parsename[:-4]+'.url'
             move_the_rest(urlname,k)
+            n_done += 1
+            print('n_done {} pics/minute {}'.format(n_done,(time.time()-start_time)/n_done))
         else:
             print(img_filename+' doesnt exists')
 
