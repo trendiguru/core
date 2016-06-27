3e2b4e787807a1a6c0dc5c649eb13a705a50eb28
yonti <yontilevin@gmail.com>
2016-06-27 09:22:20 +0300

    ebay

--- db_stuff/ebay_API_worker.py
+++ db_stuff/ebay_API_worker.py
@@ -21,7 +21,7 @@ def log2file(log_filename):
     return logger
 
 
-def GET_call(GEO, gender, sub_attribute, price_bottom=0, price_top=10000, page=1, num=1):
+def GET_call(GEO, gender, sub_attribute, price_bottom=0, price_top=10000, page=1, num=100):
     account_info = ebay_account_info[GEO]
     gender_attribute = ebay_gender[gender]
     price_attribute = 'price_range_' + str(price_bottom) + '_' + str(price_top)
@@ -29,8 +29,6 @@ def GET_call(GEO, gender, sub_attribute, price_bottom=0, price_top=10000, page=1
                 'apiKey='+account_info['API_Key'] + \
                 '&visitorUserAgent=""' \
                 '&visitorIPAddress=""' \
-                'showProductOffers = true' \
-                '&numOffersPerProduct=1' \
                 '&trackingId='+account_info['Tracking_ID'] + \
                 '&categoryId=31515' \
                 '&pageNumber=' + str(page) + \
@@ -38,6 +36,8 @@ def GET_call(GEO, gender, sub_attribute, price_bottom=0, price_top=10000, page=1
                 '&attributeValue=' + gender_attribute + \
                 '&attributeValue=' + sub_attribute + \
                 '&attributeValue=' + price_attribute
+    # 'showProductOffers = true' \
+    # '&numOffersPerProduct=1' \
 
     res = requests.get(api_call)
 
@@ -129,9 +129,15 @@ def process_items(items, gender,GEO , sub_attribute):
         sku = offer['sku']
         id_exists = collection.find_one({'id': itemId})
         sku_exists = collection.find_one({'sku': sku})
-        if id_exists or sku_exists:
+        if id_exists :
             #TODO: add checks
-            # print ('item already exists')
+            print ('ID ID ID ID')
+            continue
+
+
+        if sku_exists:
+            # TODO: add checks
+            print ('SKU SKU SKU')
             continue
 
         price = {'price': offer['basePrice']['value'],
@@ -148,8 +154,20 @@ def process_items(items, gender,GEO , sub_attribute):
         take highest res img
         '''
         img_url = img_list[-1]
+
+        if 'description' in offer.keys():
+            desc = offer['description']
+        else:
+            desc = ""
+
+        if 'shippingCost' in offer.keys():
+            shipping = offer['shippingCost']
+        else:
+            shipping = ""
+
         success, category = name2category(gender, offer['name'], sub_attribute)
         if not success:
+            print ('NOT SUCCESS NOT SUCCESS')
             continue
 
         generic = {"id": [itemId],
@@ -158,7 +176,7 @@ def process_items(items, gender,GEO , sub_attribute):
                    "images": {"XLarge": img_url},
                    "status": status,
                    "shortDescription": offer['name'],
-                   "longDescription": offer['description'],
+                   "longDescription": desc,
                    "price": price,
                    "brand": offer['store']['name'],
                    "download_data": {'dl_version': today_date,
@@ -166,7 +184,7 @@ def process_items(items, gender,GEO , sub_attribute):
                                      'fp_version': fingerprint_version},
                    "fingerprint": None,
                    "gender": gender,
-                   # "shippingInfo": offer['shippingCost'],
+                   "shippingInfo": shipping,
                    "raw_info": offer}
 
         # image = Utils.get_cv2_img_array(img_url)
@@ -190,8 +208,9 @@ def process_items(items, gender,GEO , sub_attribute):
 
 def downloader(GEO, gender, sub_attribute, price_bottom=0, price_top=10000):
     start_time = time()
+    sleep(1)
     success, item_count, items = \
-        GET_call(GEO, gender, sub_attribute, price_bottom, price_top, num=100)
+        GET_call(GEO, gender, sub_attribute, price_bottom, price_top, num=1)
 
     if not success:
         print ('GET failed')
@@ -204,27 +223,29 @@ def downloader(GEO, gender, sub_attribute, price_bottom=0, price_top=10000):
 
     if item_count > 1499 and price_top > price_bottom:
         middle = int((price_top+price_bottom)/2)
-        if middle > price_bottom:
+        if middle >= price_bottom:
             q.enqueue(downloader, args=(GEO, gender, sub_attribute, price_bottom, middle), timeout=5400)
-        if price_top > middle:
+        if price_top >= middle:
             q.enqueue(downloader, args=(GEO, gender, sub_attribute, middle, price_top), timeout=5400)
+        print ('price range %d to %d divided to %d - %d  &  %d - %d'
+               % (price_bottom, price_top, price_bottom, middle, middle, price_top))
         return
 
     end_page = item_count/100 +2
     if end_page == 17:
         end_page = 16
     new_items = total_items = 0
-    new_inserts, total = process_items(items, gender, GEO, sub_attribute)
-    new_items += new_inserts
-    total_items += total
-    if end_page > 2:
-        for i in range(2, end_page):
-            success, item_count, items = GET_call(GEO, gender, sub_attribute, price_bottom, price_top, page=i, num=100)
-            if not success:
-                continue
-            new_inserts, total = process_items(items, gender, GEO, sub_attribute)
-            new_items += new_inserts
-            total_items += total
+    # new_inserts, total = process_items(items, gender, GEO, sub_attribute)
+    # new_items += new_inserts
+    # total_items += total
+    for i in range(1, end_page):
+        sleep(1)
+        success, item_count, items = GET_call(GEO, gender, sub_attribute, price_bottom, price_top, page=i, num=100)
+        if not success:
+            continue
+        new_inserts, total = process_items(items, gender, GEO, sub_attribute)
+        new_items += new_inserts
+        total_items += total
     end_time = time()
     logger = log2file('/home/developer/yonti/ebay_'+gender+'_download_stats.log')
     summery = 'attribute: %s_%s ,price: %d to %d , item Count: %d, new: %d, download_time: %d' \
