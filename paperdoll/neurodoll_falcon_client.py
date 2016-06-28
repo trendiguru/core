576f693b085e5aa0e3dc94fb3d33cd716590a92d
Lior Sabag <lior@trendiguru.com>
2016-06-28 16:02:03 +0300

    Add ability to get grayscale mask for single category

--- paperdoll/neurodoll_falcon_client.py
+++ paperdoll/neurodoll_falcon_client.py
@@ -5,7 +5,7 @@ import requests
 CLASSIFIER_ADDRESS = "http://37.58.101.173:8080/nd"
 
 
-def pd(image_array_or_url):
+def pd(image_array_or_url, category_index):
     data = msgpack.dumps({"image": image_array_or_url})
-    resp = requests.post(CLASSIFIER_ADDRESS, data)
+    resp = requests.post(CLASSIFIER_ADDRESS, data=data, params={"categoryIndex": category_index})
     return msgpack.loads(resp.content)
