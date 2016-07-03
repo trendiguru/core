2e63df554e0927d068a1ebac1962aae1a54798ed
jeremy <jeremy@trendiguru.com>
2016-07-03 15:48:57 +0100

    more tb images

--- classifier_stuff/caffe_nns/fashion_training_data_scraper_from_meta_json.py
+++ classifier_stuff/caffe_nns/fashion_training_data_scraper_from_meta_json.py
@@ -54,6 +54,8 @@ def dl_photos(photosfile,images_savedir,start_from=0):
     listings = get_product_photos(photosfile)
     max_items = 5000000
     Utils.ensure_dir(images_savedir)
+    num_cores = multiprocessing.cpu_count()
+
     Parallel(n_jobs=num_cores)(delayed(get_file)(listings[i],i+1,images_savedir) for i in range(start_from,len(listings)))
 
 #    for i in range(len(listings)):
