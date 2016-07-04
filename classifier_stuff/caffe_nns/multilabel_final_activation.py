81970789b5cbd78de0b17a7fd1cfc2203c150e7e
jeremy <jeremy@trendiguru.com>
2016-07-04 21:01:03 +0300

    vary decision thresh in multilabel, full report

---
+++ classifier_stuff/caffe_nns/multilabel_final_activation.py
@@ -0,0 +1,125 @@
+############
+#get activations from final layer(s) of multilabel model
+############
+
+import caffe
+import time
+
+def multilabel_get_final_activation(url_or_np_array):
+    image_mean = np.array([107.0,117.0,123.0])
+    input_scale = None
+    channel_swap = [2, 1, 0]
+    raw_scale = 255.0
+    print('loading caffemodel for neurodoll (single class layers)')
+
+    start_time = time.time()
+    if isinstance(url_or_np_array, basestring):
+        print('infer_one working on url:'+url_or_np_array)
+        image = url_to_image(url_or_np_array)
+    elif type(url_or_np_array) == np.ndarray:
+        image = url_or_np_array
+# load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+#    im = Image.open(imagename)
+#    im = im.resize(required_imagesize,Image.ANTIALIAS)
+#    in_ = in_.astype(float)
+
+    in_ = imutils.resize_keep_aspect(image,output_size=required_image_size,output_file=None)
+    in_ = np.array(in_, dtype=np.float32)   #.astype(float)
+    if len(in_.shape) != 3:  #h x w x channels, will be 2 if only h x w
+        print('got 1-chan image, turning into 3 channel')
+        #DEBUG THIS , ORDER MAY BE WRONG [what order? what was i thinking???]
+        in_ = np.array([in_,in_,in_])
+    elif in_.shape[2] != 3:  #for rgb/bgr, some imgages have 4 chan for alpha i guess
+        print('got n-chan image, skipping - shape:'+str(in_.shape))
+        return
+#    in_ = in_[:,:,::-1]  for doing RGB -> BGR
+#    cv2.imshow('test',in_)
+    in_ -= np.array((104,116,122.0))
+    in_ = in_.transpose((2,0,1))
+    # shape for input (data blob is N x C x H x W), set data
+    net.blobs['data'].reshape(1, *in_.shape)
+    net.blobs['data'].data[...] = in_
+    # run net and take argmax for prediction
+    net.forward()
+#    out = net.blobs['score'].data[0].argmax(axis=0) #for a parse with per-pixel max
+    out = net.blobs['my_fc7'].data[0] #for the nth class layer #siggy is after sigmoid
+    print('shape of my_fc7:'+str(out.shape))
+    min = np.min(out)
+    max = np.max(out)
+    print('min {} max {} out shape {}'.format(min,max,out.shape))
+    elapsed_time=time.time()-start_time
+    print('infer_one elapsed time:'+str(elapsed_time))
+    return out
+
+
+def multilabel_infer_one(url):
+    image_mean = np.array([104.0,117.0,123.0])
+    input_scale = None
+    channel_swap = [2, 1, 0]
+    raw_scale = 255.0
+    print('loading caffemodel for neurodoll (single class layers)')
+
+    start_time = time.time()
+    if isinstance(url_or_np_array, basestring):
+        print('infer_one working on url:'+url_or_np_array)
+        image = url_to_image(url_or_np_array)
+    elif type(url_or_np_array) == np.ndarray:
+        image = url_or_np_array
+# load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+#    im = Image.open(imagename)
+#    im = im.resize(required_imagesize,Image.ANTIALIAS)
+#    in_ = in_.astype(float)
+    in_ = imutils.resize_keep_aspect(image,output_size=required_image_size,output_file=None)
+    in_ = np.array(in_, dtype=np.float32)   #.astype(float)
+    if len(in_.shape) != 3:  #h x w x channels, will be 2 if only h x w
+        print('got 1-chan image, turning into 3 channel')
+        #DEBUG THIS , ORDER MAY BE WRONG [what order? what was i thinking???]
+        in_ = np.array([in_,in_,in_])
+    elif in_.shape[2] != 3:  #for rgb/bgr, some imgages have 4 chan for alpha i guess
+        print('got n-chan image, skipping - shape:'+str(in_.shape))
+        return
+#    in_ = in_[:,:,::-1]  for doing RGB -> BGR
+#    cv2.imshow('test',in_)
+    in_ -= np.array((104,116,122.0))
+    in_ = in_.transpose((2,0,1))
+    # shape for input (data blob is N x C x H x W), set data
+    net.blobs['data'].reshape(1, *in_.shape)
+    net.blobs['data'].data[...] = in_
+    # run net and take argmax for prediction
+    net.forward()
+#    out = net.blobs['score'].data[0].argmax(axis=0) #for a parse with per-pixel max
+    out = net.blobs['siggy'].data[0][category_index] #for the nth class layer #siggy is after sigmoid
+    min = np.min(out)
+    max = np.max(out)
+    print('min {} max {} out shape {}'.format(min,max,out.shape))
+    out = out*255
+    min = np.min(out)
+    max = np.max(out)
+    print('min {} max {} out after scaling  {}'.format(min,max,out.shape))
+    result = Image.fromarray(out.astype(np.uint8))
+#        outname = im.strip('.png')[0]+'out.bmp'
+#    outname = os.path.basename(imagename)
+#    outname = outname.split('.jpg')[0]+'.bmp'
+#    outname = os.path.join(out_dir,outname)
+#    print('outname:'+outname)
+#    result.save(outname)
+    #        fullout = net.blobs['score'].data[0]
+    elapsed_time=time.time()-start_time
+    print('infer_one elapsed time:'+str(elapsed_time))
+ #   cv2.imshow('out',out.astype(np.uint8))
+ #   cv2.waitKey(0)
+    return out.astype(np.uint8)
+
+
+caffemodel = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_240000.caffemodel'
+solverproto = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/solver.prototxt'
+net = caffe.Net(solverproto,caffemodel, caffe.TEST)
+required_image_size = (227, 227)
+caffe.set_mode_gpu()
+caffe.set_device(0)
+
+
+if __name__ == "__main__":
+    url = 'http://diamondfilms.com.au/wp-content/uploads/2014/08/Fashion-Photography-Sydney-1.jpg'
+    result = multilabel_get_final_activation(url)
+    print('result:'+str(result))
