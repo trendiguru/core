636f55b74b3e7619fae1c32e35cf1de99db5dcf4
yonatan <yonatanguy@gmail.com>
2016-07-05 12:42:43 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/preparing_dresses_txt_file.py
+++ yonatan/preparing_dresses_txt_file.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 sets = {'train', 'cv', 'test'}
 
-train_text_file = open("dresses_train.txt", "w")
-cv_text_file = open("dresses_cv.txt", "w")
-test_text_file = open("dresses_test.txt", "w")
+train_text_file = open("db_dresses_train.txt", "w")
+cv_text_file = open("db_dresses_cv.txt", "w")
+test_text_file = open("db_dresses_test.txt", "w")
 
 counter = 0
 
@@ -19,21 +19,59 @@ for set in sets:
     for root, dirs, files in os.walk(dir_path):
         for file in files:
 
-            if 'mini' in file:
+            sleeve_dress = {'strapless', 'spaghetti_straps', 'straps', 'sleeveless',
+                            'cap_sleeve', 'short_sleeve', 'midi_sleeve', 'long_sleeve'}
+
+            if 'strapless' in file:
                 if set == 'train':
                     train_text_file.write(root + "/" + file + " 0\n")
                 elif set == 'cv':
                     cv_text_file.write(root + "/" + file + " 0\n")
                 elif set == 'test':
                     test_text_file.write(root + "/" + file + " 0\n")
-            elif 'midi' in file:
+            elif 'spaghetti_straps' in file:
                 if set == 'train':
                     train_text_file.write(root + "/" + file + " 1\n")
                 elif set == 'cv':
                     cv_text_file.write(root + "/" + file + " 1\n")
                 elif set == 'test':
                     test_text_file.write(root + "/" + file + " 1\n")
-            elif 'maxi' in file:
+            elif 'straps' in file:
+                if set == 'train':
+                    train_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'cv':
+                    cv_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'test':
+                    test_text_file.write(root + "/" + file + " 2\n")
+            elif 'sleeveless' in file:
+                if set == 'train':
+                    train_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'cv':
+                    cv_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'test':
+                    test_text_file.write(root + "/" + file + " 2\n")
+            elif 'cap_sleeve' in file:
+                if set == 'train':
+                    train_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'cv':
+                    cv_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'test':
+                    test_text_file.write(root + "/" + file + " 2\n")
+            elif 'short_sleeve' in file:
+                if set == 'train':
+                    train_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'cv':
+                    cv_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'test':
+                    test_text_file.write(root + "/" + file + " 2\n")
+            elif 'midi_sleeve' in file:
+                if set == 'train':
+                    train_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'cv':
+                    cv_text_file.write(root + "/" + file + " 2\n")
+                elif set == 'test':
+                    test_text_file.write(root + "/" + file + " 2\n")
+            elif 'long_sleeve' in file:
                 if set == 'train':
                     train_text_file.write(root + "/" + file + " 2\n")
                 elif set == 'cv':
