1ac827ba81559c20228ef4d6aa1656a71fcfc85f
yonatan <yonatanguy@gmail.com>
2016-07-07 15:59:41 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/devide_data_to_train_cv_test.py
+++ yonatan/devide_data_to_train_cv_test.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 sleeve_dress = {'strapless', 'spaghetti_straps', 'straps', 'sleeveless',
                 'cap_sleeve', 'short_sleeve', 'midi_sleeve', 'long_sleeve'}
 
-#sets = {'train', 'cv', 'test'}
-
 counter = 0
 counter_train = 640
 counter_cv = 130
@@ -22,6 +20,9 @@ for dress in sleeve_dress:
     counter = 0
 
     for root, dirs, files in os.walk(source_dir):
+        file_count = len(files)
+        print file_count
+        '''
         for file in files:
 
             old_file_location = source_dir + '/' + file
@@ -43,3 +44,4 @@ for dress in sleeve_dress:
                 break
 
 print 'counter_train = {0}, counter_cv = {1}, counter_test = {2}, counter = {3}'.format(counter_train, counter_cv, counter_test, counter)
+'''
