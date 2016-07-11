d23b1d948f9fc9128e5a024d9c94a714c7dcb6db
jeremy <jeremy@trendiguru.com>
2016-07-11 15:03:00 +0300

    add threshold to neurodoll

--- classifier_stuff/caffe_nns/multilabel.py
+++ classifier_stuff/caffe_nns/multilabel.py
@@ -7,13 +7,14 @@ import os
 import numpy as np
 import os.path as osp
 import matplotlib.pyplot as plt
-
+import urllib2,urllib
 from copy import copy
 import caffe # If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
 
 from caffe import layers as L, params as P # Shortcuts to define the net prototxt.
 
 from trendi import constants
+from trendi.utils import imutils
 
 # matplotlib inline
 def setup():
@@ -249,21 +250,63 @@ def check_accuracy(solverproto,caffemodel,num_batches=200,batch_size=1,threshold
     precision,recall,accuracy,tp,tn,fp,fn = check_acc(solver.test_nets[0], num_batches=num_batches,batch_size = batch_size, threshold=threshold)
     return precision,recall,accuracy,tp,tn,fp,fn
 
+def url_to_image(url):
+    # download the image, convert it to a NumPy array, and then read
+    # it into OpenCV format
 
-def get_multilabel_output():
+    if url.count('jpg') > 1:
+        return None
+
+    resp = urllib.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    if image.size == 0:
+        return None
+    new_image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    # return the image
+    return new_image
+
+def get_multilabel_output(url_or_np_array,required_image_size=(227,227)):
     caffemodel =  '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_340000.caffemodel'
     deployproto = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/deploy.prototxt'
-
     caffe.set_mode_gpu()
     caffe.set_device(0)
     net = caffe.Net(deployproto,caffemodel, caffe.TEST)
 
+    if isinstance(url_or_np_array, basestring):
+        print('infer_one working on url:'+url_or_np_array)
+        image = url_to_image(url_or_np_array)
+    elif type(url_or_np_array) == np.ndarray:
+        image = url_or_np_array
+# load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+#    im = Image.open(imagename)
+#    im = im.resize(required_imagesize,Image.ANTIALIAS)
+#    in_ = in_.astype(float)
+    in_ = imutils.resize_keep_aspect(image,output_size=required_image_size,output_file=None)
+    in_ = np.array(in_, dtype=np.float32)   #.astype(float)
+    if len(in_.shape) != 3:  #h x w x channels, will be 2 if only h x w
+        print('got 1-chan image, turning into 3 channel')
+        #DEBUG THIS , ORDER MAY BE WRONG [what order? what was i thinking???]
+        in_ = np.array([in_,in_,in_])
+    elif in_.shape[2] != 3:  #for rgb/bgr, some imgages have 4 chan for alpha i guess
+        print('got n-chan image, skipping - shape:'+str(in_.shape))
+        return
+#    in_ = in_[:,:,::-1]  for doing RGB -> BGR
+#    cv2.imshow('test',in_)
+    in_ -= np.array((104,116,122.0))
+    in_ = in_.transpose((2,0,1))
+    # shape for input (data blob is N x C x H x W), set data
     net.blobs['data'].reshape(1, *in_.shape)
     net.blobs['data'].data[...] = in_
     # run net and take argmax for prediction
     net.forward()
 #    out = net.blobs['score'].data[0].argmax(axis=0) #for a parse with per-pixel max
-    out = net.blobs['siggy'].data[0][category_index] #for the nth class layer #siggy is after sigmoid
+    out = net.blobs['score'].data[0] #for the nth class layer #siggy is after sigmoid
+    min = np.min(out)
+    max = np.max(out)
+    print('out  {}'.format(out))
+
+
 
 
 if __name__ =="__main__":
