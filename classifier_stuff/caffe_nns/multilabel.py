3f8ab70ae93b842451bcded391f5211c20ae387d
jeremy <jeremy@trendiguru.com>
2016-07-11 14:22:33 +0300

    add threshold to neurodoll

--- classifier_stuff/caffe_nns/multilabel.py
+++ classifier_stuff/caffe_nns/multilabel.py
@@ -250,6 +250,22 @@ def check_accuracy(solverproto,caffemodel,num_batches=200,batch_size=1,threshold
     return precision,recall,accuracy,tp,tn,fp,fn
 
 
+def get_multilabel_output():
+    caffemodel =  '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_340000.caffemodel'
+    deployproto = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/deploy.prototxt'
+
+    caffe.set_mode_gpu()
+    caffe.set_device(0)
+    net = caffe.Net(deployproto,caffemodel, caffe.TEST)
+
+    net.blobs['data'].reshape(1, *in_.shape)
+    net.blobs['data'].data[...] = in_
+    # run net and take argmax for prediction
+    net.forward()
+#    out = net.blobs['score'].data[0].argmax(axis=0) #for a parse with per-pixel max
+    out = net.blobs['siggy'].data[0][category_index] #for the nth class layer #siggy is after sigmoid
+
+
 if __name__ =="__main__":
     #TODO dont use solver to get inferences , no need for solver for that
     caffe.set_mode_gpu()
@@ -260,7 +276,7 @@ if __name__ =="__main__":
     caffemodel =  '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_240000.caffemodel'
     caffemodel =  '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_340000.caffemodel'
     solverproto = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/solver.prototxt'
-    for t in [0.5,0.6,0.7,0.8,0.85,0.9,0.92,0.95,0.98]:
+    for t in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.92,0.95,0.98]:
         p,r,a,tp,tn,fp,fn = check_accuracy(solverproto,caffemodel,threshold=t,num_batches=1000)
         with open('multilabel_accuracy_results.txt','a') as f:
             f.write('vgg_ilsvrc16_multilabel_2, threshold = '+str(t)+'\n')
