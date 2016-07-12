39a1dd5cfdfe70cd3ad9a7a615365fdf91cc1977
Nadav <nadav@trendiguru.com>
2016-07-12 17:07:47 +0300

    without pooling

--- falcon/sync_images.py
+++ falcon/sync_images.py
@@ -53,7 +53,8 @@ class Images(object):
                         relevancy_dict[url] = green.value
                     else:
                         images_to_rel_check.append(url)
-                print "POST: after deviation: {0}, {1}".format(time.time()-start, exists)
+                print "POST: after deviation: {0}, {1}".format(time.time()-start)
+                print "POST: rel_dict: {0}\n images to check: {1}".format(relevancy_dict, images_to_rel_check)
 
                 # RELEVANCY CHECK POOLING
                 # check_relevancy_partial = partial(fast_results.check_if_relevant_and_enqueue, page_url=page_url)
