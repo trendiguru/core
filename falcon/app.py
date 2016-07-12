181da9af03b59e5b5066307e62a7f19b6f895d3a
Nadav <nadav@trendiguru.com>
2016-07-12 17:00:10 +0300

    without pooling

--- falcon/app.py
+++ falcon/app.py
@@ -2,15 +2,17 @@ import falcon
 from falcon_cors import CORS
 from . import sync_images
 from .sample import QuoteResource
-from multiprocessing import Pool
+# from multiprocessing import Pool
 
-process_pool = Pool(10)
+# process_pool = Pool(10)
 
 cors = CORS(allow_all_headers=True, allow_all_origins=True, allow_all_methods=True)
 
 api = falcon.API(middleware=[cors.middleware])
 
-images = sync_images.Images(process_pool)
+# images = sync_images.Images(process_pool)
+
+images = sync_images.Images()
 
 api.add_route('/images', images)
 api.add_route('/quote', QuoteResource())
