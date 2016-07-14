314e2f5c5bf423d4a8e97956e942ba563e6cf39c
yonatan <yonatanguy@gmail.com>
2016-07-14 14:24:53 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/preparing_data_from_db_dress_sleeve.py
+++ yonatan/preparing_data_from_db_dress_sleeve.py
@@ -19,6 +19,7 @@ from PIL import Image
 import pymongo
 import argparse
 import shutil
+import yonatan_constants
 
 
 def cv2_image_to_caffe(image):
@@ -55,70 +56,23 @@ def preparing_data_from_db(argv):
 
     # dress sleeve #
     if args.input_file == 'dress_sleeve':
-        dictionary = {
-            'strapless' : [db.yonatan_dresses.find({'sleeve_length': ['true', 'false', 'false', 'false', 'false', 'false', 'false', 'false', 'false']}), 0],
-            'spaghetti_straps' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'true', 'false', 'false', 'false', 'false', 'false', 'false', 'false']}), 1],
-            'straps' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'true', 'false', 'false', 'false', 'false', 'false', 'false']}), 2],
-            'sleeveless' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'false', 'true', 'false', 'false', 'false', 'false', 'false']}), 3],
-            'cap_sleeve' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'false', 'false', 'true', 'false', 'false', 'false', 'false']}), 4],
-            'short_sleeve' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'false', 'false', 'false', 'true', 'false', 'false', 'false']}), 5],
-            'midi_sleeve' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'false', 'false', 'false', 'false', 'true', 'false', 'false']}), 6],
-            'long_sleeve' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'true', 'false']}), 7]
-            # 'asymmetry' : [db.yonatan_dresses.find({'sleeve_length': ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'false', 'true']}), 8]
-        }
+        dictionary = yonatan_constants.dress_sleeve_dict
 
     # dress length #
     elif args.input_file == 'dress_length':
-        dictionary = {
-            'mini_length': db.yonatan_dresses.count({'dress_length': ['true', 'false', 'false', 'false', 'false', 'false']}),
-            'above_knee': db.yonatan_dresses.count({'dress_length': ['false', 'true', 'false', 'false', 'false', 'false']}),
-            'knee_length': db.yonatan_dresses.count({'dress_length': ['false', 'false', 'true', 'false', 'false', 'false']}),
-            'tea_length': db.yonatan_dresses.count({'dress_length': ['false', 'false', 'false', 'true', 'false', 'false']}),
-            'ankle_length': db.yonatan_dresses.count({'dress_length': ['false', 'false', 'false', 'false', 'true', 'false']}),
-            'floor_length': db.yonatan_dresses.count({'dress_length': ['false', 'false', 'false', 'false', 'false', 'true']})
-        }
+        dictionary = yonatan_constants.dress_length_dict
 
     # men shirt sleeve #
     elif args.input_file == 'men_shirt_sleeve':
-        dictionary = {
-            'straps': db.yonatan_men_shirts.count({'shirt_sleeve_length': ['true', 'false', 'false', 'false', 'false']}),
-            'sleeveless': db.yonatan_men_shirts.count({'shirt_sleeve_length': ['false', 'true', 'false', 'false', 'false']}),
-            'short_sleeve': db.yonatan_men_shirts.count({'shirt_sleeve_length': ['false', 'false', 'true', 'false', 'false']}),
-            'midi_sleeve': db.yonatan_men_shirts.count({'shirt_sleeve_length': ['false', 'false', 'false', 'true', 'false']}),
-            'long_sleeve': db.yonatan_men_shirts.count({'shirt_sleeve_length': ['false', 'false', 'false', 'false', 'true']})
-        }
+        dictionary = yonatan_constants.men_shirt_sleeve_dict
 
     # pants length #
     elif args.input_file == 'pants_length':
-        dictionary = {
-            'bermuda': db.yonatan_pants.count({'pants_length': ['true', 'false', 'false', 'false']}),
-            'knee': db.yonatan_pants.count({'pants_length': ['false', 'true', 'false', 'false']}),
-            'capri': db.yonatan_pants.count({'pants_length': ['false', 'false', 'true', 'false']}),
-            'floor': db.yonatan_pants.count({'pants_length': ['false', 'false', 'false', 'true']})
-        }
+        dictionary = yonatan_constants.pants_length_dict
 
     # women shirt sleeve #
     elif args.input_file == 'women_shirt_sleeve':
-        dictionary = {
-            'strapless': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['true', 'false', 'false', 'false', 'false', 'false', 'false', 'false', 'false']}),
-            'spaghetti_straps': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'true', 'false', 'false', 'false', 'false', 'false', 'false', 'false']}),
-            'straps': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'true', 'false', 'false', 'false', 'false', 'false', 'false']}),
-            'sleeveless': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'false', 'true', 'false', 'false', 'false', 'false', 'false']}),
-            'cap_sleeve': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'false', 'false', 'true', 'false', 'false', 'false', 'false']}),
-            'short_sleeve': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'false', 'false', 'false', 'true', 'false', 'false', 'false']}),
-            'midi_sleeve': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'false', 'false', 'false', 'false', 'true', 'false', 'false']}),
-            'long_sleeve': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'true', 'false']}),
-            'asymmetry': db.yonatan_women_shirts.count(
-                {'shirt_sleeve_length': ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'false', 'true']})
-        }
+        dictionary = yonatan_constants.women_shirt_sleeve_dict
 
     else:
         print "wrong input!"
