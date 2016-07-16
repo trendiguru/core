8a381bf745f1e240ed0e762a36d7f1fe2f84bb8a
jeremy <jeremy@trendiguru.com>
2016-07-16 14:24:34 +0300

    nesterov

--- classifier_stuff/caffe_nns/progress_plot.py
+++ classifier_stuff/caffe_nns/progress_plot.py
@@ -227,22 +227,24 @@ def parse_logfile(f,logy):
       subtitle=subtitle+'gama'+gamma
     plt.suptitle(subtitle,fontsize=8)
     #plt.draw()
-
-    params = curve_fit(fit_exp,training_iterations,training_loss)
-    print('params:'+str(params))
-    k,a,b,x0 = params[0]
-    cov = params[1]
-    if cov[0][0] == np.inf:
-        print('bad fit')
-    else:
-        fit_y = fit_exp(training_iterations,k,a,b,x0)
-        ax1.plot(training_iterations,fit_y,linestyle='--',color='b')
-        middlex = training_iterations[len(training_iterations)/2]
-        middley = (np.max(training_loss)-np.min(training_loss))/2.0
-        ax1.text(middlex, middley, r'$y= b + k exp(a(x-x0)$', fontsize=15)
-        ax1.text(middlex, middley+1, 'b='+str(b), fontsize=15)
-
+    try:
+      params = curve_fit(fit_exp,training_iterations,training_loss)
+      print('params:'+str(params))
+      k,a,b,x0 = params[0]
+      cov = params[1]
+      if cov[0][0] == np.inf:
+          print('bad fit')
+      else:
+          fit_y = fit_exp(training_iterations,k,a,b,x0)
+          ax1.plot(training_iterations,fit_y,linestyle='--',color='b')
+          middlex = training_iterations[len(training_iterations)/2]
+          middley = (np.max(training_loss)-np.min(training_loss))/2.0
+          ax1.text(middlex, middley, r'$y= b + k exp(a(x-x0)$', fontsize=15)
+          ax1.text(middlex, middley+1, 'b='+str(b), fontsize=15)
+    except:
+      print('trouble fitting')
     if(0):
+
         params = curve_fit(fit_log,training_iterations,training_loss)
         print('params:'+str(params))
         k,a,b,x0 = params[0]
