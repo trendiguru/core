b542b1727818c1d4e3dc94cea43f944a20e322f2
jeremy <jeremy@trendiguru.com>
2016-07-17 19:11:26 +0300

    test latest resnet results

--- classifier_stuff/caffe_nns/multilabel.py
+++ classifier_stuff/caffe_nns/multilabel.py
@@ -324,8 +324,8 @@ if __name__ =="__main__":
     snapshot = 'snapshot'
     caffemodel = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_240000.caffemodel'
     caffemodel = '/home/jeremy/caffenets/multilabel/vgg_ilsvrc_16_multilabel_2/snapshot/train_iter_340000.caffemodel'
-    caffemodel = '/home/jeremy/caffenets/multilabel_resnet50_sgd_iter_120000.caffemodel'
-    solverproto = '/home/jeremy/caffenets/ResNet-50-deploy.prototxt'
+    caffemodel = '/home/jeremy/caffenets/production/multilabel_resnet50_sgd_iter_120000.caffemodel'
+    solverproto = '/home/jeremy/caffenets/production/ResNet-50-deploy.prototxt'
     for t in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.92,0.95,0.98]:
         p,r,a,tp,tn,fp,fn = check_accuracy(solverproto, caffemodel, threshold=t, num_batches=800)
         with open('multilabel_accuracy_results.txt','a') as f:
