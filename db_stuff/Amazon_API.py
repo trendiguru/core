16421206976222937f3c870b254eaa50e84fb4af
yonti <yontilevin@gmail.com>
2016-07-18 16:08:17 +0300

    database stuff

--- db_stuff/Amazon_API.py
+++ db_stuff/Amazon_API.py
@@ -332,7 +332,7 @@ def get_results(collection_name, node_id, price_flag=True, max_price=100000.0, m
         if min_price == max_price:
             total_pages=10
             print_error('same')
-        elif (max_price-min_price) == 0.01:
+        elif (max_price-min_price) < 0.02:
             print_error('diff == 0.01')
             new_items_count += get_results(collection_name, node_id,
                                            min_price=min_price, max_price=min_price, name=name)
