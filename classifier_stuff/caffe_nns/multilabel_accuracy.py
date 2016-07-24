d4a5c3a073e7abd83ed4515c3243e08548a1ec23
jeremy <jeremy@trendiguru.com>
2016-07-24 14:09:26 +0300

    res 152, lr=*10 instead of *100

--- classifier_stuff/caffe_nns/multilabel_accuracy.py
+++ classifier_stuff/caffe_nns/multilabel_accuracy.py
@@ -522,8 +522,8 @@ caffe.set_device(0)
 
 if __name__ =="__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',const=sum, default=max,help='sum the integers (default: find the max)')
+    parser.add_argument('--testproto',  help='test prototxt')
+    parser.add_argument('--caffemodel', help='caffmodel')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))
