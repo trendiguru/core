7c5ffbb3b9bb2fd954d10dbaf11bf26c737656ed
yonti <yontilevin@gmail.com>
2016-07-26 15:30:11 +0300

    yonatan comparison tool

--- Yonti/yonatan_comparison_tool.py
+++ Yonti/yonatan_comparison_tool.py
@@ -1,24 +1,27 @@
 from ..constants import db, fingerprint_weights, histograms_length
 from ..NNSearch import distance_Bhattacharyya
+import argparse
+import sys
 
 collection = db.fanni
 
-def find_n_nearest_neighbors_copy(fingerprint, database, category, number_of_matches, field=None, rule='eq', val=0,
+
+def find_n_nearest_neighbors_copy(fingerprint, database, category, number_of_matches, new_field=None, query_rule='eq', val=1,
                                   fp_weights= fingerprint_weights, hist_length=histograms_length,
                                   fp_key='fingerprint'):
 
 
     distance_function = distance_Bhattacharyya
     # list of tuples with (entry,distance). Initialize with first n distance values
-    if field == None:
+    if new_field == None:
         entries = db[database].find({'categories': category},
                                       {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
     else:
-        if rule == 'eq':
-            entries = db[database].find({'categories': category, field: val},
+        if query_rule == 'eq':
+            entries = db[database].find({'categories': category, new_field: val},
                                         {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
         else:
-            entries = db[database].find({'categories': category, field:{'$lte':val+1, '$gte':val-1}},
+            entries = db[database].find({'categories': category, new_field:{'$lte': val+1, '$gte': val-1}},
                                         {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
 
     farthest_nearest = 1
@@ -61,9 +64,33 @@ def renew_top_results(database_name, field, rule, category_id='dress', number_of
     for x, item in enumerate(items):
         idx = item['_id']
         fp = item['fingerprint']
+        val = item[field]
         top_fp = find_n_nearest_neighbors_copy(fp, database_name, category_id, number_of_results)
-        top_sp = find_n_nearest_neighbors_copy(fp, database_name, category_id, number_of_results, field,rule)
+        top_sp = find_n_nearest_neighbors_copy(fp, database_name, category_id, number_of_results, field,rule, val)
         collection.update_one({'_id':idx},{'$set':{'topresults.fp': top_fp,
                                                    'topresults.sp': top_sp}})
         print ('%d/%d' %(x, total))
 
+
+def getUserInput():
+    parser = argparse.ArgumentParser(description='"@@@ Yonatan Comparison Tool @@@')
+    parser.add_argument('-f', '--field', dest="field",required=True,
+                        help='what field to add to query by besides category')
+    parser.add_argument('-r', '--rule', dest="rule", default='eq',
+                        help='use eq for equal or range for +_1', required=True)
+    parser.add_argument('-d', '--db', dest="db",
+                        help='which database to use for the testing', required=True)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    # get user input
+    user_input = getUserInput()
+    field = user_input.field
+    rule = user_input.rule
+    db_name = user_input.db
+    if rule not in ['eq', 'range']:
+        print ('bad rule! use only eq or range')
+        sys.exit(1)
+    renew_top_results()
