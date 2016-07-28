68a77ee71b8a6e6059c5a6bfefe468d04ea4a289
jeremy <jeremy@trendiguru.com>
2016-07-28 12:46:42 +0300

    imutils err on multilabel branch

--- paperdoll/neurodoll_falcon_client.py
+++ paperdoll/neurodoll_falcon_client.py
@@ -6,7 +6,13 @@ CLASSIFIER_ADDRESS = "http://37.58.101.173:8080/nd"
 
 
 def pd(image_array_or_url, category_index=None):
-    params = params={"categoryIndex": category_index} if category_index else None
+    params = None
+    if category_index:
+        params['categoryIndex'] = category_index
+    if get:
+        params['getMultilabelResults'] = get_multilabel_results
+    print('params coming into pd:'+str(params))
+        #params = params={"categoryIndex": category_index} if category_index else None
     data = msgpack.dumps({"image": image_array_or_url})
     resp = requests.post(CLASSIFIER_ADDRESS, data=data, params=params)
     return msgpack.loads(resp.content)
