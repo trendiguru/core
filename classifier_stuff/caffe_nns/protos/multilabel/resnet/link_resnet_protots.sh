3206fc84f0778f9633b82c5fdc96f27af90e16c1
jeremy <jeremy@trendiguru.com>
2016-07-31 02:05:43 +0300

    resnet152 - lr->0.005 from 0.01, last layer lr->10 from 100

--- classifier_stuff/caffe_nns/protos/multilabel/resnet/link_resnet_protots.sh
+++ classifier_stuff/caffe_nns/protos/multilabel/resnet/link_resnet_protots.sh
@@ -7,3 +7,11 @@ for f in *;
     do echo $f ;
     ln -s /usr/lib/python2.7/dist-packages/trendi/classifier_stuff/caffe_nns/protos/multilabel/resnet/$f /home/jeremy/caffenets/multilabel/deep-residual-networks/prototxt/$f ;
 done
+
+#get jrlayers into caffe path
+ln -s /usr/lib/python2.7/dist-packages/trendi/classifier_stuff/caffe_nns/jrlayers.py /opt/caffe/python/jrlayers.py
+
+# get images db
+# rsync -r root@37.58.101.173:/home/jeremy/image_dbs/tamara_berg /home/jeremy/image_dbs/
+# get caffenets
+# rsync -r root@37.58.101.173:/home/jeremy/caffenets /home/jeremy/
