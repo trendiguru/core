796ec4652734b9b2deb072ae814147e0c933fe61
yonti <yontilevin@gmail.com>
2016-08-01 20:46:38 +0300

    phash

--- db_stuff/amazon_worker.py
+++ db_stuff/amazon_worker.py
@@ -265,11 +265,11 @@ def insert_items(collection_name, item_list, items_in_page, print_flag, family_t
                 print ('hash already exists')
                 continue
 
-            p_hash = get_p_hash(pil_image)
-            p_hash_exists = collection.find_one({'p_hash': p_hash})
-            if p_hash_exists:
-                print ('p_hash already exists')
-                continue
+            # p_hash = get_p_hash(pil_image)
+            # p_hash_exists = collection.find_one({'p_hash': p_hash})
+            # if p_hash_exists:
+            #     print ('p_hash already exists')
+            #     continue
 
             short_d = attributes['Title']
             if 'Feature' in attr_keys:
