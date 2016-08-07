dd6bd3dfb28187c5390e69c769ca1fb55e9559f6
Lior Sabag <lior@trendiguru.com>
2016-08-07 19:47:49 +0300

    Add lean pd.m which takes img array

--- paperdoll/pd_array.m
+++ paperdoll/pd_array.m
@@ -6,7 +6,7 @@ function [mask,label_names,pose] = pd(image_array)
     %profile on
     tic
     %disp('debug0')
-    load ./data/paperdoll_pipeline.mat config;
+    load /home/pd_user/paperdoll/data/paperdoll_pipeline.mat config;
     disp('debug1')
     addpath(genpath('.'))
     disp('debug2')
