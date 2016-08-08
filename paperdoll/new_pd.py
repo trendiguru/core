5d6a3ea8226f23d0ee5e4166af98775e81bf6727
Lior Sabag <lior@trendiguru.com>
2016-08-08 22:00:14 +0300

    Add wrapped functions to new_pd

--- paperdoll/new_pd.py
+++ paperdoll/new_pd.py
@@ -8,22 +8,33 @@ import matlab
 from matlab.engine import start_matlab
 import sys
 import os
+import logging
+import random
+import string
+import numpy as np
+import cv2
+
+from .. import Utils
+from .. import constants
 
 default_root = os.getcwd()
 pd_pipeline_file = 'data/paperdoll_pipeline.mat'
-eng = None
-
-def init_pd(root=default_root):
-    global eng
-    eng = start_matlab('-nodesktop -nojvm')
-    eng.eval("load '{0}' 'config';".format(os.path.join(root, pd_pipeline_file)), nargout=0)
-    eng.eval("addpath(genpath('{0}'))".format(root), nargout=0)
-    eng.eval("config{1}.scale = 200; config{1}.model.thresh = -2;", nargout=0)
+ENG = None
 
 
-def pd_py(filename):
-    if not eng:
-        init_pd()
+def init_pd_eng(root=default_root):
+    global ENG
+    if not ENG:
+        eng = start_matlab('-nodesktop -nojvm')
+        eng.eval("load '{0}' 'config';".format(os.path.join(root, pd_pipeline_file)), nargout=0)
+        eng.eval("addpath(genpath('{0}'))".format(root), nargout=0)
+        eng.eval("config{1}.scale = 200; config{1}.model.thresh = -2;", nargout=0)
+        ENG = eng
+    return eng
+
+
+def raw_parse(filename, _eng=None):
+    eng = _eng or init_pd_eng()
     eng.eval("mask = []; label_names = []; pose = [];", nargout=0)
     try:
         eng.eval("image_array = imread('{0}');".format(filename), nargout=0)
@@ -41,3 +52,35 @@ def pd_py(filename):
         print sys.exc_info()
         return
     return mask, label_names, pose
+
+
+def parse(img_url_or_cv2_array, _eng=None, filename=None):
+    img = Utils.get_cv2_img_array(img_url_or_cv2_array)
+    filename = filename or rand_string()
+    img_ok = image_big_enough(img)
+    if img_ok and cv2.imwrite(filename + '.jpg', img):
+        mask, label_dict, pose = raw_parse(filename + '.jpg', _eng=_eng)
+        mask_np = np.array(mask, dtype=np.uint8)
+        pose_np = np.array(pose, dtype=np.uint8)
+
+        return mask_np, label_dict, pose_np, filename
+    else:
+        if img_ok:
+            logging.debug("problem writing " + str(filename) + " parse()")
+        return
+
+
+def image_big_enough(img_array):
+    if img_array is None:
+        logging.debug("input image is empty")
+        return False
+    width, height = img_array.shape[0:1]
+    if width < constants.minimum_im_width or height < constants.minimum_im_height:
+        logging.debug('image dimensions too small')
+        return False
+    else:
+        return True
+
+
+def rand_string():
+    return ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
---
+++ paperdoll/new_pd_falcon.py
@@ -0,0 +1,52 @@
+import falcon
+import datetime
+
+from jaweson import json, msgpack
+from . import new_pd
+
+import random
+import string
+
+
+def randomword(length):
+    return ''.join(random.choice(string.lowercase) for i in range(length))
+
+rand_eng_name = randomword(4)
+print "{0}: Starting MATLAB engine {1}".format(datetime.datetime.now(), rand_eng_name)
+eng = matlab.engine.start_matlab('-nodesktop -nojvm')
+print "{0}: Started MATLAB engine {1}".format(datetime.datetime.now(), rand_eng_name)
+
+class PaperResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': 'I\'ve always been more interested in the future than in the past.',
+            'author': 'Grace Hopper'
+        }
+
+        resp.body = json.dumps(quote)
+
+    def on_post(self, req, resp):
+        ret = {"success": False}
+        try:
+            data = msgpack.loads(req.stream.read())
+
+            img = data.get("image")
+
+            # mask_np, label_dict, pose_np, filename
+            ret["mask"], ret["label_dict"], ret["pose"], ret["filename"] = pd.get_parse_mask_parallel(eng, img)
+            if ret["mask"] is not None:
+                ret["success"] = True
+            else:
+                ret["error"] = "No mask from PD"
+
+        except Exception as e:
+            ret["error"] = str(e)
+
+        resp.data = msgpack.dumps(ret)
+        resp.content_type = 'application/x-msgpack'
+        resp.status = falcon.HTTP_200
+
+
+api = falcon.API()
+api.add_route('/pd/', PaperResource())
