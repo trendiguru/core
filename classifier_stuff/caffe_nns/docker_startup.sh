1caa75861adbd5b28142c937af76ac5f742418e7
jeremy <jeremy@trendiguru.com>
2016-08-11 18:55:00 +0300

    threshold in neurodoll fixed

---
+++ classifier_stuff/caffe_nns/docker_startup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -s /usr/lib/python2.7/dist-packages/trendi/classifier_stuff/caffe_nns/score.py /opt/caffe/python/score.py
+ln -s /usr/lib/python2.7/dist-packages/trendi/classifier_stuff/caffe_nns/surgery.py /opt/caffe/python/surgery.py
