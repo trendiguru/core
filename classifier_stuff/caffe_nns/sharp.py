0a4c067d6ad525d89f7c0f5a36cdd337d3bd5bdd
jeremy <jeremy@trendiguru.com>
2016-08-16 16:12:48 +0300

    python-gen net

--- classifier_stuff/caffe_nns/sharp.py
+++ classifier_stuff/caffe_nns/sharp.py
@@ -368,7 +368,7 @@ def unet(db,mean_value=[112.0,112.0,112.0],n_cats=21):
                     num_output=1024,pad = 0,kernel_size=2,stride = 2,
                     weight_filler=dict(type='xavier'),bias_filler=dict(type='constant',value=0.2))
 
-    n.conv7_1,n.relu7_1 = conv_relu(n.deconv7,n_output=512,kernel_size=2,pad=1)  #watch out for padsize here, make sure outsize is 14x14
+    n.conv7_1,n.relu7_1 = conv_relu(n.deconv7,n_output=512,kernel_size=2,pad=0)  #watch out for padsize here, make sure outsize is 14x14 #indeed
     bottom=[n.conv5_3, n.conv7_1]
     n.cat7 = L.Concat(*bottom) #param=dict(concat_dim=1))
     n.conv7_2,n.relu7_2 = conv_relu(n.cat7,n_output=1024,kernel_size=3,pad=1)
