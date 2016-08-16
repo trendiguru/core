5298f3d510e229ca4ad0a40102b2eff6d63a2b51
yonatan <yonatanguy@gmail.com>
2016-08-16 15:12:43 +0300

    changed last layer from Softmaxwithloss to Softmax

---
+++ yonatan/run_caffe_training.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+import sys
+import caffe
+
+weights = 'ResNet-50-model.caffemodel'  #in brainia container jr2
+
+# init
+caffe.set_device(int(sys.argv[1]))
+caffe.set_mode_gpu()
+
+solver = caffe.SGDSolver('/home/yonatan/trendi/yonatan/resnet_50_gender_by_face/solver50_sgd.prototxt')
+
+# for finetune
+solver.net.copy_from(weights)
+
+# surgeries
+#interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+#surgery.interp(solver.net, interp_layers)
+
+
+#run net learning iterations
+for _ in range(1000):
+    solver.step(5000)
+#    score.seg_tests(solver, False, val, layer='score')
+#    jrinfer.seg_tests(solver, False, val, layer='score')
+#    progress_plot.parse_solveoutput('net_output.txt')
