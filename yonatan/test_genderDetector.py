a78fd34f00b9580f40d4ed43f2fcf846d1c3fb5c
yonatan <yonatanguy@gmail.com>
2016-08-17 19:32:50 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/test_genderDetector.py
+++ yonatan/test_genderDetector.py
@@ -16,7 +16,6 @@ import skimage
 import requests
 import dlib
 from ..utils import imutils
-import yonatan_classifier
 
 
 detector = dlib.get_frontal_face_detector()
@@ -24,13 +23,13 @@ detector = dlib.get_frontal_face_detector()
 MODLE_FILE = "/home/yonatan/trendi/yonatan/resnet_50_gender_by_face/ResNet-50-deploy.prototxt"
 PRETRAINED = "/home/yonatan/resnet50_caffemodels/caffe_resnet50_snapshot_sgd_genfder_by_face_iter_10000.caffemodel"
 caffe.set_mode_gpu()
-image_dims = [115, 115]
+image_dims = [224, 224]
 mean, input_scale = np.array([120, 120, 120]), None
 channel_swap = [2, 1, 0]
 raw_scale = 255.0
 
 # Make classifier.
-classifier = yonatan_classifier.Classifier(MODLE_FILE, PRETRAINED,
+classifier = caffe.Classifier(MODLE_FILE, PRETRAINED,
                               image_dims=image_dims, mean=mean,
                               input_scale=input_scale, raw_scale=raw_scale,
                               channel_swap=channel_swap)
