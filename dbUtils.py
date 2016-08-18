ad36422b748aba5e3b9ef5859da58ff6a28dce19
Nadav <nadav@trendiguru.com>
2016-08-18 15:59:20 +0300

    add_sleeve_to_images function

--- dbUtils.py
+++ dbUtils.py
@@ -1079,22 +1079,6 @@ def update_similar_results():
 
 def add_sleeve_length_to_relevant_items_in_images():
 
-    def parallel_sleeve_and_replace(image_obj, image):
-        try:
-            for person in image_obj['people']:
-                person_cats = set([item['category'] for item in person['items']])
-                if len(rel_cats.intersection(person_cats)):
-                    if len(image_obj['people']) > 1:
-                        image = background_removal.person_isolation(image, person['face'])
-                    for item in person['items']:
-                        if item['category'] in rel_cats:
-                            item['fp']['sleeve_length'] = sleeve_client.get_sleeve(image)['data']
-            db.images.replace_one({'_id': doc['_id']}, image_obj)
-            return True
-        except Exception as e:
-            print(e)
-
-    rel_cats = set([cat for cat in constants.features_per_category.keys() if 'sleeve_length' in constants.features_per_category[cat]])
     print("Starting, total {0} images".format(db.images.count()))
     sent = 0
     deleted = 0
@@ -1108,4 +1092,19 @@ def add_sleeve_length_to_relevant_items_in_images():
     sent += 1
 
 
+def parallel_sleeve_and_replace(image_obj, image):
+    rel_cats = set([cat for cat in constants.features_per_category.keys() if 'sleeve_length' in constants.features_per_category[cat]])
+    try:
+        for person in image_obj['people']:
+            person_cats = set([item['category'] for item in person['items']])
+            if len(rel_cats.intersection(person_cats)):
+                if len(image_obj['people']) > 1:
+                    image = background_removal.person_isolation(image, person['face'])
+                for item in person['items']:
+                    if item['category'] in rel_cats:
+                        item['fp']['sleeve_length'] = sleeve_client.get_sleeve(image)['data']
+        db.images.replace_one({'_id': image_obj['_id']}, image_obj)
+        return True
+    except Exception as e:
+        print(e)
 
