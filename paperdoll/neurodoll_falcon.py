441be5922bf3b9d961f457c8845a1204cf8f45e0
jeremy <jeremy@trendiguru.com>
2016-08-20 20:18:48 +0300

    down with yolo

--- paperdoll/neurodoll_falcon.py
+++ paperdoll/neurodoll_falcon.py
@@ -5,7 +5,7 @@ import falcon
 from .. import neurodoll, neurodoll_single_category
 from .. import neurodoll_with_multilabel
 from .. import constants
-from .darknet.pyDarknet import mydet
+#from .darknet.pyDarknet import mydet
 
 from jaweson import json, msgpack
 
@@ -31,9 +31,9 @@ class PaperResource:
         print('get combined:'+str(get_combined_results))
         get_combined_results = get_combined_results == "true" or get_combined_results == "True" or get_combined_results == True
 
-        get_yolo_results = req.get_param('getYolo')
-        print('get yolo:'+str(get_yolo_results))
-        get_yolo_results = get_yolo_results == "true" or get_yolo_results == "True" or get_yolo_results == True
+#        get_yolo_results = req.get_param('getYolo')
+#        print('get yolo:'+str(get_yolo_results))
+#        get_yolo_results = get_yolo_results == "true" or get_yolo_results == "True" or get_yolo_results == True
 
 
         ret = {"success": False}
@@ -41,10 +41,10 @@ class PaperResource:
             data = msgpack.loads(req.stream.read())
             img = data.get("image")
 
-            if get_yolo_results:
-                yolo_output = mydet.get_yolo_results(img)
-                ret['yolo_output'] = yolo_output
-                print('yolo output:'+str(yolo_output))
+#            if get_yolo_results:
+#                yolo_output = mydet.get_yolo_results(img)
+#                ret['yolo_output'] = yolo_output
+#                print('yolo output:'+str(yolo_output))
 
             if get_multilabel_results:
                 multilabel_output = neurodoll_with_multilabel.get_multilabel_output(img)
