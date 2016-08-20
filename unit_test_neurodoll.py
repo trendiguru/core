425f2a492a7f0bb1db6f846072975756d3410451
jeremy <jeremy@trendiguru.com>
2016-08-21 00:07:16 +0300

    unit tests for nd

--- unit_test_neurodoll.py
+++ unit_test_neurodoll.py
@@ -15,27 +15,26 @@ class OutcomesTest(unittest.TestCase):
     # raise RuntimeError('Test error!')
 
     def setUp(self):
+        self.url='https://s-media-cache-ak0.pinimg.com/236x/df/a3/0a/dfa30af65a46ad8267d148dcefd813d1.jpg'
         pass
 
-    def test_nd_output():
-        url='https://s-media-cache-ak0.pinimg.com/236x/df/a3/0a/dfa30af65a46ad8267d148dcefd813d1.jpg'
-        dict = nfc.pd(url)
+    def test_nd_output(self):
+        dict = nfc.pd(self.url)
         assert(dict['success'] is not None)
         print('dict from falcon dict:'+str(dict))
         return dict
 
-    def test_nd_categorical_output():
-        url='https://s-media-cache-ak0.pinimg.com/236x/df/a3/0a/dfa30af65a46ad8267d148dcefd813d1.jpg'
+    def test_nd_categorical_output(self):
         category_index = 0
-        dic = nfc.pd(url, category_index=category_index)
+        dic = nfc.pd(self.url, category_index=category_index)
         assert(dict['success'] is not None)
         if not dic['success']:
             logging.debug('nfc pd not a success')
             return False, []
         return dict
 
-    def test_multilabel_output():
-        url='https://s-media-cache-ak0.pinimg.com/236x/df/a3/0a/dfa30af65a46ad8267d148dcefd813d1.jpg'
+    def test_multilabel_output(self):
+        self.url='https://s-media-cache-ak0.pinimg.com/236x/df/a3/0a/dfa30af65a46ad8267d148dcefd813d1.jpg'
         multilabel_dict = nfc.pd(url, get_multilabel_results=True)
         assert(multilabel_dict['success'] is not None)
         print('dict from falcon dict:'+str(multilabel_dict))
