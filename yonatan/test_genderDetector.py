b92a8757661a305e421abfc33f1b90cd8f0a1921
yonatan <yonatanguy@gmail.com>
2016-08-21 14:52:20 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/test_genderDetector.py
+++ yonatan/test_genderDetector.py
@@ -23,6 +23,7 @@ detector = dlib.get_frontal_face_detector()
 MODLE_FILE = "/home/yonatan/trendi/yonatan/resnet_50_gender_by_face/ResNet-50-deploy.prototxt"
 PRETRAINED = "/home/yonatan/resnet50_caffemodels/caffe_resnet50_snapshot_sgd_genfder_by_face_iter_10000.caffemodel"
 caffe.set_mode_gpu()
+caffe.set_device(1) # choose GPU
 image_dims = [224, 224]
 mean, input_scale = np.array([120, 120, 120]), None
 channel_swap = [2, 1, 0]
