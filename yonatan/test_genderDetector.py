c5f52a24ddde8b6eee80f083a61f7f71f3442a0a
yonatan <yonatanguy@gmail.com>
2016-08-22 12:15:01 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/test_genderDetector.py
+++ yonatan/test_genderDetector.py
@@ -89,15 +89,6 @@ def theDetector(url_or_np_array):
 
     face_image = full_image[y: y + h, x: x + w]
 
-
-    print face_image
-
-    # face_image[:, :, 0] = face_image[:, :, 0] / 255.0
-    # face_image[:, :, 1] = face_image[:, :, 1] / 255.0
-    # face_image[:, :, 2] = face_image[:, :, 2] / 255.0
-
-    #cv2.imwrite('color_img.jpg', face_image)
-
     # cv2.imshow("full_image", full_image)
     # cv2.waitKey()
     #
