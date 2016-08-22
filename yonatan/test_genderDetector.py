0b564561130082971b9a1f8ab8a239c60231ddae
yonatan <yonatanguy@gmail.com>
2016-08-22 12:13:01 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/test_genderDetector.py
+++ yonatan/test_genderDetector.py
@@ -16,6 +16,7 @@ import skimage
 import requests
 import dlib
 from ..utils import imutils
+import yonatan_classifier
 
 
 detector = dlib.get_frontal_face_detector()
@@ -30,7 +31,7 @@ channel_swap = [2, 1, 0]
 raw_scale = 255.0
 
 # Make classifier.
-classifier = caffe.Classifier(MODLE_FILE, PRETRAINED,
+classifier = yonatan_classifier.Classifier(MODLE_FILE, PRETRAINED,
                               image_dims=image_dims, mean=mean,
                               input_scale=input_scale, raw_scale=raw_scale,
                               channel_swap=channel_swap)
