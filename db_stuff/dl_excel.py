daf3928d9e02091844fb4a1d9bf3a83967cb3d1d
yonti <yontilevin@gmail.com>
2016-08-22 19:14:49 +0300

    database stuff

--- db_stuff/dl_excel.py
+++ db_stuff/dl_excel.py
@@ -43,7 +43,7 @@ def category_tree_status(worksheet, merge_format, bold):
                            {'header': 'status'}]}
 
     print(categories_length)
-    worksheet.add_table('B2:F'+str(categories_length), options)
+    worksheet.add_table('B2:I'+str(categories_length), options)
     # for x in ['F', 'G']:
     #     worksheet.write_formula(x+str(categories_length), '=SUM('+x+'3:'+x+str(categories_length-1)+')')
 
