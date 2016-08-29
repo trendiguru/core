3ef48f542b1f15259de00f2272d2a0b4b81a4bc3
yonatan <yonatanguy@gmail.com>
2016-08-29 17:55:06 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/histogram_on_test_set.py
+++ yonatan/histogram_on_test_set.py
@@ -21,7 +21,8 @@ import yonatan_classifier
 array_success = np.array([])
 array_failure = np.array([])
 
-text_file = open("55k_face_test_list.txt", "r")
+#text_file = open("55k_face_test_list.txt", "r")
+text_file = open("test_dir.txt", "r")
 
 counter = 0
 test_flag = 1
