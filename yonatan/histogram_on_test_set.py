c2fc198f68cf075e854fcc2f485b0922e67bdb7c
yonatan <yonatanguy@gmail.com>
2016-08-29 12:28:54 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/histogram_on_test_set.py
+++ yonatan/histogram_on_test_set.py
@@ -53,11 +53,15 @@ for line in text_file:
     # split line to full path and label
     path = line.split()
 
+    print path[0]
+    print path[1]
+
     if path == []:
         continue
 
     # Load numpy array (.npy), directory glob (*.jpg), or image file.
     input_file = os.path.expanduser(path[0])
+    print input_file
     inputs = [caffe.io.load_image(input_file)]
     #inputs = [Utils.get_cv2_img_array(input_file)]
 
