95a235a4ebaa95af4556050358752b57c633ebd3
yonatan <yonatanguy@gmail.com>
2016-08-30 18:29:24 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/tyota2.py
+++ yonatan/tyota2.py
@@ -40,3 +40,50 @@ for root, dirs, files in os.walk(dir_path):
 
         counter += 1
         print counter
+
+
+def divide_data():
+
+    counter = 0
+    counter_train = 0
+    counter_cv = 0
+    counter_test = 0
+
+    train_dir_path = '/home/yonatan/dress_length_3_labels_sets/dress_length_3_labels_train_set/'
+    cv_dir_path = '/home/yonatan/dress_length_3_labels_sets/dress_length_3_labels_cv_set/'
+    test_dir_path = '/home/yonatan/dress_length_3_labels_sets/dress_length_3_labels_test_set/'
+
+    source_dir = '/home/yonatan/dress_length_from_my_pc'
+
+    for root, dirs, files in os.walk(source_dir):
+        file_count = len(files)
+
+        counter_train = file_count * 0.9
+        counter_cv = file_count * 0.05
+        counter_test = file_count * 0.05
+
+        for file in files:
+
+            print file
+
+            resized_image = imutils.resize_keep_aspect(file, output_size = (224, 224))
+
+            # old_file_location = source_dir + '/' + file
+            #
+            # if counter < counter_train:
+            #     new_file_location = train_dir_path + file
+            #     os.rename(old_file_location, new_file_location)
+            #     counter += 1
+            # elif counter >= counter_train and counter < counter_train + counter_cv:
+            #     new_file_location = cv_dir_path + file
+            #     os.rename(old_file_location, new_file_location)
+            #     counter += 1
+            # elif counter >= counter_train + counter_cv and counter < counter_train + counter_cv + counter_test:
+            #     new_file_location = test_dir_path + file
+            #     os.rename(old_file_location, new_file_location)
+            #     counter += 1
+            # else:
+            #     print counter
+            #     break
+
+print 'counter_train = {0}, counter_cv = {1}, counter_test = {2}, counter = {3}'.format(counter_train, counter_cv, counter_test, counter)
