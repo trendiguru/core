9abd0974978a040169abcb01fa4a4656615d10ef
yonatan <yonatanguy@gmail.com>
2016-08-30 19:09:13 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/adding_extention_manually.py
+++ yonatan/adding_extention_manually.py
@@ -18,8 +18,9 @@ for root, dirs, files in os.walk(source_dir):
     for file in files:
         if not file.endswith(".jpg"):
             print file
-            old_file_name = source_dir + '/' + file
-            new_file_name = source_dir + '/' + file + '.jpg'
+            old_file_name = root + '/' + file
+            new_file_name = root + '/' + file + '.jpg'
+            print new_file_name
             os.rename(old_file_name, new_file_name)
             counter += 1
             print counter
