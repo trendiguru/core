8e49c6bac8691aa823ce542a20bc1c9d4ab9be91
yonatan <yonatanguy@gmail.com>
2016-09-12 15:51:47 +0300

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/crop_image.py
+++ yonatan/crop_image.py
@@ -47,7 +47,7 @@ def crop(argv):
 
     print "height: {0}, width: {1}".format(h, w)
 
-    crop_img = image[1:h, w / 2:w]  # Crop from x, y, w, h -> 100, 200, 300, 400
+    crop_img = image[12:h, (w / 2) + 15:w - 30]  # Crop from x, y, w, h -> 100, 200, 300, 400
     # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
 
     cv2.imwrite("cropped_image.png", crop_img)
