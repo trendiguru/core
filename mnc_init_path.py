54d9fa78748483f706dca18badfa05327fd0cdbf
Lior Sabag <lior@trendiguru.com>
2016-09-20 16:49:36 +0300

    hardcode MNC lib paths

--- mnc_init_path.py
+++ mnc_init_path.py
@@ -10,12 +10,12 @@ def add_path(path):
     if path not in sys.path:
         sys.path.insert(0, path)
 
-cur_dir = "/root/MNC/tools"
+#cur_dir = "/root/MNC/"
 
 # Add caffe python to PYTHONPATH
-caffe_path = os.path.join(cur_dir, '..', 'caffe-mnc', 'python')
+caffe_path = "/root/MNC/caffe-mnc/python"
 add_path(caffe_path)
 
 # Add lib to PYTHONPATH
-lib_path = os.path.join(cur_dir, '..', 'lib')
+lib_path = "/root/MNC/lib"
 add_path(lib_path)
