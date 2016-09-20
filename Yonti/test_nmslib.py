f46c8573a697f05562d5572774a6f577204e131e
yonti <yontilevin@gmail.com>
2016-09-20 17:38:04 +0300

    database stuff

--- Yonti/test_nmslib.py
+++ Yonti/test_nmslib.py
@@ -15,7 +15,7 @@ def hexa2bin(hexa):
 
 
 def create_index(col_name, category, query, k):
-    space_type = 'linf'
+    space_type = 'l1'
     space_param = []
     method_name = 'small_world_rand'
     index_name = method_name + '.index'
