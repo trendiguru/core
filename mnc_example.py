b20853875a33434fa661d7b923759e31944cc3e9
jeremy <jeremy@trendiguru.com>
2016-09-21 20:17:44 +0300

    nd gpu0

--- mnc_example.py
+++ mnc_example.py
@@ -166,7 +166,7 @@ def get_mnc_output_using_falcon(url):
     orig_im = mnc_output["original_image"]
     bboxes = mnc_output["bounding_boxes"]
     first_bb = bboxes[0]
-    center_x, center_y,w,h = first_bb[0],first_bb[1],first_bb[2],first_bb[3]
+    center_x, center_y,w,h,confidence = first_bb[0],first_bb[1],first_bb[2],first_bb[3],first_bb[4]
     scalefactor = mnc_output["scale_factor"]
     center_x = center_x*scalefactor
     center_y = center_x*scalefactor
@@ -175,6 +175,7 @@ def get_mnc_output_using_falcon(url):
     pt1 = (center_x - w/2, center_y - h/2)
     pt2 = (center_x + w/2, center_y + h/2)
     cv2.rectangle(orig_im,pt1,pt2,color=[0,255,100],thickness=3)
+    cv2.putText(orig_im,'human:'+str(confidence),org=(pt1[0],pt1[1]-10))
     cv2.imwrite(im_name,orig_im)
     cv2.imwrite(im_name[:-4]+'super.jpg',superimposed_im)
     cv2.imshow('superimpose',superimposed_im)
