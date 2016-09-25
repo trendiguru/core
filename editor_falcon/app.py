ce9f305267d091803511ea3b23737d9ce33b37c3
Lior Sabag <lior@trendiguru.com>
2016-09-25 19:08:46 +0300

    Uncomment auth

--- editor_falcon/app.py
+++ editor_falcon/app.py
@@ -18,7 +18,7 @@ login, auth_middleware = falcon_jwt.get_auth_objects(
 
 cors = CORS(allow_all_headers=True, allow_all_origins=True, allow_all_methods=True)
 
-api = falcon.API(middleware=[cors.middleware])#, auth_middleware])
+api = falcon.API(middleware=[cors.middleware, auth_middleware])
 
 editor = editor.Editor()
 
