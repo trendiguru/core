d926120aedca1d9297828a8c5b2e9d292f44828e
yonti <yontilevin@gmail.com>
2016-09-25 16:49:25 +0300

    database stuff

--- db_stuff/nmslib/falcon_client.py
+++ db_stuff/nmslib/falcon_client.py
@@ -1,12 +1,12 @@
 from jaweson import msgpack
 import requests
 
-SERVER = "http://extremeli-evolution-dev-2:8080/"  # use the name of the server running the gunicorn
+SERVER = "http://extremeli-evolution-dev-2:"  # use the name of the server running the gunicorn
 
 
-def nmslib_find_top_k(fp, k , category):
+def nmslib_find_top_k(fp, k, port,category):
     data = msgpack.dumps({"fp": fp,
                           "k": k})
-    category_server = SERVER+category
+    category_server = SERVER+port+'/'+category
     resp = requests.post(category_server, data=data)
     return msgpack.loads(resp.content)
