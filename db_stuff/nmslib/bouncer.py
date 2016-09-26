581330b281fa9a9bc9d4d7ba0fad1728f395c173
yonti <yontilevin@gmail.com>
2016-09-26 11:10:09 +0300

    database stuff

--- db_stuff/nmslib/bouncer.py
+++ db_stuff/nmslib/bouncer.py
@@ -78,7 +78,7 @@ def rebuild_index(collection_name, category):
     else:
         new_version = 1
     p = find_free_port()
-    build_index.build_n_save(collection_name,category,new_version)
+    build_index.build_n_save(collection_name,category,str(new_version))
     process = 'gunicorn -b :%s falcon_app:api --env NMSLIB_INPUTS=%s/%s/%d -w 2 --preload' \
               % (p, collection_name, category, new_version)
     pid_new = subprocess.Popen(process)
