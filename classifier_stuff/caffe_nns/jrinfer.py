805617880111321bbf8fefb876e7dd2aa96beaf7
jeremy <jeremy@trendiguru.com>
2016-10-06 13:43:52 +0300

    no vis output in docker containers

--- classifier_stuff/caffe_nns/jrinfer.py
+++ classifier_stuff/caffe_nns/jrinfer.py
@@ -10,6 +10,8 @@ import argparse
 from datetime import datetime
 import caffe
 import sys
+import copy
+
 #import json
 
 # from trendi import pipeline
@@ -228,7 +230,7 @@ def compute_hist(net, save_dir, dataset, layer='score', gt='label',labels=consta
    #         print('label size:'+str(im.shape))
             im.save(savename)
             orig_image = net.blobs['data'].data[0]
-            gt =         net.blobs['label'].data[0,0]
+            gt =         copy.deepcopy(net.blobs['label'].data[0,0])
 #            print('orig image size:'+str(orig_image.shape)+' gt:'+str(gt.shape))
 #            gt_reshaped = np.reshape(gt,[gt.shape[1],gt.shape[2]])
 #            gt_reshaped = np.reshape(gt,[gt.shape[1],gt.shape[2]])
