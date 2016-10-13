b13c17b0494f0a701f90f1d6c686b7eca5e563f7
jeremy <jeremy@trendiguru.com>
2016-10-13 04:24:08 +0300

    vary trainsize

--- classifier_stuff/caffe_nns/vary_solve.py
+++ classifier_stuff/caffe_nns/vary_solve.py
@@ -167,9 +167,9 @@ def vary_trainsize():
             fp2.close
         print('n {}'.format(n))
      #   raw_input()
-    solve('../../ResNet-101-model.caffemodel',solverproto = 'ResNet-101_solver.prototxt',
-      testproto='ResNet-101-train_test.prototxt' ,type='single_label',cat='dress',
-      steps_per_iter=1,n_iter=1,n_loops=1,n_tests=10,baremetal_hostname='brainik80',classlabels=None)
+        solve('../../ResNet-101-model.caffemodel',solverproto = 'ResNet-101_solver.prototxt',
+          testproto='ResNet-101-train_test.prototxt' ,type='single_label',cat='dress',
+          steps_per_iter=1,n_iter=1,n_loops=1,n_tests=10,baremetal_hostname='brainik80',classlabels=None)
 
 
 if __name__ == "__main__":
