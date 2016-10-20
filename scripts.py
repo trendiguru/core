01a3c8cf14bd7d3e01210f7cb6b70eb32eef0345
Nadav <nadav@trendiguru.com>
2016-10-20 15:29:32 +0300

    enqueue add_results_from_collection

--- scripts.py
+++ scripts.py
@@ -3,6 +3,7 @@ __author__ = 'Nadav Paz'
 import urllib
 import os
 import pymongo
+import datetime
 from . import Utils
 from . import background_removal
 from .find_similar_mongo import get_all_subcategories
@@ -33,19 +34,19 @@ def clean_duplicates(collection, field):
     collection = db[collection]
     before = collection.count()
     sorted = collection.find().sort(field, pymongo.ASCENDING)
-    print('starting, total {0} docs'.format(before))
+    print('{0}, total {1} docs'.format(datetime.datetime.utcnow(), before))
     current_url = ""
     i = deleted = 0
     for doc in sorted:
         i += 1
-        if i % 1000 == 0:
-            print("deleted {0} docs after running on {1}".format(deleted, i))
+        # if i % 1000 == 0:
+        #     print("deleted {0} docs after running on {1}".format(deleted, i))
         if doc['image_urls'][0] != current_url:
             current_url = doc['image_urls'][0]
             deletion = collection.delete_many({'$and': [{'image_urls': doc['image_urls'][0]}, {'_id': {'$ne': doc['_id']}}]}).deleted_count
             if deletion:
                 deleted += deletion
-                print("found duplicates to {0}".format(doc['image_urls'][0]))
+                print("found {0} duplicates to {1}".format(deletion, doc['image_urls'][0]))
     print("total {0} docs were deleted".format(deleted))
 
 
