15a884bedcdbc6b81f19aba8185ffec1926aebcd
jeremy <jeremy@trendiguru.com>
2016-10-25 21:36:02 +0300

    combine binary classifier results

--- multilabel_from_binaries.py
+++ multilabel_from_binaries.py
@@ -145,7 +145,7 @@ def get_single_label_output(url_or_np_array,net, required_image_size=(224,224),r
 #   possible return out which has more info (namel the actual values which somehow relate to confidence in the answer)
 https://s-media-cache-ak0.pinimg.com/564x/9a/9d/f7/9a9df7455232035c6284ad1961816fd8.jpg
 if __name__ == "__main__":
-    urls = ['http://diamondfilms.com.au/wp-content/uploads/2014/08/Fashion-Photography-Sydney-1.jpg',
+    urls = ['https://s-media-cache-ak0.pinimg.com/236x/ce/64/a0/ce64a0dca7ad6d609c635432e9ae1413.jpg',
             'http://pinmakeuptips.com/wp-content/uploads/2015/02/1.4.jpg',
             'https://s-media-cache-ak0.pinimg.com/564x/9a/9d/f7/9a9df7455232035c6284ad1961816fd8.jpg',
             'http://2.bp.blogspot.com/-VmiQlqdliyE/U9nyto2L1II/AAAAAAAADZ8/g30i4j_YZeI/s1600/310714+awayfromblue+kameleon+shades+chambray+shift+dress+converse+louis+vuitton+neverfull+mbmj+scarf.png',
