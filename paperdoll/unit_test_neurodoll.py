0d869b720ec67411dd577ff358dd348f5bb654fb
jeremy <jeremy@trendiguru.com>
2016-10-30 18:35:17 +0200

    check combine_nd_w_ml to make sure everything works

--- paperdoll/unit_test_neurodoll.py
+++ paperdoll/unit_test_neurodoll.py
@@ -4,11 +4,6 @@ __author__ = 'jeremy'
 
 import unittest
 import time
-from rq import Queue
-
-import paperdoll_parse_enqueue
-import paperdolls
-import redis_conn
 import neurodoll_falcon_client as nfc
 import numpy as np
 
@@ -26,29 +21,38 @@ class OutcomesTest(unittest.TestCase):
 
     def test_nd_multilabel_combined(self):
 
+
         url = 'http://i.imgur.com/ahFOgkm.jpg'
         print('testing nd on:'+url)
+        start_time=time.time()
         mask = nfc.pd(url)
         assert(mask is not None)
         print('mask shape:'+mask.shape)
         print('unique mask values:'+np.unique(mask))
+        print('elapsed:'+str(time.time()-start_time))
 
         print('testing nd w get_multilabel_results:'+url)
+        start_time=time.time()
         multilabel_dict = nfc.pd(url, get_multilabel_results=True)
         assert(multilabel_dict is not None)
         print('multilabel dict:'+multilabel_dict)
+        print('elapsed:'+str(time.time()-start_time))
 
 
         print('testing nd w multilabel combined results:'+url)
+        start_time=time.time()
         multilabel_dict = nfc.pd(url, get_combined_results=True)
         assert(multilabel_dict is not None)
         print('combined dict:'+multilabel_dict)
+        print('elapsed:'+str(time.time()-start_time))
 
 
         print('testing nd w get all graylevels:'+url)
+        start_time=time.time()
         multilabel_dict = nfc.pd(url, get_all_graylevels=True)
         assert(multilabel_dict is not None)
         print('get_all_graylevels dict:'+multilabel_dict)
+        print('elapsed:'+str(time.time()-start_time))
 
 
     #run a timing test
