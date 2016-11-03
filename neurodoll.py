19380186a109e22b142c6cb0403529ba9e085cd5
jeremy <jeremy@trendiguru.com>
2016-11-03 12:13:25 +0200

    for nadav love jeremy

--- neurodoll.py
+++ neurodoll.py
@@ -419,13 +419,15 @@ def analyze_graylevels(url_or_np_array,labels=constants.ultimate_21):
     compressed_gl = cv2.resize(gl,(compressed_w,compressed_h))
     big_out = np.zeros([compressed_h*n_rows,compressed_w*n_rows])
     print('bigsize:'+str(big_out.shape))
+
     for i in range(5):
         for j in range(5):
+            n = i*n_rows+j
             if n>gl.shape[2]:
                 break
             print('y0 {} y1 {} x0 {} x1 {}'.format(i*h,(i+1)*h,j*w,(j+1)*w))
-            big_out[i*h:(i+1)*h,j*w:(j+1)*w] = compressed_gl[:,:,i*n_rows+j]
-    cv2.imshow('bigout',big_out)
+            big_out[i*h:(i+1)*h,j*w:(j+1)*w] = compressed_gl[:,:,n]
+            cv2.imshow('bigout',big_out)
 
 def get_all_category_graylevels_ineff(url_or_np_array,required_image_size=(256,256)):
     start_time = time.time()
