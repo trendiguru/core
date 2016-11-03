a3094060bd97a5e0755344ea670f64135d71cf0e
jeremy <jeremy@trendiguru.com>
2016-11-03 16:52:19 +0200

    for nadav love jeremy

--- neurodoll.py
+++ neurodoll.py
@@ -1031,7 +1031,9 @@ if __name__ == "__main__":
         for i in range(21):
             get_category_graylevel(url,category_index = i)
 
-    analyze_graylevels(urls[0])
+  #  analyze_graylevels(urls[0])
+    get_category_graylevel(urls[0],4)
+
     #get output of combine_nd_and_ml
     test_combine = False
     if test_combine:
