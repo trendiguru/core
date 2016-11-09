380ab37a4467c273a883011a9fa3458d5530fafe
jeremy <jeremy@trendiguru.com>
2016-11-09 16:55:31 +0200

    back to nd+ml

--- neurodoll.py
+++ neurodoll.py
@@ -1301,6 +1301,12 @@ def combine_neurodoll_and_multilabel(url_or_np_array,multilabel_threshold=0.7,me
     cv2.imwrite(graymask_filename,pixlevel_categorical_output)
     nice_output = imutils.show_mask_with_labels(graymask_filename,constants.ultimate_21,save_images=True,original_image=orig_filename,visual_output=test_on)
 
+    #save final_mask
+    final_mask_filename = orig_filename[:-4]+'finalmask.jpg'
+    print('finalmask file:'+graymask_filename)
+    cv2.imwrite(final_mask_filename,final_mask)
+    nice_output = imutils.show_mask_with_labels(final_mask_filename,constants.ultimate_21,save_images=True,original_image=orig_filename,visual_output=test_on)
+
     return final_mask
 
 
