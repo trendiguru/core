6027bfccc3b0545d44b224d54a80244af2bfa930
jeremy <jeremy@trendiguru.com>
2016-11-15 13:14:39 +0200

    get pd results on tb db (for training pixlevel)

--- classifier_stuff/get_pd_results_on_tamara_berg_db.py
+++ classifier_stuff/get_pd_results_on_tamara_berg_db.py
@@ -1,9 +1,11 @@
 __author__ = 'jeremy'
 
+import numpy as np
 
 from trendi.paperdoll import pd_falcon_client
 from trendi.utils import imutils
 from trendi import Utils,constants
+from trendi.paperdoll import pd
 import cv2
 
 def get_pd_results(url):
@@ -14,6 +16,15 @@ def get_pd_results(url):
     imgfilename = 'orig.jpg'
     cv2.imwrite(imgfilename,image)
     seg_res = pd_falcon_client.pd(image)
+    print('segres:'+str(seg_res))
+    mask = seg_res['mask']
+    label_dict = seg_res['label_dict']
+    pose = seg_res['pose']
+    mask_np = np.array(mask, dtype=np.uint8)
+    pose_np = np.array(pose, dtype=np.uint8)
+    pd.convert_and_save_results(mask_np, label_dict, pose_np, imgfilename, image, url)
+
+
     maskfilename = 'testout.png'
     cv2.imwrite(maskfilename,seg_res)
     imutils.show_mask_with_labels(maskfilename,constants.fashionista_categories_augmented_zero_based,original_image = imgfilename,save_images=True)
