72c7a5bf71db456eea0e8b9437a29e4a9079db29
Lior Sabag <lior@trendiguru.com>
2016-11-16 15:26:38 +0200

    Good to go

---
+++ features_api/classifier_falcon.py
@@ -0,0 +1,29 @@
+from importlib import import_module
+import falcon
+from jaweson import json, msgpack
+
+class Classifier(object):
+    def __init__(self, feature_name, package="trendi.features"):
+        self.feature = import_module(".{0}".format(feature_name), package)
+
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': 'I\'ve always been more interested in {0} than in the past.'.format(self.feature),
+            'author': 'Grace Hopper'
+        }
+
+        resp.body = json.dumps(quote)
+
+    def on_post(self, req, resp):
+        ret = {"success": False}
+	try:
+            data = msgpack.loads(req.stream.read())
+            image_or_url = data.get("image_or_url")
+            ret["data"] = self.feature.execute(image_or_url)
+            ret["success"] = True
+        except Exception as e:
+            ret["error"] = str(e)
+        resp.data = msgpack.dumps(ret)
+        resp.content_type = 'application/x-msgpack'
+        resp.status = falcon.HTTP_200
