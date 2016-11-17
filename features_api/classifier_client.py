628bcbbdcc2b2489ae62dc527c3088c19148baf7
Lior Sabag <lior@trendiguru.com>
2016-11-17 12:36:32 +0200

    Move to feature_config

--- features_api/classifier_client.py
+++ features_api/classifier_client.py
@@ -1,16 +1,8 @@
 from jaweson import msgpack
 import requests
-from .feature_config import FEATURES         
-
-def feature_url(feature):
-    return FEATURES[feature]["server"].rstrip('/') + "/" + feature
-
-for f in FEATURES:
-    f["url"] = feature_url(f)
+from .feature_config import FEATURES
 
 def get(feature, image_or_url, **kwargs):
     data = msgpack.dumps({"image_or_url": image_or_url}.update(kwargs))
     resp = requests.post(FEATURE[feature]["url"], data=data)
     return msgpack.loads(resp.content)
-
-
