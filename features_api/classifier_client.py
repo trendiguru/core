97d1c3ef5a7ba443cf5c5749474697fb0452f9b8
Lior Sabag <lior@trendiguru.com>
2016-11-17 12:42:40 +0200

    typo

--- features_api/classifier_client.py
+++ features_api/classifier_client.py
@@ -4,5 +4,5 @@ from .feature_config import FEATURES
 
 def get(feature, image_or_url, **kwargs):
     data = msgpack.dumps({"image_or_url": image_or_url}.update(kwargs))
-    resp = requests.post(FEATURE[feature]["url"], data=data)
+    resp = requests.post(FEATURES[feature]["url"], data=data)
     return msgpack.loads(resp.content)
