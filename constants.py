1a57e0d1be872341fbbd135ae4931bda5d4dfeb6
Lior Sabag <lior@trendiguru.com>
2016-11-21 19:26:32 +0200

    Move healthyceleb to ebay_US

--- constants.py
+++ constants.py
@@ -58,7 +58,7 @@ products_per_ip_pid = {'default':
                        "sg3SH5yif242E5jL":
                                  {'default': 'amazon_US'},
                        "Y8Y4jENvaJ2Lsklz":
-                                 {'default': 'amazon_US'},
+                                 {'default': 'ebay_US'},
                        }
 products_per_site = {'default': 'amazon_US', 'fashionseoul.com': 'GangnamStyle', 'fazz.co': 'amazon_US',
                      'stylebook.de': 'amazon_DE', 'recruit-lifestyle.co.jp': 'recruit'}
