602d2308791eb8cbe10d2c84ca7192beb9ec72d7
Lior Sabag <lior@trendiguru.com>
2016-11-22 22:20:40 +0200

    Add GPU_DEVICE

--- features_api/app.py
+++ features_api/app.py
@@ -11,13 +11,17 @@ from .classifier_falcon import Classifier
 # --env FEATURES_JSON='["collar", "sleeve"]'
 
 features = json.loads(os.environ.get("FEATURES_JSON", "[]"))
-feature_package_string = os.environ.get("FEATURE_PACKAGE", "trendi.features")
-if features is "*":
-    feature_package = import_module(feature_package_string)
-    features = pkgutil.iter_modules(os.path.dirname(feature_package.__file__))
+gpu_device = os.environ.get("GPU_DEVICE")
+gpu_device = int(gpu_device) if gpu_device else None
+
+# To allow loading multiple (all) features per gunicorn, deprecated for now.
+#feature_package_string = os.environ.get("FEATURE_PACKAGE", "trendi.features")
+#if features is "*":
+#    feature_package = import_module(feature_package_string)
+#    features = pkgutil.iter_modules(os.path.dirname(feature_package.__file__))
 
 api = falcon.API()
 for f in features:
     fpkg = import_module("." + f, feature_package_string)
     if hasattr(fpkg, 'execute') and hasattr(fpkg, 'distance'):
-        api.add_route('/{0}'.format(f), Classifier(f, feature_package_string))
+        api.add_route('/{0}'.format(f), Classifier(f, feature_package_string, gpu_device=gpu_device))
