0dc81750f7518310235b25cf339cbc54fc47d7bf
jeremy <jeremy@trendiguru.com>
2016-11-22 02:25:34 +0200

    pixel annotation falcon answering on brainib. remove finished masks from .json

--- classifier_stuff/caffe_nns/conversion_utils.py
+++ classifier_stuff/caffe_nns/conversion_utils.py
@@ -163,6 +163,7 @@ def gen_json(images_dir='data/pd_output',annotations_dir='data/pd_output',
     the_dict = {'labels': labels, 'imageURLs':[], 'annotationURLs':[]}
 
     for f in images:
+        print('looking at '+f)
         annotation_file = os.path.basename(f).replace('.jpg',mask_suffix)
         annotation_file = os.path.join(annotations_dir,annotation_file)
         if ignore_finished:
