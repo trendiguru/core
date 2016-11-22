757078b6f0fdcbf4afc3ed58ae1bde734f8aea14
Lior Sabag <lior@trendiguru.com>
2016-11-22 18:01:13 +0200

    Update to use new Feature class

--- features_api/classifier_falcon.py
+++ features_api/classifier_falcon.py
@@ -1,19 +1,18 @@
-from importlib import import_module
+import traceback
+# from importlib import import_module
 import falcon
 from jaweson import json, msgpack
-from ..features.config import FEATURES
 from ..features.feature import Feature
 
-
-class Classifier(object):
-    def __init__(self, feature_name, gpu_device=None, package="trendi.features"):
+class ClassifierResource(object):
+    def __init__(self, feature_name, gpu_device=None):
         # self.feature = import_module(".{0}".format(feature_name), package)
         self.feature = Feature(feature_name, gpu_device=gpu_device)
 
     def on_get(self, req, resp):
         """Handles GET requests"""
         quote = {
-            'quote': 'I\'ve always been more interested in {0} than in the past.'.format(self.feature),
+            'quote': 'I\'ve always been more interested in {0} than in the past.'.format(self.feature.name),
             'author': 'Grace Hopper'
         }
 
@@ -25,8 +24,11 @@ class Classifier(object):
             data = msgpack.loads(req.stream.read())
             ret["data"] = self.feature.execute(**data)
             ret["success"] = True
+            resp.status = falcon.HTTP_200
         except Exception as e:
             ret["error"] = str(e)
+            ret["trace"] = traceback.format_exc()
+        
         resp.data = msgpack.dumps(ret)
         resp.content_type = 'application/x-msgpack'
-        resp.status = falcon.HTTP_200
+        
