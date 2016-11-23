760f1e9ccc7bb3e8fe634327d6bd22d7f32c7321
Nadav <nadav@trendiguru.com>
2016-11-23 14:33:52 +0200

    **kwargs @get_feature_fp

--- fingerprint_core.py
+++ fingerprint_core.py
@@ -68,7 +68,7 @@ def get_feature_fp(feature, image, mask=None):
         print 'color'
         return color.execute(image, histograms_length, fingerprint_length, mask)
     else:
-        return classifier_client.get(feature, image, mask)
+        return classifier_client.get(feature, image, mask=mask)
     
 #    data = msgpack.dumps({"image_or_url": image, "mask": mask})
 #    resp = requests.post(FEATURES_CLIENT_ADDRESS+feature, data=data)
