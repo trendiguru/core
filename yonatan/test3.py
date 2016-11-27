48b3b5b2bb61d8c99461ca6e0a3423b13c456e57
yonatan <yonatanguy@gmail.com>
2016-11-27 19:21:15 +0200

    changed last layer from Softmaxwithloss to Softmax

--- yonatan/test3.py
+++ yonatan/test3.py
@@ -1,63 +1,56 @@
 #!/usr/bin/env python
 
-__author__ = 'yonatan_guy'
-
-import numpy as np
 import os
-import caffe
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import shutil
+import yonatan_constants
+from .. import background_removal, utils, constants
 import sys
+import skimage.io
+from scipy.ndimage import zoom
+from skimage.transform import resize
+import caffe
+from ..utils import imutils
 import argparse
 import glob
 import time
-from trendi import background_removal, Utils, constants
-import cv2
-import urllib
 import skimage
+import urllib
+from PIL import Image
+import pymongo
+import dlib
 import requests
-import yonatan_classifier
-
-# ## style ##
-MODLE_FILE = "/home/yonatan/trendi/yonatan/resnet_152_style/ResNet-152-deploy.prototxt"
-PRETRAINED = "/home/yonatan/style_classifier/style_second_try/resnet152_caffemodels_8_11_16/caffe_resnet152_snapshot_style_5_categories_iter_5000.caffemodel"
-# ## collar ##
-# MODLE_FILE = "/home/yonatan/trendi/yonatan/resnet_152_collar_type/ResNet-152-deploy.prototxt"
-# PRETRAINED = "/home/yonatan/collar_classifier/resnet152_caffemodels_6_11_16/caffe_resnet152_snapshot_collar_9_categories_iter_2500.caffemodel"
-
-# caffe.set_device(int(sys.argv[1]))
-caffe.set_device(3)
-
-caffe.set_mode_gpu()
-image_dims = [224, 224]
-mean, input_scale = np.array([104.0, 116.7, 122.7]), None
-channel_swap = [2, 1, 0]
-raw_scale = 255.0
-
-# Make classifier.
-classifier = yonatan_classifier.Classifier(MODLE_FILE, PRETRAINED,
-                              image_dims=image_dims, mean=mean,
-                              input_scale=input_scale, raw_scale=raw_scale,
-                              channel_swap=channel_swap)
-
-print "Done initializing!"
 
 
-def cv2_image_to_caffe(image):
-    return skimage.img_as_float(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).astype(np.float32)
+detector = dlib.get_frontal_face_detector()
 
 
-def distance(v1, v2):
-    if len(v1) != 8 or len(v2) != 8:
-        print "length of v1 or v2 is not 8!"
-        return None
-    return np.linalg.norm(v1 - v2)
+def person_isolation(image, face):
+    x, y, w, h = face
+    image_copy = np.zeros(image.shape, dtype=np.uint8)
+    x_back = np.max([x - 1.5 * w, 0])
+    x_ahead = np.min([x + 2.5 * w, image.shape[1] - 2])
+    image_copy[:, int(x_back):int(x_ahead), :] = image[:, int(x_back):int(x_ahead), :]
+    return image_copy
 
 
-def theDetector(url_or_np_array):
+def find_that_face(image, max_num_of_faces=10):
+    faces = detector(image, 1)
+    faces = [[rect.left(), rect.top(), rect.width(), rect.height()] for rect in list(faces)]
+    if not len(faces):
+        return {'are_faces': False, 'faces': []}
+    return {'are_faces': len(faces) > 0, 'faces': faces}
 
-    label = ''
 
+def crop_person_figure_by_face(url_or_np_array):
+
+    print "Starting the cropping!"
     # check if i get a url (= string) or np.ndarray
     if isinstance(url_or_np_array, basestring):
+        #full_image = url_to_image(url_or_np_array)
         response = requests.get(url_or_np_array)  # download
         full_image = cv2.imdecode(np.asarray(bytearray(response.content)), 1)
     elif type(url_or_np_array) == np.ndarray:
@@ -65,59 +58,24 @@ def theDetector(url_or_np_array):
     else:
         return None
 
+    #checks if the face coordinates are inside the image
     if full_image is None:
         print "not a good image"
         return None
 
-    image_for_caffe = [cv2_image_to_caffe(full_image)]
+    #resized_image = imutils.resize_keep_aspect(full_image, output_size=(124, 124))
 
-    if image_for_caffe is None:
+    faces = background_removal.find_face_dlib(full_image)
+
+    if not faces["are_faces"]:
+        print "didn't find any faces"
         return None
 
-    # Classify.
-    start = time.time()
-    predictions = classifier.predict(image_for_caffe)
-    print("Done in %.2f s." % (time.time() - start))
+    print faces["faces"][0] # just checking if the face that found seems in the right place
 
-    #max_result = max(predictions[0])
+    height, width, channels = full_image.shape
 
-    max_result_index = np.argmax(predictions[0])
+    person_figure = person_isolation(full_image, faces["faces"][0])
 
-    predict_label = int(max_result_index)
+    print cv2.imwrite('person_figure.jpg', person_figure)
 
-    ## style ##
-    if predict_label == 0:
-        label = 'swimsuit'
-    elif predict_label == 1:
-        label = 'sports'
-    elif predict_label == 2:
-        label = 'others'
-    elif predict_label == 3:
-        label = 'prom'
-    elif predict_label == 4:
-        label = 'bride_dress'
-
-    # ## collar ##
-    # if predict_label == 0:
-    #     label = 'crew_neck'
-    # elif predict_label == 1:
-    #     label = 'scoop_neck'
-    # elif predict_label == 2:
-    #     label = 'v_neck'
-    # elif predict_label == 3:
-    #     label = 'deep_v_neck'
-    # elif predict_label == 4:
-    #     label = 'Henley_t_shirts'
-    # elif predict_label == 5:
-    #     label = 'polo_collar'
-    # elif predict_label == 6:
-    #     label = 'tie_neck'
-    # elif predict_label == 7:
-    #     label = 'turtleneck'
-    # elif predict_label == 8:
-    #     label = 'Hooded_T_Shirt'
-
-
-    print "label: {0}".format(label)
-
-    print "prediction: {0}".format(predictions[0])
