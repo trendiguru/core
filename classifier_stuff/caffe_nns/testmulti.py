5df21852c2f8e17f2d5b82ecc7f2ef23f343e367
jeremy <jeremy@trendiguru.com>
2016-12-04 17:15:00 +0200

    look for  cause of orig images not sohwing up in pixlevel results

--- classifier_stuff/caffe_nns/testmulti.py
+++ classifier_stuff/caffe_nns/testmulti.py
@@ -1,5 +1,7 @@
 __author__ = 'jeremy'
 import multiprocessing
+from trendi import simple_pool
+import time
 
 def squared(x):
     return x**2
@@ -30,5 +32,50 @@ def test_multi(bsize):
     print theout1
     print theout2
 
+class snorb():
+    def msquared(self,x):
+        return x**2
+
+    def msquared_cubed(self,x):
+        return x**2,x**3
+
+    def mnoargs(self):
+        time.sleep(1)
+        return (3**2,3**3)
+
+    def mhelpnoargs(self,x):
+        r=self.mnoargs()
+        time.sleep(1)
+
+#        resq.put(r)
+        #return r
+
+    def test_multi(self,bsize):
+    #    bsize = 4
+
+
+        ins = range(5,bsize+5)
+
+        print('ins:'+str(ins))
+        outs = simple_pool.map2(self.mhelpnoargs, ins,ouput_ctype=(int))
+#        outs = pool.map(self.msquared_cubed, ins)
+        print('outs:'+str(outs))
+
+        # result_queue = multiprocessing.Queue(10)
+        # jobstodo = [self.mhelpnoargs(result_queue, fargs) for fargs in ins]
+        # jobs = [multiprocessing.Process(j) for j in jobstodo]
+        # for job in jobs: job.start()
+        # for job in jobs: job.join()
+        # results = [result_queue.get() for j in jobstodo]
+        # print results
+
+
+
+
+
 if __name__ == "__main__":
-    test_multi(6)
+#    test_multi(6)
+    start=time.time()
+    s=snorb()
+    s.test_multi(5)
+    print('elapsed time'+str(time.time()-start))
--- simple_pool.py
+++ simple_pool.py
@@ -19,10 +19,38 @@ def map(f, inputs, num_processes=None, ouput_ctype=None):
         [return_values.append(o.value) for o in outputs]
     return return_values
 
+import numpy as np
+
+def map2(f, inputs, num_processes=None, ouput_ctype=None):
+    ouput_ctype = ouput_ctype or ctypes.c_bool
+    num_processes = num_processes or int(0.75 * cpu_count())
+    vars1=range(num_processes)
+    vars2=range(num_processes)
+    #doesnt work since i need two numpy arrays to be returned
+    outputs = [Value(ctypes.c_bool) for i in xrange(num_processes)]
+    #doesnt work since its pass by value
+    outputs = [[vars1[i],vars2[i]] for i in xrange(num_processes)]
+    #doesnt work since 'this type has no size'
+#    outputs = [[Value(np.array),Value(np.array)] for i in xrange(num_processes)]
+    chunked_inputs = [inputs[i:i + num_processes] for i in xrange(0, len(inputs), num_processes)]
+    return_values = []
+    for input_chunk in chunked_inputs:
+        processes = [Process(target=worker2, args=(f, input_chunk[i], outputs[i]))
+                     for i in xrange(len(input_chunk))]
+        [p.start() for p in processes]
+        [p.join() for p in processes]
+        [return_values.append(o) for o in outputs]
+    return return_values
 
 def worker(f, args, output):
     output.value = f(*args)
 
+def worker2(f, args, output):
+#    output[0],output[1] = f(args)
+#    output.value = f(args)
+    output = f(args)
+    print('output {} args {}'.format(output,args))
+    return output
 
 def check_small(input1, input2):
     print "Will wait then work"
