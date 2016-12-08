3429619147a38d94c27a5497db1e958ce0e8ffe3
jeremy <jeremy@trendiguru.com>
2016-12-08 17:28:32 +0200

    class a vs. class b

--- classifier_stuff/caffe_nns/mnc_utils.py
+++ classifier_stuff/caffe_nns/mnc_utils.py
@@ -3,6 +3,15 @@ import cPickle
 import os
 import numpy as np
 
+
+#jeremy attempts to make .pkl files for use with MNC
+#see https://github.com/daijifeng001/MNC/issues/5
+#...yes, you need to implement MNC/pascal_voc_det.py
+##in gt_overlap, it is 'overlap between classes'.#
+##For the mask_max, this is because, I want to pass the mask before re-size to 21x21 to top layers. At this stage, mask size will be num_mask x 1 x mask_height x mask_width.#
+##But different masks have different h/w, so I generate a blob according to the max value (num_mask x 1 x max(mask_height) x max(mask_width)) over each mask and pad with 0.
+
+
 def checkout_pkl_file(thefile):
     if os.path.exists(thefile):
         with open(thefile, 'rb') as fid:
@@ -57,9 +66,9 @@ def checkout_mask_pkl_file(thefile):
         max0=0
         max1=0
         for m in gt_masks:
-            gt0 = m[0]
+            gt0 = m.shape[0]
             max0=gt0 if gt0>max0 else max0
-            gt1 = m[1]
+            gt1 = m.shape[1]
             max1=gt1 if gt1>max1 else max1
         count += 1
         print('calculated max {},{}'.format(max0,max1))
