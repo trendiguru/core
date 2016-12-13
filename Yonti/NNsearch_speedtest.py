03e5ad9726ff63984e1db9b1ca425f2a7c309f4b
yonti <yontilevin@gmail.com>
2016-12-13 18:20:15 +0200

    speedNN

--- Yonti/NNsearch_speedtest.py
+++ Yonti/NNsearch_speedtest.py
@@ -1,4 +1,21 @@
 import timeit
-import random
+import numpy as np
+import pymongo
+from ..constants import db
 
+
+collection = 'amazon_US_Female'
+annoy_top_results = np.random.randint(10000, size=100)
+category = 'dress'
+
+
+def test1():
+    entries = db[collection].find({"AnnoyIndex": {"$in": annoy_top_results}, 'categories': category},
+                                  {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
+                                  cursor_type=pymongo.cursor.CursorType.EXHAUST)
+
+    for i in entries:
+        print i
+
+timeit.timeit(test1)
 
