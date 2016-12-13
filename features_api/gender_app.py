d6ab0ece5ac1c2c73ff034c19f972be063a03d5f
jeremy <jeremy@trendiguru.com>
2016-12-13 23:42:15 +0200

    gender app

---
+++ features_api/gender_app.py
@@ -0,0 +1,39 @@
+__author__ = 'yonatan'
+
+import falcon
+import numpy as np
+from jaweson import json, msgpack
+from ..yonatan import new_genderDetector
+
+
+class NeuralResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': 'I\'ve always been more interested in the future than in the past.',
+            'author': 'Grace Hopper'
+        }
+
+        resp.body = json.dumps(quote)
+
+    def on_post(self, req, resp):
+        ret = {"success": False}
+        try:
+            data = msgpack.loads(req.stream.read())
+            image = data.get("image")
+            face = data.get("face")
+            ret["gender"] = new_genderDetector.theDetector(image, face)
+            if ret["gender"] is not None:
+                ret["success"] = True
+            else:
+                ret["error"] = "NN returned None"
+        except Exception as e:
+            ret["error"] = str(e)
+
+        resp.data = msgpack.dumps(ret)
+        resp.content_type = 'application/x-msgpack'
+        resp.status = falcon.HTTP_200
+
+
+api = falcon.API()
+api.add_route('/gender', NeuralResource())
