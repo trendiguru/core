d0a6395e640014f370341899259d6b0d2583194c
jeremy rutman <jeremy.rutman@yahoo.com>
2016-12-14 01:29:09 +0200

    Update deployment_config.py

--- features_api/deployment_config.py
+++ features_api/deployment_config.py
@@ -5,6 +5,10 @@ DEPLOYMENTS =  {"collar":{"server":"http://37.58.101.173:8085"},
              "dress_texture": {"server": "http://37.58.101.173:8082"},
              "gender":{"server":"http://37.58.101.173:8357"}}
 
+#currently gender is started differently 
+#gunicorn -b :8357 -w 5 -k gevent -n gender app:api
+#and also it needs dlib, so you need to do pip install dlib in the container
+
 # Generate url, by adding /{feature} to the end of server url
 for feature, depl in DEPLOYMENTS.iteritems():
     depl["url"] = depl["server"].rstrip('/') + "/" + feature
