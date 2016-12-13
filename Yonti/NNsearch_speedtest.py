339738b1132d632562cd8feba4f3136c9c7516d2
yonti <yontilevin@gmail.com>
2016-12-13 19:19:56 +0200

    speedNN

--- Yonti/NNsearch_speedtest.py
+++ Yonti/NNsearch_speedtest.py
@@ -2,7 +2,8 @@ from time import time
 import numpy as np
 import pymongo
 from ..constants import db
-
+import gevent
+from gevent import Greenlet
 
 collection = 'amazon_US_Female'
 category = 'dress'
@@ -14,137 +15,85 @@ def timeit(f, number, name='function'):
     annoy_top_results = list(np.random.randint(480000, size=1000))
 
     t1 = time()
-    for i in range(number):
-        f()
+    f(number)
     t2 = time()
     print '{} duration = {}'.format(name,(t2-t1)/float(number))
 
 
-def test1():
-    """with exhaust"""
-    entries = db[collection].find({"AnnoyIndex": {"$in": annoy_top_results}, 'categories': category},
+def withH(b):
+    """with exhaust - divided to 10 subgroups"""
+    bs = 1000/b
+    for i in range(b):
+        small_list = annoy_top_results[i*bs:(i+1)*bs]
+        len(annoy_top_results)
+        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
+                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
+                                      cursor_type=pymongo.cursor.CursorType.EXHAUST)
+        for i in entries:
+            pass
+
+
+def without(b):
+    """without exhaust - divided to 10 subgroups"""
+
+    bs = 1000 / b
+    for i in range(b):
+        small_list = annoy_top_results[i * bs:(i + 1) * bs]
+        len(annoy_top_results)
+        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
+                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
+        for i in entries:
+            pass
+
+
+def get_batchWH(batch):
+    entries = db[collection].find({"AnnoyIndex": {"$in": batch}, 'categories': category},
                                   {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
                                   cursor_type=pymongo.cursor.CursorType.EXHAUST)
     for i in entries:
         pass
 
+    return {'d':'done'}
 
-def test2():
-    """without exhaust"""
-    entries = db[collection].find({"AnnoyIndex": {"$in": annoy_top_results}, 'categories': category},
+
+def get_batchWO(batch):
+    entries = db[collection].find({"AnnoyIndex": {"$in": batch}, 'categories': category},
                                   {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
     for i in entries:
         pass
 
 
-def test3():
-    """with exhaust - divided to 10 subgroups"""
-
-    for i in range(10):
-        small_list = annoy_top_results[i*100:(i+1)*100]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
-                                      cursor_type=pymongo.cursor.CursorType.EXHAUST)
-        for i in entries:
-            pass
+    return {'d': 'done'}
 
 
-def test4():
-    """without exhaust - divided to 10 subgroups"""
-
-    for i in range(10):
-        small_list = annoy_top_results[i * 100:(i + 1) * 100]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
-        for i in entries:
-            pass
+def diviWH(b):
+    bs = 1000/b
+    queries = {q: Greenlet.spawn(get_batchWH, annoy_top_results[q*bs:(q+1)*bs]) for q in range(b)}
+    gevent.joinall(queries.values())
+    d = {k: v for k, v in queries.iteritems()}
 
 
-def test5():
-    """with exhaust - divided to 20 subgroups"""
-
-    for i in range(20):
-        small_list = annoy_top_results[i * 50:(i + 1) * 50]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
-                                      cursor_type=pymongo.cursor.CursorType.EXHAUST)
-        for i in entries:
-            pass
+def diviWO(b):
+    bs = 1000 / b
+    queries = {q: Greenlet.spawn(get_batchWO, annoy_top_results[q * bs:(q + 1) * bs]) for q in range(b)}
+    gevent.joinall(queries.values())
+    d = {k: v for k, v in queries.iteritems()}
 
 
-def test6():
-    """without exhaust - divided to 20 subgroups"""
+timeit(withH, number=1, name='with EXHAUST')
+timeit(without, number=1, name='without EXHAUST')
+timeit(withH, number=10, name='with EXHAUST /10')
+timeit(without, number=10, name='without EXHAUST /10')
+timeit(withH, number=20, name='with EXHAUST /20')
+timeit(without, number=20, name='without EXHAUST /20')
+timeit(withH, number=50, name='with EXHAUST /50')
+timeit(without, number=50, name='without EXHAUST /50')
+timeit(withH, number=100, name='with EXHAUST /100')
+timeit(without, number=100, name='without EXHAUST /100')
 
-    for i in range(20):
-        small_list = annoy_top_results[i * 50:(i + 1) * 50]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
-        for i in entries:
-            pass
-
-
-def test7():
-    """with exhaust - divided to 50 subgroups"""
-
-    for i in range(50):
-        small_list = annoy_top_results[i * 20:(i + 1) * 20]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
-                                      cursor_type=pymongo.cursor.CursorType.EXHAUST)
-        for i in entries:
-            pass
+timeit(diviWH, number=10, name='divi with EXHAUST /10')
+timeit(diviWO, number=10, name='divi without EXHAUST /10')
+timeit(diviWH, number=50, name='divi with EXHAUST /50')
+timeit(diviWO, number=50, name='divi without EXHAUST /50')
 
 
-def test8():
-    """without exhaust - divided to 50 subgroups"""
-
-    for i in range(50):
-        small_list = annoy_top_results[i * 20:(i + 1) * 20]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
-        for i in entries:
-            pass
-
-
-def test9():
-    """with exhaust - divided to 100 subgroups"""
-
-    for i in range(100):
-        small_list = annoy_top_results[i * 10:(i + 1) * 10]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1},
-                                      cursor_type=pymongo.cursor.CursorType.EXHAUST)
-        for i in entries:
-            pass
-
-
-def test10():
-    """without exhaust - divided to 100 subgroups"""
-
-    for i in range(100):
-        small_list = annoy_top_results[i * 10:(i + 1) * 10]
-        len(annoy_top_results)
-        entries = db[collection].find({"AnnoyIndex": {"$in": small_list}, 'categories': category},
-                                      {"id": 1, "fingerprint": 1, "images.XLarge": 1, "clickUrl": 1})
-        for i in entries:
-            pass
-
-n=1
-
-timeit(test2, number=n, name='without EXHAUST')
-timeit(test3, number=n, name='with EXHAUST /10')
-timeit(test4, number=n, name='without EXHAUST /10')
-timeit(test5, number=n, name='with EXHAUST /20')
-timeit(test6, number=n, name='without EXHAUST /20')
-timeit(test7, number=n, name='with EXHAUST /50')
-timeit(test8, number=n, name='without EXHAUST /50')
-timeit(test9, number=n, name='with EXHAUST /100')
-timeit(test10, number=n, name='without EXHAUST /100')
-timeit(test1, number=n, name='with EXHAUST')
