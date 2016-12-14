33f7d3ca4e548dfa742f4f47df05b903b88e6923
yonti <yontilevin@gmail.com>
2016-12-14 11:07:53 +0200

    speedNN

--- Yonti/NNsearch_speedtest.py
+++ Yonti/NNsearch_speedtest.py
@@ -7,15 +7,15 @@ from gevent import Greenlet
 import random
 
 collection = 'amazon_US_Female'
-category = 'dress'
-annoy_top_results = list(np.random.randint(440000, size=1000))
+category = 'skirt'
+annoy_top_results = list(np.random.randint(44000, size=1000))
 annoy_new = ['skirt_{}'.format(i) for i in annoy_top_results]
 
 def timeit(f, number, name='function'):
     global annoy_top_results, annoy_new
     rndstate = random.getstate()
     random.setstate(rndstate)
-    annoy_top_results = list(np.random.randint(480000, size=1000))
+    annoy_top_results = list(np.random.randint(44000, size=1000))
     annoy_new = ['skirt_{}'.format(i) for i in annoy_top_results]
     t1 = time()
     f(number)
