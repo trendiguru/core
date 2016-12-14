2f56f0486f80826325ac85509222bd8576bbcffa
jeremy <jeremy@trendiguru.com>
2016-12-14 03:40:06 +0200

    gender app

--- features_api/unit_test_features.py
+++ features_api/unit_test_features.py
@@ -2,6 +2,10 @@ import unittest
 
 from trendi.features_api import classifier_client
 
+#for face detection
+from trendi.yonatan import cropping
+
+
 class OutcomesTest(unittest.TestCase):
     # examples of things to return
     # def testPass(self):
@@ -13,20 +17,30 @@ class OutcomesTest(unittest.TestCase):
 
 
     def setUp(self):
-
-        pass
+        self.urls = ['http://i1.adis.ws/s/boohooamplience/azz51307_ms.jpg',
+  'https://static.missguided.co.uk/media/catalog/product/cache/3/image/600x870/9df78eab33525d08d6e5fb8d27136e95/h/i/high_neck_lace_dress_kirstie_07.07.16_hm_144201_a.jpg' ]
 
 
     def test_features(self):
 
-        urls = ['http://i1.adis.ws/s/boohooamplience/azz51307_ms.jpg',
-  'https://static.missguided.co.uk/media/catalog/product/cache/3/image/600x870/9df78eab33525d08d6e5fb8d27136e95/h/i/high_neck_lace_dress_kirstie_07.07.16_hm_144201_a.jpg' ]
-        features = ['collar','sleeve_length','length','style','dress_texture','gender']
-        for url in urls:
+        features = ['collar','sleeve_length','length','style','dress_texture']
+        for url in self.urls:
             for feature in features:
                 result = classifier_client.get(feature,url)
                 print('result for {} on {} is {}'.format(feature,url,result))
                 print
+
+
+    def test_gender(self):
+
+        urls = ['http://i1.adis.ws/s/boohooamplience/azz51307_ms.jpg',
+  'https://static.missguided.co.uk/media/catalog/product/cache/3/image/600x870/9df78eab33525d08d6e5fb8d27136e95/h/i/high_neck_lace_dress_kirstie_07.07.16_hm_144201_a.jpg' ]
+        features = ['collar','sleeve_length','length','style','dress_texture']
+        for url in self.urls:
+            face = cropping.find_that_face(url,1)
+            result = classifier_client.get('gender',url,{'face':face})
+            print('result for gender on {} is {}'.format(url,result))
+
 
 if __name__ == "__main__":
     unittest.main()
