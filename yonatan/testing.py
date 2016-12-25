76dd9328c31c1293bdbd958cc560316a95b969d8
yonatan <yonatanguy@gmail.com>
2016-12-25 18:16:43 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	features/config.py

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -14,8 +14,11 @@ with open('/data/jeremy/image_dbs/deep_fashion/category_and_attribute_prediction
         # print line.split('  ')
         match = re.match(r"([0-9]+)", line, re.I)
         if match:
+            print 'YES'
             items = match.groups()
             print items
+        else:
+            print "NO"
 
 # attribute_txt_file = open('/data/jeremy/image_dbs/deep_fashion/category_and_attribute_prediction/list_attr_cloth.txt', 'r')
 
