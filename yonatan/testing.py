770a0a4983c4ec990f7081720cb4ad7ecf30fb4c
yonatan <yonatanguy@gmail.com>
2016-12-25 17:31:31 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	features/config.py

--- yonatan/testing.py
+++ yonatan/testing.py
@@ -2,16 +2,26 @@
 
 __author__ = 'yonatan_guy'
 
+import re
 
 
 
 
-with open('/data/jeremy/image_dbs/deep_fashion/category_and_attribute_prediction/list_attr_cloth.txt', 'r') as handle:
-    for line in handle:
-        print line.split('  ')
 
 
-# train_text_file = open("/home/yonatan/faces_stuff/55k_face_train_list.txt", "r")
-#
-#
+# with open('/data/jeremy/image_dbs/deep_fashion/category_and_attribute_prediction/list_attr_cloth.txt', 'r') as handle:
+#     for line in handle:
+#         print line.split('  ')
+
+attribute_txt_file = open('/data/jeremy/image_dbs/deep_fashion/category_and_attribute_prediction/list_attr_cloth.txt', 'r')
+
+
+match = re.match(r"([a-z]+)([0-9]+)", attribute_txt_file, re.I)
+if match:
+    items = match.groups()
+    print items
+    # items is ("foo", "21")
+
+
+
 # train_text_file.write("\"" + i + "\": [\'" + attribute + "\', \'" + type + "\']")
