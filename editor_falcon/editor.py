6b236dde2ca772f15c4ac1c5391ea867afce1c9a
Lior Sabag <lior@trendiguru.com>
2017-01-04 17:52:45 +0200

    Try to fix double logging

--- editor_falcon/editor.py
+++ editor_falcon/editor.py
@@ -7,13 +7,13 @@ from bson import json_util
 import logging
 import sys
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root.addHandler(ch)
+# root = logging.getLogger()
+# root.setLevel(logging.DEBUG)
+# ch = logging.StreamHandler(sys.stdout)
+# ch.setLevel(logging.DEBUG)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# ch.setFormatter(formatter)
+# root.addHandler(ch)
 USER_FILTER = 'stylebook'
 db = constants.db
 
