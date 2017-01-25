4303ec8c041f13eacde5afb67e1b383ebd792c82
yonti <yontilevin@gmail.com>
2017-01-25 20:19:12 +0200

    shopstyle

--- db_stuff/shutterstock/shutterstock_image_api.py
+++ db_stuff/shutterstock/shutterstock_image_api.py
@@ -128,12 +128,15 @@ def divide_dates(pair):
     start_year, start_month, start_day = start_date
     end_year, end_month, end_day = end_date
     mid_date = ((end_year+start_year)/2, (end_month+start_month)/2, (end_day+start_day)/2)
-    pair1 = (start_date, mid_date)
     if mid_date == start_date:
         print (mid_date)
         if mid_date[1] < 12 and mid_date[1] < end_date[1]+1:
             mid_date = (mid_date[0], mid_date[1] + 1, mid_date[2])
+            pair1 = (start_date, mid_date)
+
         elif mid_date[2] < 28 and mid_date[2] < end_date[2]+1:
+
+            pair1 = (start_date, mid_date)
             mid_date = (mid_date[0], mid_date[1], mid_date[2] + 1)
         else:
             pair1 = ()
