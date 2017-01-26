707cc7c98b4af345e5b938908fd00abbe734431f
jeremy <jeremy@trendiguru.com>
2017-01-26 18:40:41 +0200

    dont flush

--- downloaders/categorical_dl.py
+++ downloaders/categorical_dl.py
@@ -271,8 +271,7 @@ def simple_cat_dl(cat,collection,db,dl=True,dl_dir='./',use_visual_output=False,
             print('doc not being saved')
         print('attempts {}/{} success {} un {} no_images {} nodoc {} nosize {} nhash {} n_already {}'
               .format(attempts,count,succesful_dl,unsuccesful_dl,no_images,no_doc,no_known_size,n_hashed_names,n_already_dl))
-    dict=
-        {'cat':cat,
+    dict={'cat':cat,
         'collection':collection,
         'attempts':attempts,
         'succesful_dl',succesful_dl,
