d8c087dd261997d9c2a773214ee1433fcbb97521
Lior Sabag <lior@trendiguru.com>
2017-01-30 00:26:24 +0200

    Typo

--- new_image_notifier.py
+++ new_image_notifier.py
@@ -4,7 +4,7 @@ from .constants import db
 
 SLACK_URL = "https://hooks.slack.com/services/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"
 
-def notify_new_images(query):
+def notify_new_image(query):
     im = db.images.find_one(query, {"image_urls":1, "page_urls":1})
     message = "New <{0}|image> processed on page <{1}|page>".format(im["image_urls"][-1], im["page_urls"][-1])
     send_message(message)
