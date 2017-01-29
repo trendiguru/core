18646ea6adf5a09b7a8b5a2623492be8d229adde
yonatan <yonatanguy@gmail.com>
2017-01-29 13:17:13 +0200

    update relevant_items in config.py

--- yonatan/grabCut.py
+++ yonatan/grabCut.py
@@ -31,7 +31,6 @@ def grabcut(url_or_np_array):
     bgdModel = np.zeros((1 ,65) ,np.float64)
     fgdModel = np.zeros((1 ,65) ,np.float64)
 
-
     cv2.grabCut(img ,mask ,rect ,bgdModel ,fgdModel ,5 ,cv2.GC_INIT_WITH_RECT)
 
     mask2 = np.where(( mask ==2 ) |( mask ==0) ,0 ,1).astype('uint8')
