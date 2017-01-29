f657fd1ec79f7652a67bc1375e3b3a619b5b38c4
Lior Sabag <lior@trendiguru.com>
2017-01-30 00:16:29 +0200

    Create new_image_notifier

---
+++ new_image_notifier.py
@@ -0,0 +1,14 @@
+import requests
+from bson import json_util as json
+from .constants import db
+
+SLACK_URL = "https://hooks.slack.com/services/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"
+
+def notify_new_images(query):
+    im = db.images.find_one(query, {"image_urls":1, "page_urls":1})
+    message = "New <{0}|image> processed on page <{1}|page>".format(im["image_urls"][-1], im["page_urls"][-1])
+    send_message(message)
+
+def send_message(msg):
+    payload = {"text": msg}
+    resp = requests.post(SLACK_URL, data=json.dumps(payload))
