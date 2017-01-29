d94c2161d189550f8ba98e6b80030c42702751f6
yonatan <yonatanguy@gmail.com>
2017-01-29 13:26:53 +0200

    update relevant_items in config.py

--- yonatan/grabCut.py
+++ yonatan/grabCut.py
@@ -23,7 +23,8 @@ def grabcut(url_or_np_array):
         return None
 
     print "image.shape: {0}\nimage.shape[0]: {1}\nimage.shape[1]: {2}".format(img.shape, img.shape[0], img.shape[1])
-    rect = (1, 1, img.shape[1] - 1, img.shape[0] - 1)
+    rect = (5, 5, img.shape[1] - 5, img.shape[0] - 5)
+    ## rect is in the form of (x, y, w, h)
     # rect = sys.argv[2]
 
     mask = np.zeros(img.shape[:2] ,np.uint8)
