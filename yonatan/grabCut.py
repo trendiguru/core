5412bb5b2726b2c6b9debff75c24df7868fb4086
yonatan <yonatanguy@gmail.com>
2017-01-29 12:09:16 +0200

    update relevant_items in config.py

--- yonatan/grabCut.py
+++ yonatan/grabCut.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import requests
 
 # def grabcut(img):
-# 
+#
 #     img = cv2.imread(img)
 
 
@@ -22,22 +22,24 @@ def grabcut(url_or_np_array):
     else:
         return None
 
-    rect = img.shape
+
+    rect = (1,2,3,4)
     # rect = sys.argv[2]
 
-    mask = np.zeros(img.shape[:2],np.uint8)
+    mask = np.zeros(img.shape[:2] ,np.uint8)
 
-    bgdModel = np.zeros((1,65),np.float64)
-    fgdModel = np.zeros((1,65),np.float64)
+    bgdModel = np.zeros((1 ,65) ,np.float64)
+    fgdModel = np.zeros((1 ,65) ,np.float64)
 
 
-    cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+    cv2.grabCut(img ,mask ,rect ,bgdModel ,fgdModel ,5 ,cv2.GC_INIT_WITH_RECT)
 
-    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-    img = img*mask2[:,:,np.newaxis]
+    mask2 = np.where(( mask ==2 ) |( mask ==0) ,0 ,1).astype('uint8')
+    img = img* mask2[:, :, np.newaxis]
 
     # plt.imshow(img),plt.colorbar(),plt.show()
     # cv2.imwrite("/data/yonatan/yonatan_files/grabcut_image.jpg", img)
 
     print cv2.imwrite("/data/yonatan/linked_to_web/grabcut_testing.jpg", img)
     # return img
+
