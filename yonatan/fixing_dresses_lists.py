33a4104bab6b93d5dc49f47cd5ebe8b709affe95
yonatan <yonatanguy@gmail.com>
2017-02-05 14:39:40 +0200

    update relevant_items in config.py

--- yonatan/fixing_dresses_lists.py
+++ yonatan/fixing_dresses_lists.py
@@ -102,6 +102,7 @@ print "Done with images_new!!"
 #
 # text_file.flush()
 
+counter_bad = 0
 
 for root, dirs, files in os.walk('/data/dress_detector/resized_images'):
     for file in files:
@@ -110,12 +111,19 @@ for root, dirs, files in os.walk('/data/dress_detector/resized_images'):
 
         boxes_new.append(line_in_list_boxes)
 
+        try:
+            line_in_list_images = io.imread('/data/dress_detector/resized_images/' + file)
+        except:
+            print "bad image!!"
+            counter_bad += 1
+            continue
 
-        line_in_list_images = io.imread('/data/dress_detector/resized_images/' + file)
 
         images_new.append(line_in_list_images)
 
         print file
+
+print "counter_bad: {0}".format(counter_bad)
 
 # np.array(boxes_new).dump(open('/data/dress_detector/boxes_small_set.npy', 'wb'))
 #
