765f809dd97023176185049eab3914411dd42960
yonatan <yonatanguy@gmail.com>
2017-02-08 18:51:56 +0200

    update relevant_items in config.py

--- yonatan/train_object_detector.py
+++ yonatan/train_object_detector.py
@@ -67,7 +67,7 @@ options = dlib.simple_object_detector_training_options()
 # Since faces are left/right symmetric we can tell the trainer to train a
 # symmetric detector.  This helps it get the most value out of the training
 # data.
-options.add_left_right_image_flips = False
+options.add_left_right_image_flips = True
 # The trainer is a kind of support vector machine and therefore has the usual
 # SVM C parameter.  In general, a bigger C encourages it to fit the training
 # data better but might lead to overfitting.  You must find the best C value
