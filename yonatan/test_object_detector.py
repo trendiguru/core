4953225b939b89d8089384aedec4a3054e350255
yonatan <yonatanguy@gmail.com>
2017-02-08 18:27:28 +0200

    update relevant_items in config.py

--- yonatan/test_object_detector.py
+++ yonatan/test_object_detector.py
@@ -141,27 +141,27 @@ def theDetector(url_or_np_array):
     # center = (w / 2, h / 2)
     # M = cv2.getRotationMatrix2D(center, 90, 1.0)
 
-    rotate_image = np.rot90(full_image, 1)
-    rotate_image2 = np.rot90(full_image, 3)
-
-    resized_image = imutils.resize_keep_aspect(full_image, output_size=(300, 300))
+    # rotate_image = np.rot90(full_image, 1)
+    # rotate_image2 = np.rot90(full_image, 3)
+    #
+    # resized_image = imutils.resize_keep_aspect(full_image, output_size=(300, 300))
 
     dets = find_dress_dlib(full_image)
-    dets2 = find_dress_dlib(rotate_image)
-    dets3 = find_dress_dlib(rotate_image2)
-    dets4 = find_dress_dlib(resized_image)
-
-    for i in range(0, len(dets)):
-        print "left: {0}, top: {1}, right: {2}, bottom: {3} -- regular".format(dets[i].left(), dets[i].top(), dets[i].right(), dets[i].bottom())
-
-    for i in range(0, len(dets2)):
-        print "left: {0}, top: {1}, right: {2}, bottom: {3} -- rotate90".format(dets2[i].left(), dets2[i].top(), dets2[i].right(), dets2[i].bottom())
-
-    for i in range(0, len(dets3)):
-        print "left: {0}, top: {1}, right: {2}, bottom: {3} -- rotate270".format(dets3[i].left(), dets3[i].top(), dets3[i].right(), dets3[i].bottom())
-
-    for i in range(0, len(dets4)):
-        print "left: {0}, top: {1}, right: {2}, bottom: {3} -- resize".format(dets4[i].left(), dets4[i].top(), dets4[i].right(), dets4[i].bottom())
+    # dets2 = find_dress_dlib(rotate_image)
+    # dets3 = find_dress_dlib(rotate_image2)
+    # dets4 = find_dress_dlib(resized_image)
+    #
+    # for i in range(0, len(dets)):
+    #     print "left: {0}, top: {1}, right: {2}, bottom: {3} -- regular".format(dets[i].left(), dets[i].top(), dets[i].right(), dets[i].bottom())
+    #
+    # for i in range(0, len(dets2)):
+    #     print "left: {0}, top: {1}, right: {2}, bottom: {3} -- rotate90".format(dets2[i].left(), dets2[i].top(), dets2[i].right(), dets2[i].bottom())
+    #
+    # for i in range(0, len(dets3)):
+    #     print "left: {0}, top: {1}, right: {2}, bottom: {3} -- rotate270".format(dets3[i].left(), dets3[i].top(), dets3[i].right(), dets3[i].bottom())
+    #
+    # for i in range(0, len(dets4)):
+    #     print "left: {0}, top: {1}, right: {2}, bottom: {3} -- resize".format(dets4[i].left(), dets4[i].top(), dets4[i].right(), dets4[i].bottom())
 
     for d in dets:
         if d.left() < 0:
