b0fcadb8cd79445c17ca0561233ab92d7f3ecc85
Lior Sabag <lior@trendiguru.com>
2017-02-09 16:57:07 +0200

    Add default_collection system.

--- editor_falcon/temp_editor_users.py
+++ editor_falcon/temp_editor_users.py
@@ -5,11 +5,13 @@ USERS = {
     "john@john.co":
     {
         "email": "john@john.co",
-        "password": sha256_crypt.encrypt("john_is_cool")
+        "password": sha256_crypt.encrypt("john_is_cool"),
+        "default_collection": "shopstyle_US"
     },
     "stylebook@stylebook.de":
     {
         "email": "stylebook@stylebook.de",
-        "password": sha256_crypt.encrypt("stylebook")
+        "password": sha256_crypt.encrypt("stylebook"),
+        "default_collection": "shopstyle_DE"
     }
 }
