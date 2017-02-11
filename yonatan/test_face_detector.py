3618cf9fb4db1822eff83e1c175e11c36663520a
yonatan <yonatanguy@gmail.com>
2017-02-12 00:34:00 +0200

    update relevant_items in config.py

--- yonatan/test_face_detector.py
+++ yonatan/test_face_detector.py
@@ -69,7 +69,10 @@ def theDetector(url_or_np_array):
 
     # faces = background_removal.find_face_dlib(full_image)
 
-    # faces = find_face_dlib(full_image, 10)
+    faces0 = find_face_dlib(full_image, 10)
+    print "len(faces0) without restrictions: {0}".format(len(faces0['faces']))
+
+
     faces = background_removal.find_face_dlib(full_image, 4)
 
     print "len(faces): {0}".format(len(faces['faces']))
