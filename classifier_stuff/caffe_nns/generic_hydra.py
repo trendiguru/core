457fff1e5551203c8a4df6ee9cb38d9465156c75
jeremy <jeremy@trendiguru.com>
2017-02-13 12:34:01 +0200

    keep common last layer (dont add __n)

--- classifier_stuff/caffe_nns/generic_hydra.py
+++ classifier_stuff/caffe_nns/generic_hydra.py
@@ -63,7 +63,7 @@ def create_new_proto(names, lastlayer, output_name):
     '''
     new_proto = open(output_name, 'w')
 
-    for i in range(len(names)):
+    for i in range(len(names)):#
 #        f = open('{}.prototxt'.format(names[i]), 'r')
         f = open(names[i], 'r')
         print('getting layers from {}'.format(names[i]))
