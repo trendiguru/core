f967a38cbb00f056e92c15f781bc0422c464bf2b
jeremy <jeremy@trendiguru.com>
2017-02-13 22:06:27 +0200

    removed defense folder

--- defense/defense_testpost.py
+++ defense/defense_testpost.py
@@ -22,5 +22,6 @@ def bring_forth_the_hydra(image_array_or_url, gpu=1):
 
 if __name__ == "__main__":
     url = 'http://www.thecantoncitizen.com/wp-content/uploads/2013/08/dunkin-robbery.jpg'
+    url = 'http://mediaweb.wftv.com/photo/2016/12/02/MTD-HOME%20BURGLARIES%20-%205pm%20-.jpgo_20161202234727517_6738720_ver1.0_640_360.jpg'
     resp = bring_forth_the_hydra(url)
     print(resp)
