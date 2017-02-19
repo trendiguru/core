31dccbcc6d43bfb760b4c0328ca196e53599a13a
yonatan <yonatanguy@gmail.com>
2017-02-19 16:28:35 +0200

    update relevant_items in config.py

---
+++ yonatan/prepare_pixelevel_dresses.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import glob
+import numpy as np
+
+import dlib
+from skimage import io
+import cv2
+
+
+path = "/data/jeremy/image_dbs/pixlevel_images/pixlevel_fullsize_labels_v3"
+
+dress_counter = 0
+dress_list = []
+
+for root, dirs, files in os.walk(path):
+
+    for file in files:
+
+        if not file.endswith(".png"):
+            continue
+
+        labeled_image = cv2.imread(os.path.join(root, file))
+
+        is_dress = np.any(labeled_image == 14)  # 14 is the pixel value represents a dress
+
+        if is_dress:
+            dress_counter += 1
+            dress_list.append(file)
+
+
+
