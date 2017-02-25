48a81aad18628d7670a83e18767b699fb59cbfe6
jeremy <jeremy@trendiguru.com>
2017-02-25 21:39:05 +0200

    start attempt to compare pd to nd+binary all classifier calls thru falcon

--- classifier_stuff/caffe_nns/compare_pd_to_nd_plus_binary.py
+++ classifier_stuff/caffe_nns/compare_pd_to_nd_plus_binary.py
@@ -13,9 +13,12 @@ from trendi import Utils
 from trendi.utils import imutils
 from trendi.paperdoll import hydra_tg_falcon_client
 from trendi.paperdoll import neurodoll_falcon_client
+from trendi.utils import imutils
+from trendi import pipeline
+
 #from trendi import neurodoll
 
-def get_pd_results(image_file):
+def get_live_pd_results(image_file,save_dir='/data/jeremy/image_dbs/tg/pixlevel/pixlevel_fullsize_test_pd_results'):
     #use the api - so first get the image onto the web , then aim the api at it
     copycmd = 'scp '+image_file+' root@104.155.22.95:/var/www/results/pd_test/'+os.path.basename(image_file)
     subprocess.call(copycmd,shell=True)
@@ -23,6 +26,43 @@ def get_pd_results(image_file):
     url = 'http://extremeli.trendi.guru/demo/results/pd_test/'+os.path.basename(image_file)
     resp = pd_falcon_client.pd(url)
     print('resp:'+str(resp))
+    label_dict = resp['label_dict']
+    mask = resp['mask']
+    #label_dict = {fashionista_categories_augmented_zero_based[i]:i for i in range(len(fashionista_categories_augmented_zero_based))}
+    print label_dict
+    if len(mask.shape) == 3:
+        mask = mask[:,:,0]
+
+    before_pd_conclusions_name = os.path.join(save_dir,image_file[:-4]+'_pd.bmp')
+    cv2.imwrite(before_pd_conclusions_name,mask)
+    imutils.show_mask_with_labels(before_pd_conclusions_name,constants.fashionista_categories_augmented,save_images=True)
+
+    after_mask = pipeline.after_pd_conclusions(mask, label_dict)
+    after_pd_conclusions_name = os.path.join(save_dir,image_file[:-4]+'_after_pd_conclusions.bmp')
+    cv2.imwrite(after_pd_conclusions_name,after_mask)
+    imutils.show_mask_with_labels(after_pd_conclusions_name,constants.fashionista_categories_augmented,save_images=True)
+
+
+
+    #pose also available , resp['pose']
+    #make list of labels in ultimate_21 format
+    labels = []
+    for i in len(constants.ultimate_21):
+        if i in np.unique(mask):
+            labels[i] = label_dict[i]
+        else:
+            labels[i] = ''
+    imutils.show_mask_with_labels(mask,labels=labels)
+
+def get_saved_pd_results(mask_file):
+    img_arr = cv2.imread(mask_file)
+    if not img_arr:
+        print('couldnt open '+mask_file)
+        return None
+    vals = np.unique(img_arr)
+    #make sure vals with almost no pixels are tossed out
+    multilabel_result = [(i in vals) for i in range(len(constants.ultimate_21))]
+    print('result from {} has uniques {}, list looks like {}'.format(mask_file,vals,multilabel_result))
 
 def get_hydra_nd_results(url):
     hydra_result = hydra_tg_falcon_client.hydra_tg(url)
@@ -87,3 +127,6 @@ def dl_images(source_domain='stylebook.de',text_filter='',dl_dir='/data/jeremy/i
 
     #move the images with more than one person
     imutils.do_for_all_files_in_dir(imutils.one_person_per_image,dl_dir,visual_output=False)
+
+if __name__ == "__main__":
+    Utils.map_function_on_dir(get_live_pd_results,'/data/jeremy/image_dbs/tg/pixlevel/pixlevel_fullsize_test/')
