31f989b7d1ebceeb2917267d54a306ce116eebf6
jeremy <jeremy@trendiguru.com>
2017-02-27 13:25:50 +0200

    convert pd output at will , save legends, send to webserver

--- downloaders/label_conversions.py
+++ downloaders/label_conversions.py
@@ -183,9 +183,9 @@ def convert_pd_output(mask, label_names, new_labels=constants.fashionista_catego
     success = True #assume innocence until proven guilty
     print('attempting convert and save, shapes:'+str(mask.shape)+' new:'+str(new_mask.shape))
     for label in label_names: # need these in order
+        pd_index = label_names[label]
         if label in new_labels and pd_index in mask:
             new_index = new_labels.index(label) + 0  # number by  0=null, 55=skin  , not 1=null,56=skin
-            pd_index = label_names[label]
             if new_index is None:
                 new_index = 0  #map unused categories (used in fashionista but not pixlevel v2)  to background
             print('pd index '+str(pd_index)+' for '+str(label)+': gets new index:'+str(new_index)+' '+str(new_labels[new_index]))
