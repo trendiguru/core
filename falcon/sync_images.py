80a0b32191d9b14f9258081f7e5a034bf02298b9
Lior Sabag <lior@trendiguru.com>
2017-03-01 18:17:46 +0200

    gevent patch for quick speedup

--- falcon/sync_images.py
+++ falcon/sync_images.py
@@ -49,7 +49,9 @@ class Images(object):
         try:
             if type(images) is list and page_url is not None:
                 if method == 'pd':
-                    relevancy_dict = {url: page_results.handle_post(url, page_url, products, 'pd') for url in images}
+                    relevancy_dict = {url: gevent.spawn(page_results.handle_post, url, page_url, products, 'pd') for url in images}
+                    gevent.joinall(relevancy_dict.values())
+                    relevancy_dict =  {url: green.value for url, green in relevancy_dict.iteritems()}
                     ret["success"] = True
                     ret["relevancy_dict"] = relevancy_dict
                 else:
