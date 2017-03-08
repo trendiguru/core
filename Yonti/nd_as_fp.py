d531f7fd8a6105bee2e8ef5031a2b33a8ed045d3
yonti <yontilevin@gmail.com>
2017-03-08 17:33:56 +0200

    ndasfp

--- Yonti/nd_as_fp.py
+++ Yonti/nd_as_fp.py
@@ -1,15 +1,12 @@
 from ..constants import db
 from ..paperdoll.neurodoll_falcon_client import nd
 from tqdm import tqdm
-import sys
-import os
 
 
 def create_test_collection(category):
     new_col = 'nd_as_fp'
     all_items_in_category = db.shopstyle_US_Female.find({'categories': category})
     for item in tqdm(all_items_in_category):
-        sys.stdout = open(os.devnull, "w")
         try:
             new_obj = {'images': {'XLarge': item['images']['XLarge']},
                        'fp': item['fingerprint'],
@@ -26,5 +23,3 @@ def create_test_collection(category):
         except Exception as e:
             print e
 
-        finally:
-            sys.stdout = sys.__stdout__
