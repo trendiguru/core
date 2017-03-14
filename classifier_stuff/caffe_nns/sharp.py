6ddf6fe2c98680c368eba411899bc4adb686457a
jeremy <jeremy@trendiguru.com>
2017-03-14 19:56:08 +0200

    u crashes while non-u doesnt , trackin gbug

--- classifier_stuff/caffe_nns/sharp.py
+++ classifier_stuff/caffe_nns/sharp.py
@@ -550,9 +550,9 @@ def jr_resnet_u(n_bs=[2,3,5,2],source='trainfile',batch_size=10,nout_initial=64,
         final_cross_index = i
     #    residual = ave_pool(l, 7, stride=1)
 
-    loss = L.SoftmaxWithLoss(l, label)
-    acc = L.Accuracy(l, label, include=dict(phase=getattr(caffe_pb2, 'TEST')))
-    return to_proto(loss, acc)
+    # loss = L.SoftmaxWithLoss(l, label)
+    # acc = L.Accuracy(l, label, include=dict(phase=getattr(caffe_pb2, 'TEST')))
+    # return to_proto(loss, acc)
 
 
     pad = 3
