dff7a24ee6bd8c394e1d84524e938b3ef6e1d290
jeremy <jeremy@trendiguru.com>
2017-04-04 13:43:35 +0300

    put nd on charles, gpu 0

--- paperdoll/unit_test_neurodoll.py
+++ paperdoll/unit_test_neurodoll.py
@@ -19,7 +19,7 @@ class OutcomesTest(unittest.TestCase):
         url = 'http://i.imgur.com/ahFOgkm.jpg'
         print('testing nd on:'+url)
         start_time=time.time()
-        results_dict = nfc.pd(url)
+        results_dict = nfc.nd(url)
         assert(results_dict is not None)
         assert('mask' in results_dict)
         mask = results_dict['mask']
@@ -31,7 +31,7 @@ class OutcomesTest(unittest.TestCase):
         url = 'http://i.imgur.com/ahFOgkm.jpg'
         print('testing nd w get_multilabel_results:'+url)
         start_time=time.time()
-        multilabel_dict = nfc.pd(url, get_multilabel_results=True)
+        multilabel_dict = nfc.nd(url, get_multilabel_results=True)
         assert(multilabel_dict is not None)
         print('multilabel dict:'+str(multilabel_dict))
         print('elapsed:'+str(time.time()-start_time))
@@ -40,7 +40,7 @@ class OutcomesTest(unittest.TestCase):
         url = 'http://i.imgur.com/ahFOgkm.jpg'
         print('testing nd w multilabel combined results:'+url)
         start_time=time.time()
-        multilabel_dict = nfc.pd(url, get_combined_results=True)
+        multilabel_dict = nfc.nd(url, get_combined_results=True)
         assert(multilabel_dict is not None)
         print('combined dict:'+str(multilabel_dict))
         print('elapsed:'+str(time.time()-start_time))
@@ -49,7 +49,7 @@ class OutcomesTest(unittest.TestCase):
         url = 'http://i.imgur.com/ahFOgkm.jpg'
         print('testing nd w get all graylevels:'+url)
         start_time=time.time()
-        multilabel_dict = nfc.pd(url, get_all_graylevels=True)
+        multilabel_dict = nfc.nd(url, get_all_graylevels=True)
         assert(multilabel_dict is not None)
         print('get_all_graylevels dict:'+str(multilabel_dict))
         print('elapsed:'+str(time.time()-start_time))
