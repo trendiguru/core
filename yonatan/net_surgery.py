14c9d1d28c1310ba5dbef02845c7c5e380fc5d5f
yonatan <yonatanguy@gmail.com>
2017-04-06 03:10:01 +0300

    update relevant_items in config.py

--- yonatan/net_surgery.py
+++ yonatan/net_surgery.py
@@ -14,7 +14,7 @@ for layer in all_params: #loops over different layers
     for i in range(len(net.params[layer])): #loops over stuff in layer
         #for instance there may be weights and biases , or just weights
         print "len(net.params[layer]): {0}".format(len(net.params[layer]))
-        params[i] = net.params[layer][i].data
+        # params[i] = net.params[layer][i].data
 
 
 
