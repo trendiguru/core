8e29a50831f3da0a8403014e76840955da6e844d
jeremy <jeremy@trendiguru.com>
2017-04-09 00:14:40 +0300

    ml generation from tb using cosistency check

--- hydra_paper/create_hydra_imagelists.py
+++ hydra_paper/create_hydra_imagelists.py
@@ -1,8 +1,8 @@
 __author__ = 'jeremy'
 from trendi.classifier_stuff.caffe_nns import create_nn_imagelsts
 from trendi import constants
-
-def create_multilabel_imagefile(in_docker=True):
+import pymongo
+def create_single_label_imagefile(in_docker=True):
     '''
     original cats in db are from this list
     web_tool_categories_v2 = ['bag', 'belt', 'cardigan','coat','dress', 'eyewear', 'footwear', 'hat','jacket','jeans',
