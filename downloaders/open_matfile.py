ebcbb65d57738fc1500537a14dac81fc02177900
jeremy <jeremy@trendiguru.com>
2017-04-17 16:32:50 +0300

    attempt to grok mall dataset http://personal.ie.cuhk.edu.hk/~ccloy/downloads_mall_dataset.html

--- downloaders/open_matfile.py
+++ downloaders/open_matfile.py
@@ -43,15 +43,32 @@ def show_rects(n,head_positions):
 #        y=im_height-y
         cv2.rectangle(img_arr,(x-xsize,y-xsize),(x+xsize,y+xsize),color=[255,0,100],thickness=2)
     cv2.imshow('img',img_arr)
+
     cv2.waitKey(0)
+    return(img_arr)
 
 if __name__=="__main__":
     f1 = inspect_matlab_dataset(filename='/data/jeremy/image_dbs/hls/mall_dataset/mall_feat.mat')
 
     frame = load_malldataset()
     print frame.shape
+    n=1
+    name = '/data/jeremy/image_dbs/hls/mall_dataset/frames/seq_%06d'%n+'.jpg'
+    if not os.path.isfile(name):
+        print('{} is not a file'.format(name))
+        exit()
+    print(name)
+    img = cv2.imread(name)
+    height , width , layers =  img.shape
+    video = cv2.VideoWriter('video.xvid',-1,1,(width,height))
+
+
     for n in range(1,200):
         framedat = frame[0,n]
         head_outer = framedat[0,:][0]
         head_positions = head_outer[0]
-        show_rects(n,head_positions)
+        img=show_rects(n,head_positions)
+
+        video.write(img)
+        cv2.destroyAllWindows()
+        video.release()
