7dbcddbe4c078ec5c93c38d6ce47fcb65319c724
yonatan <yonatanguy@gmail.com>
2017-04-20 02:32:41 +0300

    update relevant_items in config.py

--- yonatan/face_landmark_detector.py
+++ yonatan/face_landmark_detector.py
@@ -99,4 +99,7 @@ def detect(url_or_np_array):
                 cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
             print "jjjjj: {}".format(j)
 
-    print cv2.imwrite("/data/yonatan/linked_to_web/face_landmarks/image2.jpg", image)
+    # print cv2.imwrite("/data/yonatan/linked_to_web/face_landmarks/image2.jpg", image)
+
+    cv2.imshow("Output", image)
+    cv2.waitKey(0)
