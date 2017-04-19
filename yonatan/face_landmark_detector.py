b59c89f680c876802b400e0f43738734959336e0
yonatan <yonatanguy@gmail.com>
2017-04-20 01:12:02 +0300

    update relevant_items in config.py

--- yonatan/face_landmark_detector.py
+++ yonatan/face_landmark_detector.py
@@ -136,8 +136,9 @@ def detect(url_or_np_array):
 
         # loop over the (x, y)-coordinates for the facial landmarks
         # and draw them on the image
-        for (x, y) in shape:
+        for j, (x, y) in enumerate(shape):
             cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+            print "jjjjj: {}".format(j)
 
     print cv2.imwrite("/data/yonatan/linked_to_web/face_landmarks/image1.jpg", image)
 
