80e08d36c0a0d3ef7b54673fc9e0548c02768f1b
yonatan <yonatanguy@gmail.com>
2017-04-20 15:59:34 +0300

    update relevant_items in config.py

--- yonatan/face_landmark_detector.py
+++ yonatan/face_landmark_detector.py
@@ -133,7 +133,7 @@ def detect(url_or_np_array):
         k = cv2.waitKey(33)
         # if k == 97:
         if k & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
+            # cv2.destroyAllWindows()
             # cv2.waitKey(1)
             break
         elif k == -1:  # normally -1 returned,so don't print it
