464e19dfd1dda0c877241bdc1a7ec75dd230dd3c
jeremy <jeremy@trendiguru.com>
2017-05-10 00:25:00 +0300

    max image size 1000

--- defense/defense_falcon_yolo.py
+++ defense/defense_falcon_yolo.py
@@ -201,6 +201,11 @@ class HLS_YOLO:
             print('sizes: {} {}'.format(img_arr_bgr,img_arr))
             img_arr = img_arr_bgr
         print('img arr size {}'.format(img_arr.shape))
+        if img_arr.shape[0]>1500 or img_arr.shape[1]>1500:  #maybe this is causing prob at http://a.abcnews.com/images/US/150815_yt_phillypd_33x16_1600.jpg
+            maxside=max(img_arr.shape)
+            reduction_factor = maxside/1000 #force maxside to 1000
+            img_arr = cv2.resize(img_arr,(int(img_arr.shape[1]/reduction_factor),int(img_arr.shape[0]/reduction_factor)))
+            print('reduced size to {}'.format(img_arr.shape))
             #generate randonm filename
         hash = hashlib.sha1()
         hash.update(str(time.time()))
