66b59277ce5324b7e959d48acf96538f62942a39
Lior Sabag <lior@trendiguru.com>
2017-05-27 19:16:27 +0300

    Update create_indexes.py

--- db_stuff/create_indexes.py
+++ db_stuff/create_indexes.py
@@ -1,7 +1,7 @@
 # TODO: Make this generic etc..
 
+# These are based on/necessary for shopstyle_dl script
 db.categories.create_index("id", background=True)
-db.shopstyle_US_Female_cache.create_index?
 db.shopstyle_US_Female_cache.create_index([("dl_version",1),("filter_params", 1)], background=True)
 db.shopstyle_US_Female.create_index("p_hash", background=True)
 db.shopstyle_US_Female.create_index("id", background=True)
