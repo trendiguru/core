ac4b542a829a4b48d328e7f6ece7277bfcdac226
jeremy <jeremy@trendiguru.com>
2017-06-18 19:37:28 +0300

    test faces - what is the good face detector these days

--- classifier_stuff/test_faces.py
+++ classifier_stuff/test_faces.py
@@ -4,29 +4,54 @@ import cv2
 
 from trendi import background_removal
 
-def compare(img_arr):
+def compare_detection_methods(img_arr):
+    cv2.imshow('orig',img_arr)
+
     ffc = background_removal.find_face_ccv(img_arr, max_num_of_faces=100)
     print('ffc:'+str(ffc))
+    if ffc['are_faces'] :
+        faces = ffc['faces']
+        img_ffc = img_arr.copy()
+        for face in faces:
+            cv2.rectangle(img_ffc,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),(255,100,0),5)
+        cv2.imshow('ffc',img_ffc)
 
     ff_cascade = background_removal.find_face_cascade(img_arr, max_num_of_faces=10)
     print('ffcas:'+str(ff_cascade))
+    if ff_cascade['are_faces'] :
+        faces = ff_cascade['faces']
+        img_ff_cascade = img_arr.copy()
+        for face in faces:
+            cv2.rectangle(img_ff_cascade,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),(255,100,0),5)
+        cv2.imshow('ffcascade',img_ff_cascade)
+
 
     ff_dlib = background_removal.find_face_dlib_with_scores(img_arr, max_num_of_faces=100)
     print('ffdlib:'+str(ff_dlib))
+    if ff_dlib['are_faces'] :
+        faces = ff_dlib['faces']
+        img_ff_dlib = img_arr.copy()
+        for face in faces:
+            cv2.rectangle(img_ff_dlib,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),(255,100,0),5)
+        cv2.imshow('ff_dlib',img_ff_dlib)
+    cv2.waitKey(0)
 
+def check_dir(dir):
+    print('doing dir {}'.format(subdir))
+    files = [os.path.join(subdir,f) for f in os.listdir(subdir) if not os.path.isdir(os.path.join(subdir,f))]
+    print('{} files in {}'.format(len(files),subdir))
+    for f in files :
+        img_arr = cv2.imread(f)
+        if img_arr is None:
+            print('got none img arr for {}'.format(f))
+            continue
+        compare_detection_methods(img_arr)
 
 if __name__ == "__main__":
-    dir = '/media/jeremy/9FBD-1B00/data/jeremy/image_dbs/mongo'
-    subdirs = [os.path.join(dir, f) for f in os.listdir(dir)]
+    dir = '/media/jeremy/9FBD-1B00/data/jeremy/image_dbs/mongo/amazon_us_female'
+    subdirs = [os.path.join(dir, d) for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+    print('{} subdirs in {}'.format(len(subdirs),dir))
     for subdir in subdirs:
-        files = [os.path.join(subdir,f) for f in os.listdir(subdir)]
-        print('{} files in {}'.format(len(files),subdir))
-        for file in files :
-            img_arr = cv2.imread(file)
-            if img_arr is None:
-                print('got none img arr for {}'.format(file))
-                continue
-            compare(img_arr)
 
     # background_removal.choose_faces(img_arr, faces_list, max_num_of_faces)
     # background_removal.score_face(face, image)
