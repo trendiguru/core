a9349aa25313e3805e3283c5795739ac50a7cbf8
jeremy <jeremy@trendiguru.com>
2017-06-18 18:09:10 +0300

    test faces - what is the good face detector these days

--- classifier_stuff/test_faces.py
+++ classifier_stuff/test_faces.py
@@ -1,30 +1,37 @@
 __author__ = 'Nadav Paz'
-
-import string
-import collections
 import os
-import dlib
 import cv2
-import numpy as np
-import rq
-from . import constants
-from . import Utils
-from . import ccv_facedetector as ccv
-from . import kassper
-import time
-from functools import partial
+
 from trendi import background_removal
 
-detector = dlib.get_frontal_face_detector()
-
 def compare(img_arr):
-    background_removal.find_face_ccv(image_arr, max_num_of_faces=100):
-    background_removal.find_face_cascade(image, max_num_of_faces=10):
-    background_removal.find_face_dlib_with_scores(image, max_num_of_faces=100):
-    background_removal.choose_faces(image, faces_list, max_num_of_faces):
-    background_removal.score_face(face, image):
-    background_removal.face_is_relevant(image, face):
-    background_removal.is_skin_color(face_ycrcb):
+    ffc = background_removal.find_face_ccv(img_arr, max_num_of_faces=100)
+    print('ffc:'+str(ffc))
+
+    ff_cascade = background_removal.find_face_cascade(img_arr, max_num_of_faces=10)
+    print('ffcas:'+str(ff_cascade))
+
+    ff_dlib = background_removal.find_face_dlib_with_scores(img_arr, max_num_of_faces=100)
+    print('ffdlib:'+str(ff_dlib))
+
+
+if __name__ == "__main__":
+    dir = '/media/jeremy/9FBD-1B00/data/jeremy/image_dbs/mongo'
+    subdirs = [os.path.join(dir, f) for f in os.listdir(dir)]
+    for subdir in subdirs:
+        files = [os.path.join(subdir,f) for f in os.listdir(subdir)]
+        print('{} files in {}'.format(len(files),subdir))
+        for file in files :
+            img_arr = cv2.imread(file)
+            if img_arr is None:
+                print('got none img arr for {}'.format(file))
+                continue
+            compare(img_arr)
+
+    # background_removal.choose_faces(img_arr, faces_list, max_num_of_faces)
+    # background_removal.score_face(face, image)
+    # background_removal.face_is_relevant(image, face)
+    # background_removal.is_skin_color(face_ycrcb):
 # variance_of_laplacian(image):
 # is_one_color_image(image):
 # average_bbs(bb1, bb2):
