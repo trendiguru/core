2f94f127ff1ccccbdd99248dcd4205bf96a496d6
jeremy <jeremy@trendiguru.com>
2017-07-26 18:26:20 +0300

    fixing new pyyolo for use with current nets

--- classifier_stuff/caffe_nns/bb_accuracy.py
+++ classifier_stuff/caffe_nns/bb_accuracy.py
@@ -463,7 +463,7 @@ def get_results_and_analyze(imagelist='/mnt/hls/voc_rio_udacity_kitti_insecam_sh
         if img_arr is None:
             print('could not get img file '+str(imgfile))
             continue
-        proposals = bb_results.get_local_pyyolo_results(img_arr)
+        proposals = bb_results.local_yolo(img_arr)
         print('proposals:'+str(proposals))
         imutils.x1y1x2y2_list_to_xywh(proposals[dict_format['annotations']])
         gt = read_various_training_formats.yolo_to_tgdict(labelfile)
