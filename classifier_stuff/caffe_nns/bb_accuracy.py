44fab30cdd5570d03bfe56487ba33f6f7a3f5f19
jeremy <jeremy@trendiguru.com>
2017-07-29 14:20:58 +0300

    tf approach

--- classifier_stuff/caffe_nns/bb_accuracy.py
+++ classifier_stuff/caffe_nns/bb_accuracy.py
@@ -437,8 +437,8 @@ def mAP_and_iou(gt_detections,guess_detections,dict_format={'annotations':'annot
 def get_results_and_analyze(imagelist='/mnt/hls/voc_rio_udacity_kitti_insecam_shuf_no_aug_test.txt',n_tests=1000,
                             img_dir='/data/jeremy/image_dbs/hls/voc_rio_udacity_kitti_insecam_shuf_no_aug_test/',
                             confidence_threshold = 0.2,
-                            gtfile = '/mnt/hls/voc_rio_udacity_kitti_insecam_shuf_no_aug_gt_labels_run14.json',
-                            proposalsfile = '/mnt/hls/voc_rio_udacity_kitti_insecam_shuf_no_aug_proposal_labelsP_run14.json',
+                            gtfile = '/mnt/hls/voc_rio_udacity_kitti_insecam_shuf_no_aug_gt_labels_tf1.json',
+                            proposalsfile = '/mnt/hls/voc_rio_udacity_kitti_insecam_shuf_no_aug_proposal_labelsP_tf1.json',
                             dict_format = {'annotations':'annotations','bbox_xywh':'bbox_xywh','object':'object','confidence':'confidence'}):
     with open(imagelist,'r') as fp:
         lines = fp.readlines()
